1cab360bd12563fb64a5819b4521ecceb58448bf
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-04-17 19:13:05 +0000

    Added base ORM classes
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@7 698dff77-f410-0410-8918-cf622e150f36

--- pony/main.py
+++ pony/main.py
@@ -1,1 +1,6 @@
 # -*- coding: cp1251 -*-
+
+from pony.orm import Persistent
+from pony.orm import Optional, Required, Unique, PrimaryKey
+from pony.orm import List, Set
+
--- pony/orm.py
+++ pony/orm.py
@@ -1,1 +1,35 @@
 # -*- coding: cp1251 -*-
+
+###############################################################################
+
+class Attribute(object):
+    pass
+
+class Optional(Attribute): pass
+class Required(Attribute): pass
+class Unique(Required): pass
+class PrimaryKey(Unique): pass
+
+class Relation(object): pass
+class Set(Relation): pass
+class List(Relation): pass
+
+###############################################################################
+
+class PersistentMeta(type):
+    pass
+
+###############################################################################
+
+class Persistent(object):
+    __metaclass__ = PersistentMeta
+
+
+
+
+
+
+
+
+
+
