2d35a5445758e58bacede1a39b013d1095803600
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-05-14 14:29:18 +0000

    SQL symbols for AST generation was extracted from 'dbapiprovider.py' into new module, 'sqlsymbols.py'
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@14 698dff77-f410-0410-8918-cf622e150f36

--- pony/dbapiprovider.py
+++ pony/dbapiprovider.py
@@ -2,27 +2,6 @@
 
 import sys, threading
 
-################################################################################
-
-symbols = [ 'SELECT', # 'INSERT', 'UPDATE', 'DELETE',
-            'FROM', 'LEFT_JOIN', 'WHERE', # 'GROUP_BY', 'HAVING',
-            'UNION', 'INTERSECT', 'EXCEPT',
-            'ORDER_BY', 'LIMIT', 'ASC', 'DESC',
-            'DISTINCT', 'ALL', 'AGGREGATES',
-            'COUNT', 'SUM', 'MIN', 'MAX', 'AVG',
-            'TABLE', 'COLUMN', 'PARAM', 'VALUE', 'AND', 'OR', 'NOT',
-            'EQ', 'NE', 'LT', 'LE', 'GT', 'GE', 'IS_NULL', 'IS_NOT_NULL',
-            'LIKE', 'NOT_LIKE', 'BETWEEN', 'NOT_BETWEEN',
-            'IN', 'NOT_IN', 'EXISTS', 'NOT_EXISTS',
-            'ADD', 'SUB', 'MUL', 'DIV', 'CONCAT' ]
-
-globals().update((s, s) for s in symbols)
-
-##binary_ops = dict(EQ='=', NE='<>', LT='<', LE='<=', GT='>', GE='>=',
-##                  ADD='+', SUB='-', MUL='*', DIV='/', CONCAT='||')
-
-################################################################################
-
 def quote_name(name):
     return '"%s"' % name.replace('"', '""')
 
