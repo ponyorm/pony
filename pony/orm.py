e0182ba156b320f83ca8408f8a574de89a878f5b
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-05-17 17:01:11 +0000

    Attribute and Persistent initialization
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@16 698dff77-f410-0410-8918-cf622e150f36

--- pony/orm.py
+++ pony/orm.py
@@ -1,6 +1,6 @@
 # -*- coding: cp1251 -*-
 
-import threading
+import itertools, operator, sys, threading
 
 import utils
 
@@ -70,29 +70,151 @@ class DatabaseInfo(object):
 
 ################################################################################
 
+next_id = itertools.count().next
+
 class Attribute(object):
-    pass
+    __slots__ = '_id', '_initialized', 'name', 'full_name', 'py_type', 'options'
+    def __init__(self, py_type, **options):
+        self._id = next_id()
+        self._initialized = False
+        self.py_type = py_type
+        self.options = options
+        self.name = None
+        self.owner = None
+    def _init_(self):
+        assert not self._initialized
+        if isinstance(self.py_type, type) \
+           and issubclass(self.py_type, Persistent):
+            self._init_reverse()
+        self._initialized = True
+    def _init_reverse(self):
+        t = self.py_type
+        reverse = self.options.get('reverse')
+        if isinstance(reverse, str):
+            reverse = getattr(t, reverse, None)
+            if reverse is None: raise AttributeError(
+                'Reverse attribute for %s not found' % self)
+        if reverse is None:
+            candidates = [ attr for attr in t._attrs_
+                                if attr.py_type is self.owner
+                                and attr.options.get('reverse') \
+                                    in (self, self.name, None)  ]
+            for attr in candidates:
+                if attr.options.get('reverse') in (self, self.name):
+                    if reverse is not None: raise AttributeError(
+                        'Ambiguous reverse attribute for %s' % self)
+                    reverse = attr
+            if reverse is None:
+                if len(candidates) == 0: raise AttributeError(
+                    'Reverse attribute for %s not found' % self)
+                if len(candidates) > 1: raise AttributeError(
+                    'Ambiguous reverse attribute for %s' % self)
+                reverse = candidates[0]
+        if not isinstance(reverse, Attribute): raise AttributeError(
+            'Incorrect reverse attribute type for %s' % self)
+        if reverse.py_type is not self.owner \
+           or reverse.options.get('reverse') not in (self, self.name, None):
+            raise AttributeError('Inconsistent attributes %s and %s'
+                                 % (self, reverse))
+        self.options['reverse'] = reverse
+        assert reverse.options.get('reverse') in (self, self.name, None)
+        reverse.options['reverse'] = self
+    def __str__(self):
+        if self.owner is None: return '?.%s' % (self.__name__ or '?')
+        return '%s.%s' % (self.owner.__name__, self.name or '?')
+    def __repr__(self):
+        return '<%s : %s>' % (self, self.__class__.__name__)
+    def __get__(self, obj, type=None):
+        return self
+    def __set__(self, obj, value):
+        pass
+    def __delete__(self, obj):
+        pass
 
 class Optional(Attribute): pass
 class Required(Attribute): pass
-class Unique(Required): pass
+class Unique(Required):
+    __slots__ = 'attrs',
+    def __init__(self, *type_or_attrs):
+        if len(type_or_attrs) == 0:
+            assert TypeError('Invalid count of positional arguments')
+        elif len(type_or_attrs) == 1:
+            py_type = type_or_attrs
+            Attribute.__init__(self, py_type)
+            self.attrs = None
+        else:
+            Attribute.__init__(self, None)
+            self.attrs = type_or_attrs
+        cls_dict = sys._getframe(1).f_locals
+        cls_dict.setdefault('_keys_', []).append(self)
+       
 class PrimaryKey(Unique): pass
-
-class Relation(object): pass
-class Set(Relation): pass
-class List(Relation): pass
+        
+class Collection(Attribute): pass
+class Set(Collection): pass
+class List(Collection): pass
 
 ################################################################################
 
+class _PonyInfo(object):
+    __slots__ = 'persistent_classes', 'uninitialized_attrs'
+    def __init__(self):
+        self.persistent_classes = {}  # map(class_name -> class)
+        self.uninitialized_attrs = {} # map(referenced_class_name -> attr_list)
+
 class PersistentMeta(type):
-    pass
+    def __init__(cls, cls_name, bases, cls_dict):
+        super(PersistentMeta, cls).__init__(cls_name, bases, dict)
+        outer_dict = sys._getframe(1).f_locals
+        pony_info = outer_dict.get('_pony_info_')
+        if pony_info is None:
+            pony_info = outer_dict['_pony_info_'] = _PonyInfo()
+        cls._cls_init_(cls_name, pony_info)
 
 ################################################################################
 
 class Persistent(object):
     __metaclass__ = PersistentMeta
+    @classmethod
+    def _cls_init_(cls, cls_name, info):
+        info.persistent_classes[cls_name] = cls
+        my_attrs = cls._attrs_ = []
+        for attr_name, x in cls.__dict__.items():
+            if isinstance(x, Attribute):
+                my_attrs.append(x)
+                x.name = attr_name
+                x.owner = cls
+        my_attrs.sort(key = operator.attrgetter('_id'))
 
+        other_attrs = info.uninitialized_attrs.get(cls_name, [])
+        for attr in other_attrs:
+            attr.py_type = cls
+            if 'reversed' in attr.options:
+                other_attrs.remove(attr)
+                attr._init_()
+        for attr in my_attrs:
+            if isinstance(attr.py_type, str):
+                other_name = attr.py_type
+                other_cls = info.persistent_classes.get(other_name)
+                if other_cls is None:
+                    u = info.uninitialized_attrs
+                    u.setdefault(other_name, []).append(attr)
+                    continue
+                else: attr.py_type = other_cls
+            if 'reverse' in attr.options: attr._init_()
 
+        for attr in other_attrs: attr._init_()
+        for attr in my_attrs:
+            if not isinstance(attr.py_type, str):
+                attr._init_()
+
+        if not hasattr(cls, '_keys_'): cls._keys_ = []
+        for key in cls._keys_:
+            if not key._initialized:
+                key.name = None
+                key.owner = cls
+                key.py_type = None
+                key._init_()
 
 
 
