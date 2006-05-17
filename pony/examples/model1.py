52e0735dd7b7b909537ffbd14c434478735208bd
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-05-17 17:03:44 +0000

    Example of simple model
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@17 698dff77-f410-0410-8918-cf622e150f36

---
+++ pony/examples/model1.py
@@ -0,0 +1,31 @@
+# -*- coding: cp1251 -*-
+
+from pony.main import *
+
+class Group(Persistent):
+    number = PrimaryKey(str)
+    faculty = Required(int)
+    graduate_year = Required(int)
+    speciality = Required(int)
+    subjects = Set('Subject')
+    students = Set('Student')
+
+class Subject(Persistent):
+    name = PrimaryKey(unicode)
+    groups = Set(Group)
+    marks = Set('Mark')
+
+class Student(Persistent):
+    number = PrimaryKey(int)
+    first_name = Required(unicode)
+    mid_name = Optional(unicode)
+    last_name = Required(unicode)
+    group = Required(Group)
+    marks = Set('Mark')
+
+class Mark(Persistent):
+    student = Required(Student)
+    subject = Required(Subject)
+    value = Required(int)
+    PrimaryKey(student, subject)
+    
