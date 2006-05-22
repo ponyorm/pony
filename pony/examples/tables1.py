eb1d09df199930ea0073c523d3d384be9ea417e3
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-05-22 07:13:46 +0000

    Syntax changed: table.column -> table['column']
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@27 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/tables1.py
+++ pony/examples/tables1.py
@@ -4,30 +4,30 @@ import pony
 from pony import *
 
 GROUP = Table('Group')
-GROUP.number = Column(str, primary_key=True)
-GROUP.faculty = Column(int, not_null=True)
-GROUP.graduate_year = Column(int, not_null=True)
+GROUP['number'] = Column(str, primary_key=True)
+GROUP['faculty'] = Column(int, not_null=True)
+GROUP['graduate_year'] = Column(int, not_null=True)
 
 SUBJECT = Table('Subject')
-SUBJECT.name = Column(unicode, primary_key=True)
+SUBJECT['name'] = Column(unicode, primary_key=True)
 
 GROUP_SUBJECT = Table('Group_Subject')
-GROUP_SUBJECT.group = Column(str, not_null=True, foreign_key=GROUP.number)
-GROUP_SUBJECT.subject = Column(unicode, not_null=True,
-                               foreign_key = SUBJECT.name)
+GROUP_SUBJECT['group'] = Column(str, not_null=True, foreign_key=GROUP.number)
+GROUP_SUBJECT['subject'] = Column(unicode, not_null=True,
+                                  foreign_key = SUBJECT.name)
 
 STUDENT = Table('Student')
-STUDENT.number = Column(int, primary_key=True)
-STUDENT.first_name = Column(unicode, not_null=True)        
-STUDENT.mid_name = Column(unicode, not_null=False)
-STUDENT.last_name = Column(unicode, not_null=True)
-STUDENT.group = Column(str, not_null=True, foreign_key=GROUP.number)
+STUDENT['number'] = Column(int, primary_key=True)
+STUDENT['first_name'] = Column(unicode, not_null=True)        
+STUDENT['mid_name'] = Column(unicode, not_null=False)
+STUDENT['last_name'] = Column(unicode, not_null=True)
+STUDENT['group'] = Column(str, not_null=True, foreign_key=GROUP.number)
 
 MARK = Table('Mark')
-MARK.student = Column(int, not_null=True, foreign_key=STUDENT.number)
-MARK.subject = Column(unicode, not_null=True, foreign_key=SUBJECT.name)
-MARK.value = Column(int, not_null=True)
-MARK._primary_key_ = MARK.student, MARK.subject
+MARK['student'] = Column(int, not_null=True, foreign_key=STUDENT.number)
+MARK['subject'] = Column(unicode, not_null=True, foreign_key=SUBJECT.name)
+MARK['value'] = Column(int, not_null=True)
+MARK.set_primary_key(MARK.student, MARK.subject)
 
 
 
