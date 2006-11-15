5992465b297269dd05a02337bee97616a3ccf2d1
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-11-15 14:24:09 +0000

    Tables example changed
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@50 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/tables1.py
+++ pony/examples/tables1.py
@@ -1,45 +1,27 @@
-# -*- coding: cp1251 -*-
-
 from pony.orm import *
 
-GROUP = Table('Group')
-GROUP['number'] = Column(str)
-GROUP['faculty'] = Column(int, not_null=True)
-GROUP['graduate_year'] = Column(int, not_null=True)
-GROUP.set_primary_key('number')
+m = Mapping()
 
-SUBJECT = Table('Subject')
-SUBJECT['name'] = Column(unicode)
-SUBJECT.set_primary_key('name')
+Group = m.table('Group',
+    Column('number', str, pk=True),
+    Column('faculty', int, not_null=True),
+    Column('graduate_year', int, not_null=True))
 
-GROUP_SUBJECT = Table('Group_Subject')
-GROUP_SUBJECT['group'] = Column(str, not_null=True, foreign_key=GROUP['number'])
-GROUP_SUBJECT['subject'] = Column(unicode, not_null=True,
-                                  foreign_key = SUBJECT['name'])
-GROUP_SUBJECT.set_primary_key('group', 'subject')
+Subject = m.table('Subject',
+    Column('name', unicode, pk=True))
 
-STUDENT = Table('Student')
-STUDENT['number'] = Column(int)
-STUDENT['first_name'] = Column(unicode, not_null=True)        
-STUDENT['mid_name'] = Column(unicode)
-STUDENT['last_name'] = Column(unicode, not_null=True)
-STUDENT['group'] = Column(str, not_null=True, foreign_key=GROUP['number'])
-STUDENT.set_primary_key('number')
+Group_Subject = m.table('Group_Subject',
+    Column('group', str, pk=True, fk=Group['number']),
+    Column('subject', unicode, pk=True, fk=Subject['name']))
 
-MARK = Table('Mark')
-MARK['student'] = Column(int, not_null=True, foreign_key=STUDENT['number'])
-MARK['subject'] = Column(unicode, not_null=True, foreign_key=SUBJECT['name'])
-MARK['value'] = Column(int, not_null=True)
-MARK.set_primary_key(MARK['student'], MARK['subject'])
-
-
-
-
-
-
-
-
-
-
-
-
+Student = m.table('Student',
+    Column('number', int, pk=True),
+    Column('first_name', unicode, not_null=True)
+    Column('mid_name', unicode),
+    Column('last_name', unicode, not_null=True),
+    Column('group', str, not_null=True, fk=Group['number']))
+              
+Mark = m.table('Mark',
+    Column('student', int, not_null=True, pk=True, fk=Student['number']),
+    Column('subject', unicode, not_null=True, pk=True, fk=Subject['name'])
+    Column('value', int, not_null=True))
