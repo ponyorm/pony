7ee3067f1370550d5ebdb0c13bd92027388ccd17
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2006-11-15 14:17:28 +0000

    orm.py module was removed
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@49 698dff77-f410-0410-8918-cf622e150f36

--- pony/utils.py
+++ pony/utils.py
@@ -5,67 +5,8 @@ from operator import itemgetter
 from os import urandom
 from re import compile
 
-class NameMapMixin(object):
-    __slots__ = '__list', '__dict'
-    def __init__(self):
-        self.__list = [] # list of (name, value) pairs or None values
-        self.__dict = {} # mapping: name -> index in list
-    def __len__(self):
-        return len(self.__dict)
-    def __iter__(self):
-        return imap(itemgetter(1), ifilter(None, self.__list))
-    def __contains__(self, key):
-        return key in self.__dict
-    def __getitem__(self, x):
-        if isinstance(x, basestring): return self.__list[self.__dict[x]][1]
-        if len(self.__list) != len (self.__dict): self._pack()
-        if isinstance(x, slice): return map(itemgetter(1), self.__list[x])
-        return self.__list[x][1]
-    def __setitem__(self, name, value):
-        if not isinstance(name, basestring):
-            raise TypeError('Unexpected name type')
-        new = len(self.__list)
-        index = self.__dict.setdefault(name, new)
-        if index is new: self.__list.append((name, value))
-        else: self.__list[index] = (name, value)
-    def __delitem__(self, x):
-        if isinstance(x, basestring):
-            self.__list[self.__dict.pop(x)] = None
-            if len(self.__list) > len(self.__dict) * 2: self._pack()
-            return
-        if len(self.__list) != len(self.__dict):
-            self.__list = filter(None, self.__list)
-        del self.__list[x]
-        self.__dict = dict((name, i) for i, (name, value)
-                                     in enumerate(self.__list))
-    def _pack(self):
-        self.__list = filter(None, self.__list)
-        self.__dict = dict((name, i) for i, (name, value)
-                                     in enumerate(self.__list))
-    def _clear(self):
-        self.__list = []
-        self.__dict = {}
-    def _get(self, key, default=None):
-        try: index = self.__dict[key]
-        except KeyError: return default
-        return self.__list[index][1]
-    
-
 def error_method(*args, **kwargs):
     raise TypeError
-
-class FrozenDict(dict):
-    "Dictionary subclass with __hash__ method. Cannot be modified."
-
-    def __init__(self, x):
-        dict.__init__(self, x)
-        self._hash = hash(tuple(sorted(self.iteritems())))
-
-    __setitem__ = __delitem__ = clear = update = setdefault = pop = popitem \
-        = error_method
-
-    def __hash__(self):
-        return self._hash
 
 ident_re = compile(r'[A-Za-z_]\w*')
 
