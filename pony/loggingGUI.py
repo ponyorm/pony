4d3c5fd8762e4c5962118e1cfd55dabf59feb6ff
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-05-14 17:40:11 +0000

    GUI refactoring, part three: TabSet component, request and response grids
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@175 698dff77-f410-0410-8918-cf622e150f36

--- pony/loggingGUI.py
+++ pony/loggingGUI.py
@@ -13,7 +13,7 @@ class Grid(object):
         self.parent = parent
         self.frame = Frame(parent, **params)
         self.listboxes = []
-        for name, width in column_list:
+        for name, width, fixed in column_list:
             col_frame = Frame(self.frame)
             label = Label(col_frame, text=name, relief=RIDGE)
             listbox = Listbox(col_frame, width=width, height=18,
@@ -21,10 +21,10 @@ class Grid(object):
             listbox.bind('<Button-1>', self.select)
             listbox.bind('<B1-Motion>', self.select)
             listbox.bind('<Leave>', lambda e: 'break')
-            if name == 'timestamp':
+            if fixed:
                 col_frame.pack(side=LEFT, expand=N, fill=Y)
                 label.pack(fill=X)
-                listbox.pack(side=LEFT, expand=Y, fill=Y)
+                listbox.pack(side=LEFT, expand=N, fill=Y)
             else:
                 col_frame.pack(side=LEFT, expand=Y, fill=BOTH)
                 label.pack(fill=X)
@@ -49,18 +49,49 @@ class Grid(object):
         return 'break'
     def on_select(self, row_num):
         pass
-    def add_row(self, *args):
+    def add(self, *args):
         assert len(args) == len(self.listboxes)
         for listbox, text in zip(self.listboxes, args):
             listbox.insert(END, text)
         if self._show_last_record:
             for listbox in self.listboxes: listbox.see(END)
+    def clear(self):
+        for listbox in self.listboxes: listbox.delete(0, END)
+        self.selected = -1
     def _set_show_last_record(self, value):
         self._show_last_record = value
         if value:
             for listbox in self.listboxes: listbox.see(END)
     show_last_record = property(attrgetter('_show_last_record'),
                                 _set_show_last_record)
+
+class TabSet(object):
+    def __init__(self, parent, **params):
+        self.parent = parent
+        self.frame = Frame(parent, **params)
+        self.top = Frame(self.frame, relief=GROOVE, borderwidth=1)
+        self.top.pack(side=TOP, expand=N, fill=X)
+        self.content = Frame(self.frame)
+        self.content.pack(side=TOP, expand=Y, fill=BOTH)
+        self.buttons = []
+        self.tabs = []
+        self.current_tab = None
+        self.current_tab_number = IntVar(0)
+    def add(self, tab_name):
+        tab = Frame(self.content)
+        tab_number = len(self.tabs)
+        button = Radiobutton(self.top, text=tab_name, width=20, indicatoron=0,
+                             variable=self.current_tab_number, value=tab_number,
+                             command=lambda: self.display_tab(tab))
+        button.pack(side=LEFT)
+        self.tabs.append(tab)
+        self.buttons.append(button)
+        if not tab_number: self.display_tab(tab)
+        return tab
+    def display_tab(self, tab):
+        if self.current_tab is not None: self.current_tab.forget()
+        tab.pack(expand=Y, fill=BOTH)
+        self.current_tab=tab
 
 class ViewerWidget(Frame):
     def __init__(self, root): 
@@ -74,22 +105,35 @@ class ViewerWidget(Frame):
         self.headers={} 
         self.tab_buttons=[]
 
-        self.current_tab = None
-        self.current_tab_n = IntVar(0)
-        self.tab_count = 0
-
         screen_w = root.winfo_screenwidth()
         screen_h = root.winfo_screenheight()
         root.geometry("%dx%d%+d%+d" % (w, h, (screen_w-w)/2, (screen_h-h)/2))
 
-        self.grid = Grid(self, [('timestamp', 20), ('type', 20)], height=h/2)
+        self.grid = Grid(self, [('timestamp', 20, True), ('type', 20, False)],
+                         height=h/2)
         self.grid.frame.pack(side=TOP, fill=X)
         self.grid.show_last_record = True
         self.grid.on_select = self.show_record
-          
-        self.down=Frame(self, height=h/2, relief=GROOVE, borderwidth=2)
-        self.down.pack(side=TOP, expand=YES, fill=BOTH)        
-        self.create_tabs(self.down)
+
+        self.tabs = TabSet(self, height=h/2, relief=GROOVE, borderwidth=2)
+        self.tabs.frame.pack(side=TOP, expand=YES, fill=BOTH)
+
+        self.request_tab = self.tabs.add("request")
+        self.request_headers = Grid(self.request_tab,
+                                    [("HTTP request header", 20, False),
+                                     ("value", 20, False)])
+        self.request_headers.frame.pack(side=TOP, expand=YES, fill=BOTH)
+
+        self.response_tab = self.tabs.add("response")
+        self.response_info = Label(self.response_tab, text='', justify=LEFT,
+                                   font='Helvetica 8 bold',
+                                   wraplength=self.width-20)
+        self.response_info.pack(side=TOP, anchor=W)
+        self.response_headers = Grid(self.response_tab,
+                                     [("HTTP response header", 20, False),
+                                      ("value", 20, False)])
+        self.response_headers.frame.pack(side=TOP, expand=YES, fill=BOTH)
+        
         self.makemenu()
         self.pack(expand=YES, fill=BOTH)
         self.ds=DataSupplier(self)
@@ -114,116 +158,30 @@ class ViewerWidget(Frame):
         self.grid.show_last_record = self.showLastRecord.get()
 
     def show_since_start(self):
+        self.grid.clear()
         self.clear_tabs()
-        self.main_col_list[0].delete(0, END)
-        self.main_col_list[1].delete(0, END)
         self.ds.stop()
         self.ds=DataSupplier(self, loadLastOnly=self.showSinceStart.get())
         #self.ds.loadLastOnly=self.showSinceStart.get()
         self.ds.start()
 
-    def _create_tab_blank(self, tab_name, tab_number):
-        tab_field=Frame(self.tab_body_field)       
-        tab_button = Radiobutton(self.tab_buttons_field, text=tab_name, width=20, indicatoron=0,
-                                 variable=self.current_tab_n , value=tab_number,
-                                 command=lambda tab_field=tab_field: self.display_tab(tab_field))
-        tab_button.pack(side=LEFT)
-        self.tab_buttons.append(tab_button)
-        return tab_field
-
-    def create_summary_tab(self):
-        summary_tab=self._create_tab_blank("summary", 0) 
-        summary_frame=Frame(summary_tab)
-        summary_frame.pack(expand=Y, fill=BOTH)
-        self.summary_field=Text(summary_tab)
-        self.summary_field.pack(expand=Y, fill=BOTH)
-        return summary_tab 
-
-    def create_request_tab(self):
-        def scroll_hdrs(*args):
-            self.req_lb1.yview(*args)
-            self.req_lb2.yview(*args)
-
-        def select_row(e):
-            row = self.req_lb1.nearest(e.y)
-            for lb in (self.req_lb1, self.req_lb2):
-                lb.select_clear(0,END)
-                lb.select_set(row)
-            return 'break'
-
-        request_tab=self._create_tab_blank("request", 1) 
-        headers_frame=Frame(request_tab)
-        headers_frame.pack(expand=Y, fill=BOTH)
-        self.req_lb1=Listbox(headers_frame, borderwidth=0, width=10, exportselection=FALSE)
-        self.req_lb1.pack(side=LEFT, expand=Y, fill=BOTH) 
-        self.req_lb2=Listbox(headers_frame, borderwidth=0, width=90, exportselection=FALSE)
-        self.req_lb2.pack(side=LEFT, expand=Y, fill=BOTH)
-        for lb in (self.req_lb1, self.req_lb2):
-            lb.bind('<Button-1>', select_row)
-            lb.bind('<B1-Motion>', select_row)          
-            lb.bind('<Leave>', lambda e: 'break')      
-        sb=Scrollbar(headers_frame, orient=VERTICAL, command=scroll_hdrs)
-        self.req_lb1.config(yscrollcommand=sb.set)
-        sb.pack(side=RIGHT, fill=Y)
-        return request_tab
-
-    def create_response_tab(self): 
-        def scroll_hdrs(*args):
-            self.res_lb1.yview(*args)
-            self.res_lb2.yview(*args)
-
-        def select_row(e):
-            row = self.res_lb1.nearest(e.y)
-            for lb in (self.res_lb1, self.res_lb2):
-                lb.select_clear(0,END)
-                lb.select_set(row)
-            return 'break'
-       
-        response_tab=self._create_tab_blank("response", 2) 
-        self.response_info=Label(response_tab, text='', justify=LEFT,
-                           font='Helvetica 8 bold', wraplength= self.width-20)
-        self.response_info.pack(side=TOP, anchor=W) 
-        headers_frame=Frame(response_tab)
-        headers_frame.pack(expand=Y, fill=BOTH)
-        self.res_lb1=Listbox(headers_frame, borderwidth=0, width=10, exportselection=FALSE)
-        self.res_lb1.pack(side=LEFT, expand=Y, fill=BOTH) 
-        self.res_lb2=Listbox(headers_frame, borderwidth=0, width=90, exportselection=FALSE)
-        self.res_lb2.pack(side=LEFT, expand=Y, fill=BOTH)
-        for lb in (self.res_lb1, self.res_lb2):
-            lb.bind('<Button-1>', select_row)
-            lb.bind('<B1-Motion>', select_row)          
-            lb.bind('<Leave>', lambda e: 'break')      
-        sb=Scrollbar(headers_frame, orient=VERTICAL, command=scroll_hdrs)
-        self.req_lb1.config(yscrollcommand=sb.set)
-        sb.pack(side=RIGHT, fill=Y)
-
-
-    def create_tabs(self, parent):  
-        self.tab_buttons_field=Frame(parent, relief=GROOVE, borderwidth=1)
-        self.tab_buttons_field.pack(side=TOP, expand=N, fill=X)
-       
-        self.tab_body_field=Frame(parent)
-        self.tab_body_field.pack(side=TOP, expand=Y, fill=BOTH)
-
-        # self.current_tab=self.create_summary_tab()
-        self.current_tab = self.create_request_tab()
-        self.create_response_tab()
-        self.current_tab.pack(expand=Y, fill=BOTH) #show the first tab          
-
-    def display_tab(self, tab):
-        self.current_tab.forget()
-        tab.pack(expand=Y, fill=BOTH)
-        self.current_tab=tab
+##    def create_summary_tab(self):
+##        summary_tab=self._create_tab_blank("summary", 0) 
+##        summary_frame=Frame(summary_tab)
+##        summary_frame.pack(expand=Y, fill=BOTH)
+##        self.summary_field=Text(summary_tab)
+##        self.summary_field.pack(expand=Y, fill=BOTH)
+##        return summary_tab 
 
     def add_record(self, rec):       
         self.records.append(rec)
-        self.grid.add_row(rec.data['timestamp'][:-7], rec.data['text'])
+        self.grid.add(rec.data['timestamp'][:-7], rec.data['text'])
 
     def clear_tabs(self):
         # self.summary_field.delete(1.0, END)
         self.response_info.config(text='')
-        for lb in (self.req_lb1 , self.req_lb2, self.res_lb1, self.res_lb2):
-            lb.delete(0, END)
+        self.request_headers.clear()
+        self.response_headers.clear()
 
     def show_record(self, rec_no):
         rec=self.records[rec_no]
@@ -247,31 +205,33 @@ class ViewerWidget(Frame):
 class Record(object):
     def __init__(self, data):
         self.data = data
+    def draw(self, widget):
+        pass
 
 class StartRecord(Record):
-    def draw(self, widget):
-        for i, button in enumerate(widget.tab_buttons ):
-            if i == 0:
-                button.select()
-                button.invoke()
-            else: button.forget()              
-        # data = self.data
-        # txt='TIMESTAMP: \t%s\nTEXT: \t\t%s' % (data['timestamp'], data['text'])
-        # widget.summary_field.insert(END, txt)
+    pass
+##    def draw(self, widget):
+##        for i, button in enumerate(widget.tabs.buttons ):
+##            if i == 0:
+##                button.select()
+##                button.invoke()
+##            else: button.forget()              
+##        # data = self.data
+##        # txt='TIMESTAMP: \t%s\nTEXT: \t\t%s' % (data['timestamp'], data['text'])
+##        # widget.summary_field.insert(END, txt)
 
 class RequestRecord(Record): 
     def draw(self, widget):
         data = self.data
-        for i, button in enumerate(widget.tab_buttons):
-            if i == 0:
-                pass
-                #button.select() 
-                #button.invoke()
-            else: button.pack(side=LEFT)
+##        for i, button in enumerate(widget.tabs.buttons):
+##            if i == 0:
+##                pass
+##                #button.select() 
+##                #button.invoke()
+##            else: button.pack(side=LEFT)
         # widget.summary_field.insert(END, "some summary text")
         for k, v in data['headers'].items():
-            widget.req_lb1.insert(END, k)
-            widget.req_lb2.insert(END, v)
+            widget.request_headers.add(k, v)
         crit='process_id=%i and thread_id=%i' % (data['process_id'], data['thread_id'])
         rows=search_log(criteria=crit, start_from=data['id'], max_count=-1)
         for rec in rows:
@@ -284,8 +244,7 @@ class RequestRecord(Record):
                 txt='TEXT: \t%s %s\nDELAY: \t%s' % (rec['type'], rec['text'], delta)
                 widget.response_info.config(text=txt)
                 for k, v in rec['headers']:
-                    widget.res_lb1.insert(END, k)
-                    widget.res_lb2.insert(END, v)
+                    widget.response_headers.add(k, v)
                 #break
 
 class DataSupplier(threading.Thread):
