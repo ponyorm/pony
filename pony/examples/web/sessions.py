4d22568237870560747342d68c9580fdb91b62b1
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-05-16 17:29:03 +0000

    Extended session example
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@187 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/web/sessions.py
+++ pony/examples/web/sessions.py
@@ -1,15 +1,22 @@
 from pony.main import *
 
+import random
+
 use_autoreload()
 
 @http('/')
 def index():
     user = get_user()
+    session = get_session()
+    if 'x' in session: session['x'] += 1
     return html()
 
 @http('/login?user=$user')
 def login(user=None):
     if user: set_user(user)
+    session = get_session()
+    session['x'] = 0
+    session['y'] = random.random()
     return html()
 
 @http('/logout')
@@ -19,3 +26,4 @@ def logout():
     return html()
 
 start_http_server('localhost:8080')
+show_gui()
