bfc3bb470ded92dbbe78317e6c00627ae45d7f02
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-05-31 08:55:53 +0000

    Work-around lxml (or libxslt2) bug
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@217 698dff77-f410-0410-8918-cf622e150f36

--- pony/xslt.py
+++ pony/xslt.py
@@ -23,10 +23,12 @@ def transform(xml):
 xml2html_template = """
 <xsl:stylesheet version = '1.0'
                 xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
-    <xsl:output method="html" encoding="%s" indent="%s" %s/>
-    <xsl:template match="*">
-        <xsl:copy-of select="*" />
-    </xsl:template>
+  <xsl:output method="html" encoding="%s" indent="%s" %s/>
+  <xsl:template match="/ | @* | node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@* | node()" />
+    </xsl:copy>
+  </xsl:template>
 </xsl:stylesheet>
 """
 
