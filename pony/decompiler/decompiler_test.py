66fc96059030a26b046e0fa8b8c11501e231cef0
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-06-05 07:52:22 +0000

    Replace tabs to spaces
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@223 698dff77-f410-0410-8918-cf622e150f36

--- pony/decompiler/decompiler_test.py
+++ pony/decompiler/decompiler_test.py
@@ -7,44 +7,42 @@ class TestDecompiler:
 
     def test_1(self):
         g =        (s for s in Student)
-	expected = "s for s in .0"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "s for s in .0"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_2(self):
         g =        ( (s,d,w) for t in Student   if ((4 != x.amount or amount * 3 > 20 or amount * 2 < 5) and amount * 8 == 20))
-	expected = "['w', 'd', 's'] for t in .0 if ((((4 != x.amount) or ((amount * 3 > 20) or (amount * 2 < 5)))) and (amount * 8 == 20))"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "['w', 'd', 's'] for t in .0 if ((((4 != x.amount) or ((amount * 3 > 20) or (amount * 2 < 5)))) and (amount * 8 == 20))"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_3(self):
         g =   (s for s in Student if   s.age > 20 and   (s.group.number == 4142  or 'Math' in s.marks.subject.name))
-	expected = "s for s in .0 if ((s.age > 20) and ((s.group.number == 4142) or (Math in s.marks.subject.name)))"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "s for s in .0 if ((s.age > 20) and ((s.group.number == 4142) or (Math in s.marks.subject.name)))"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_4(self):
         g =        ( (s,d,w) for t in Student   if ((4 != x.a) or (a * 3 > 20) or (a * 2 < 5) and (a * 8 == 20)))
-	expected = "['w', 'd', 's'] for t in .0 if ((4 != x.a) or ((a * 3 > 20) or ((a * 2 < 5) and (a * 8 == 20))))"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "['w', 'd', 's'] for t in .0 if ((4 != x.a) or ((a * 3 > 20) or ((a * 2 < 5) and (a * 8 == 20))))"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_5(self):
         g =        ( (s,d,w) for t in Student   if ( 4 != x.a  or  a * 3 > 20 ) and ( a * 2 < 5  or  a * 8 == 20 ))
-	expected = "['w', 'd', 's'] for t in .0 if ((((4 != x.a) or (a * 3 > 20))) and ((a * 2 < 5) or (a * 8 == 20)))"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "['w', 'd', 's'] for t in .0 if ((((4 != x.a) or (a * 3 > 20))) and ((a * 2 < 5) or (a * 8 == 20)))"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_6(self):
         g =   (s for t in Student if a == 5)
-	expected = "s for t in .0 if a == 5"
-	result = decompile_tostring(g)
-	assert result == expected
+        expected = "s for t in .0 if a == 5"
+        result = decompile_tostring(g)
+        assert result == expected
 
     def test_7(self):
         g =       ( (s,t,w) for t in Student if ((4 != x.a.b or a * 3 > 20 or a * 2 < 5 and v == 6) and a * 8 == 20 or (f > 4) ))
-	expected = "['w', 't', 's'] for t in .0 if ((((((4 != x.a.b) or ((a * 3 > 20) or ((a * 2 < 5) and (v == 6))))) and (a * 8 == 20))) or (f > 4))"
-	result = decompile_tostring(g)
-	assert result == expected
-
-
+        expected = "['w', 't', 's'] for t in .0 if ((((((4 != x.a.b) or ((a * 3 > 20) or ((a * 2 < 5) and (v == 6))))) and (a * 8 == 20))) or (f > 4))"
+        result = decompile_tostring(g)
+        assert result == expected
