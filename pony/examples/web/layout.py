4ab649f240e6e29c687c84723769bdd4887263c4
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-06-24 02:31:45 +0000

    Layout example was redone
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@240 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/web/layout.py
+++ pony/examples/web/layout.py
@@ -1,28 +1,9 @@
-# -*- coding: cp1251 -*-
-
 from pony.main import *
 
 use_autoreload()
 
-def header(title):
-    return html('''
-        <title>$title</title>
-        <header>
-          <h1>$title</h1>
-        </header>
-        ''')
-
-@http("/")
+@http('/')
 def index():
     return html()
-
-@http
-def page1():
-    pass
-
-
-def sidebar():
-    return html("")
-    
 
 start_http_server()
