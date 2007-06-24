7f7c2bb1a6c408193a5eaff02cdfa58632fa60ac
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-06-24 02:20:17 +0000

    HTML normalization completely redone again
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@238 698dff77-f410-0410-8918-cf622e150f36

--- pony/_xslt.py
+++ pony/_xslt.py
@@ -13,150 +13,130 @@ def html2xml(x, encoding='ascii'):
     if isinstance(x, str): x = unicode(x, encoding)
     return etree.HTML(x)
 
-block_level_tags = set('''
-    address blockquote center dir div dl fieldset form h1 h2 h3 h4 h5 h6 hr
-    isindex menu noframes noscript ol p pre table ul'''.split())
-
-layout_tags = set('header footer sidebar content column layout'.split())
-layout_tags_xpath = etree.XPath('|'.join(layout_tags))
-
-def normalization_is_needed(html):
+def normalize(html):
+    # print etree.tostring(html, pretty_print=True), '\n'
     head = html.find('head')
     body = html.find('body')
-    if head is not None and layout_tags_xpath(head): return True
-    elif body is None: return False
-    elif layout_tags_xpath(body): return True
+    if body is None: body = SubElement(html, 'body')
+    if head is None:
+        head = html.makeelement('head')
+        html.insert(-1, head)
+    head_list = layout_tags_xpath(head)
+    body[0:0] = head_list
+    if head_list: pass
+    elif layout_tags_xpath(body): pass
     else:
         for p in body.findall('p'):
-            if layout_tags_xpath(p): return True
-    return False
-
-def normalize(html):
-    if not normalization_is_needed(html): return
-    head = html.find('head')
-    body = html.find('body')
-    body_list = []
-    if head is not None: body_list.extend(layout_tags_xpath(head))
-    if body is None: body = SubElement(html, 'body')
-    else:
-        text = body.text
-        if text and (not text.isspace() or u'\xa0' in text):
-            body_list.append(text)
-            body.text = None
-        body_list.extend(unnest(body))
-    body[:] = normalize_elements(html, body_list)
-
-def unnest(elements):
-    result = []
-    for x in elements:
-        result.append(x)
-        tag, tail = x.tag, x.tail
-        if tag == 'layout':
-            result.extend(unnest(x))
-            x[:] = []
-        elif tag == 'p' or tag in layout_tags:
-            for i, y in enumerate(x):
-                ytag = y.tag
-                if y.tag in layout_tags:
-                    if tag == 'content' and ytag == 'column': continue
-                    result.extend(unnest(x[i:]))
-                    x[i:] = []
-                    break
-        if tail and not tail.isspace():
-            result.append(x.tail)
-            x.tail = None
-    return result
-
-def normalize_elements(document, elements):
-    result = []
-    content = column = None
-    explicit_columns = False
-    for x in elements:
-        tag = getattr(x, 'tag', None)
-        if tag == 'column':
-            if content is None or not explicit_columns:
-                if content is not None: normalize_content(content)
-                content = document.makeelement('content')
-                result.append(content)
-                explicit_columns = True
-            column = None
-            content.append(x)
+            if layout_tags_xpath(p): break
+        else: return
+    i = 0
+    while i < len(body):
+        x = body[i]
+        tag = x.tag
+        if tag == 'p': unnest_p(x)
         elif tag in layout_tags:
-            if content is not None: normalize_content(content)
-            if tag == 'content': normalize_content(x)
-            elif tag in ('layout', 'sidebar'): normalize_width(x)
-            content = column = None
-            result.append(x)
+            if tag == 'layout': unnest_layout(x)
+            elif tag == 'content': unnest_content(x)
+            else: unnest_element(x)
+        i += 1
+    # print etree.tostring(html, pretty_print=True), '\n'
+    elements = []
+    content = None
+    text = body.text
+    if text and not text.isspace():
+        content = body.makeelement('content')
+        content.text = text
+        body.text = None
+        elements.append(content)
+    for x in body:
+        tag = x.tag
+        if tag in layout_tags and tag != 'column':
+            if tag == 'content':
+                if content is not None: normalize_content(content)
+                content = None
+            elements.append(x)
+            tail = x.tail
+            layout_tags[tag](x)
+            if tail and not tail.isspace():
+                content = body.makeelement('content')
+                content.text = tail
+                x.tail = None
+                elements.append(content)
         else:
-            if column is None or explicit_columns:
-                if content is None or explicit_columns:
-                    if content is not None: normalize_content(content)
-                    content = document.makeelement('content')
-                    result.append(content)
-                    explicit_columns = False
-                column = SubElement(content, 'column')
-            if tag is not None: column.append(x)
-            elif not len(column): column.text = (column.text or '') + x
-            else:
-                 last_child = column[-1]
-                 last_child.tail = (last_child.tail or '') + x
+            if content is None:
+                content = body.makeelement('content')
+                elements.append(content)
+            content.append(x)
     if content is not None: normalize_content(content)
-    return result
+    body[:] = elements
+    # print etree.tostring(html, pretty_print=True), '\n'
+
+def normalize_layout(layout):
+    normalize_width(layout)
+
+def normalize_header(header):
+    pass
+
+def normalize_footer(footer):
+    pass
+
+def normalize_sidebar(sidebar):
+    normalize_width(sidebar)
 
 def normalize_content(content):
-    list = []; append = list.append
+    elements = []
     column = None
     text = content.text
     if text and not text.isspace():
         column = content.makeelement('column')
-        append(column)
         column.text = text
         content.text = None
+        elements.append(column)
     for x in content:
         if x.tag == 'column':
             if column is not None: normalize_column(column)
-            normalize_column(x)
             column = None
-            append(x)
+            elements.append(x)
             tail = x.tail
+            normalize_column(x)
             if tail and not tail.isspace():
                 column = content.makeelement('column')
-                append(column)
                 column.text = tail
                 x.tail = None
+                elements.append(column)
         else:
             if column is None:
                 column = content.makeelement('column')
-                append(column)
+                elements.append(column)
             column.append(x)
     if column is not None: normalize_column(column)
-    content[:] = list
+    content[:] = elements
 
 def normalize_column(column):
-    list = []; append = list.append
+    elements = []
     p = None
     text = column.text
     if text and not text.isspace():
         p = column.makeelement('p')
-        append(p)
         p.text = text
         column.text = None
+        elements.append(p)
     for x in column:
         if x.tag in block_level_tags:
             p = None
-            append(x)
+            elements.append(x)
             tail = x.tail
             if tail and not tail.isspace():
                 p = column.makeelement('p')
-                append(p)
                 p.text = tail
                 x.tail = None
+                elements.append(p)
         else:
             if p is None:
                 p = column.makeelement('p')
-                append(p)
+                elements.append(p)
             p.append(x)
-    column[:] = list
+    column[:] = elements
 
 def normalize_width(element):
     width = element.get('width')
@@ -164,11 +144,73 @@ def normalize_width(element):
     try: number = int(width[:-2])
     except ValueError: return
     element.set('width', width[:-2])
-    
+
+layout_tags = dict(
+    layout=normalize_layout,
+    header=normalize_header,
+    footer=normalize_footer,
+    sidebar=normalize_sidebar,
+    content=normalize_content,
+    column=normalize_column,
+    )
+layout_tags_xpath = etree.XPath('|'.join(layout_tags))
+
+block_level_tags = set('''
+    address blockquote center dir div dl fieldset form h1 h2 h3 h4 h5 h6 hr
+    isindex menu noframes noscript ol p pre table ul'''.split())
+
+def _unnest(x):
+    parent = x.getparent()
+    tail = parent.tail
+    if tail and not tail.isspace():
+        last = parent[-1]
+        last.tail = (last.tail or '') + tail
+        parent.tail = None
+    parent2 = parent.getparent()
+    i = parent.index(x)
+    j = parent2.index(parent)
+    parent2[j+1:j+1] = parent[i:]
+
+def unnest_p(p):
+    i = 0
+    while i < len(p):
+        x = p[i]
+        if x.tag in layout_tags: _unnest(x)
+        i += 1
+
+def unnest_layout(layout):
+    if len(layout): _unnest(layout[0])
+    text = layout.text
+    if text and not text.isspace():
+        layout.text = None
+        layout.tail = text
+
+def unnest_content(content):
+    i = 0
+    while i < len(content):
+        x = content[i]
+        tag = x.tag
+        if tag == 'p': unnest_p(x)
+        elif tag == 'column': unnest_element(x)
+        elif tag in layout_tags: _unnest(x)
+        i += 1
+
+def unnest_element(element):
+    i = 0
+    while i < len(element):
+        x = element[i]
+        tag = x.tag
+        if tag == 'p': unnest_p(x)
+        elif tag in layout_tags: _unnest(x)
+        i += 1
+
 xslt_filename = os.path.join(os.path.dirname(__file__), 'transform.xslt')
 xslt = etree.XSLT(etree.parse(xslt_filename))
 
 def transform(xml):
+##    f = file('test.xml', 'w')
+##    xml.getroottree().write(f)
+##    f.close()
     return xslt(xml)
 
 xml2html_template = """
