ee4e72e681e8c0e36567ab7b922337ee597be509
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-08-29 14:03:33 +0000

    Changes in pony.orm
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@290 698dff77-f410-0410-8918-cf622e150f36

--- pony/orm.py
+++ pony/orm.py
@@ -5,148 +5,139 @@ from itertools import count, izip
 from pony import utils
 from pony.thirdparty import etree
 
-class DiagramError(Exception): pass
-class MappingError(Exception): pass
-class TransactionError(Exception): pass
+class OrmError(Exception): pass
 
-def _error_method(self, *args, **keyargs):
-    raise TypeError
+class DiagramError(OrmError): pass
+class SchemaError(OrmError): pass
+class MappingError(OrmError): pass
+class TransactionError(OrmError): pass
 
-class Local(threading.local):
-    def __init__(self):
-        self.transaction = None
+class Mapping(object):
+    _cache = {}
+    def __new__(cls, filename):
+        mapping = cls._cache.get(filename)
+        if mapping is not None: return mapping
+        mapping = object.__new__(cls)
+        mapping._init_(filename)
+        return cls._cache.setdefault(filename, mapping)
+    def _init_(self, filename):
+        self.filename = filename
+        self.tables = {}   # table_name -> TableMapping
+        if not os.path.exists(filename):
+            raise MappingError('File not found: %s' % filename)
+        document = etree.parse(filename)
+        for telement in document.findall('table'):
+            table = TableMapping(telement)
+            if self.tables.setdefault(table.name, table) is not table:
+                raise MappingError('Duplicate table definition: %s'%table.name)
 
-local = Local()
+class TableMapping(object):
+    def __init__(self, element):
+        self.name = element.get('name')
+        if not self.name:
+            raise MappingError("Table element without 'name' attribute")
+        self.entities = element.get('entity', '').split()
+        self.relations = element.get('relation', '').split()
+        if self.entities and self.relations: raise MappingError(
+            'For table %r both entity name and relations are specified. '
+            'It is not allowed' % table.name)
+        self.columns = []
+        self.cdict = {}
+        for celement in element.findall('column'):
+            col = ColumnMapping(self.name, celement)
+            if self.cdict.setdefault(col.name, col) is not col:
+                raise MappingError('Duplicate column definition: %s.%s'
+                                   % (self.name, col.name))
+            self.columns.append(col)
 
-class Transaction(object):
-    def __init__(self, data_source):
-        if local.transaction is not None: raise TransactionError(
-            'Transaction already started in thread %d' % thread.get_ident())
-        self.data_source = data_source
-        self.diagrams = set()
-        self.cache = {} # Table -> TableCache
-        local.transaction = self
-    def _close(self):
-        assert local.transaction is self
-        while self.diagrams:
-            diagram = self.diagrams.pop()
-            # diagram.lock.acquire()
-            # try:
-            diagram.transactions.remove(self)
-            # finally: diagram.lock.release()
-        local.transaction = None
-    def commit(self):
-        raise NotImplementedError
-    def rollback(self):
-        raise NotImplementedError
-
-def get_transaction():
-    return local.transaction
-
-def begin():
-    if local.transaction is not None: raise TransactionError(
-        'Transaction already started in thread %d' % thread.get_ident())
-    outer_dict = sys._getframe(1).f_locals
-    data_source = outer_dict.get('_data_source_')
-    if data_source is None: raise TransactionError(
-        'Can not start transaction, because default data source is not set')
-    return Transaction(data_source)
-
-def commit():
-    tr = local.transaction
-    if tr is None: raise TransactionError(
-        'Transaction not started in thread %d' % thread.get_ident())
-    tr.commit()
-
-def rollback():
-    tr = local.transaction
-    if tr is None: raise TransactionError(
-        'Transaction not started in thread %d' % thread.get_ident())
-    tr.rollback()
+class ColumnMapping(object):
+    def __init__(self, table_name, element):
+        self.name = element.get('name')
+        if not self.name: raise MappingError(
+            'Error in table definition %r: '
+            'Column element without "name" attribute' % table_name)
+        self.domain = element.get('domain')
+        self.attrs = [ attr.split('.')
+                       for attr in element.get('attr', '').split() ]
+        self.kind = element.get('kind')
+        if self.kind not in (None, 'discriminator'): raise MappingError(
+            'Error in column definition %s.%s: invalid column kind: %s'
+            % (table_name, self.name, self.kind))
+        cases = element.findall('case')
+        if cases and self.kind != 'discriminator': raise MappingError(
+            'Non-discriminator column %s.%s contains cases.It is not allowed'
+            % (table_name, self.name))
+        self.cases = [ (case.get('value'), case.get('entity'))
+                       for case in cases ]
+        for value, entity in self.cases:
+            if not value or not entity: raise MappingError(
+                'Invalid discriminator case in column %s.%s'
+                % (table_name, self.name))
 
 class DataSource(object):
-    _lock = threading.Lock() # threadsafe access to cache of datasources
     _cache = {}
+    _cache_lock = threading.Lock() # threadsafe access to cache of datasources
     def __new__(cls, provider, *args, **keyargs):
         self = object.__new__(cls)
         self._init_(provider, *args, **keyargs)
         key = (self.provider, self.mapping, self.args,
                tuple(sorted(self.keyargs.items())))
-        return cls._cache.setdefault(key, self) # is it thread safe?
-               # I think - yes, if args & keyargs only contains
-               # types with C-written __eq__ and __hash__
+        _cache_lock.acquire()
+        try: return cls._cache.setdefault(key, self)
+        finally: _cache_lock.release()
     def _init_(self, provider, *args, **keyargs):
+        self.lock = threading.RLock() # threadsafe access to datasource schema
         self.provider = provider
         self.args = args
         self.keyargs = keyargs
         self.mapping = keyargs.pop('mapping', None)
+
+        self.transactions = set()        
+        self.tables = {}    # table_name -> TableInfo
+        self.entities = {}  # Entity -> EntityInfo
+    def clear_schema(self):
+        self.lock.acquire()
+        try:
+            if self.transaction: raise SchemaError(
+                'Cannot clear datasource schema information '
+                'because it is used by active transaction')
+            self.entities.clear()
+            self.tables.clear()
+        finally: self.lock.release()
     def get_connection(self):
         provider = self.provider
         if isinstance(provider, basestring):
             provider = utils.import_module('pony.dbproviders.' + provider)
         return provider.connect(*self.args, **self.keyargs)
 
-next_id = count().next
+class TableInfo(object):
+    def __init__(self, data_source, name):
+        self.data_source = data_source
+        self.name = name
+        self.entities = []
+        self.columns = []
 
-class Attribute(object):
-    def __init__(self, py_type, *args, **keyargs):
-        self._id_ = next_id()
-        self.py_type = py_type
-        self.name = None
-        self.entity = None
-        self.args = args
-        self.options = keyargs
-        self.reverse = keyargs.pop('reverse', None)
-        if self.reverse is None: pass
-        elif not isinstance(self.reverse, (basestring, Attribute)):
-            raise TypeError("Value of 'reverse' option must be name of "
-                            "reverse attribute). Got: %r" % self.reverse)
-        elif not (self.py_type, basestring):
-            raise DiagramError('Reverse option cannot be set for this type %r'
-                            % self.py_type)
-    def __str__(self):
-        owner_name = self.entity is None and '?' or self.entity.__name__
-        return '%s.%s' % (owner_name, self.name or '?')
-    def __repr__(self):
-        return '<%s: %s>' % (self, self.__class__.__name__)
+class ColumnInfo(object):
+    def __init__(self, table, name):
+        self.table = table
+        self.name = name
+        self.attrs = []
+        table.columns.append(self)
 
-class Optional(Attribute):
-    pass
-
-class Required(Attribute):
-    pass
-
-class Unique(Required):
-    def __new__(cls, *args, **keyargs):
-        if not args: raise TypeError('Invalid count of positional arguments')
-        attrs = tuple(a for a in args if isinstance(a, Attribute))
-        non_attrs = [ a for a in args if not isinstance(a, Attribute) ]
-        if attrs and non_attrs: raise TypeError('Invalid arguments')
-        cls_dict = sys._getframe(1).f_locals
-        keys = cls_dict.setdefault('_keys_', set())
-        if issubclass(cls, PrimaryKey): tuple_class = _PrimaryKeyTuple
-        else: tuple_class = tuple
-        if not attrs:
-            result = Required.__new__(cls, *args, **keyargs)
-            keys.add(tuple_class((result,)))
-            return result
-        else: keys.add(tuple_class(attrs))
-
-class PrimaryKey(Unique):
-    pass
-
-class _PrimaryKeyTuple(tuple):
-    pass
-
-class Collection(Attribute):
-    pass
-
-class Set(Collection):
-    pass
-
-##class List(Collection): pass
-##class Dict(Collection): pass
-##class Relation(Collection): pass
+class Diagram(object):
+    def __init__(self):
+        self.lock = threading.RLock()
+        self.entities = {} # entity_name -> Entity
+        self.transactions = set()
+    def clear(self):
+        self.lock.acquire()
+        try: self._clear()
+        finally: self.lock.release()
+    def _clear(self):
+        self.lock.acquire()
+        try:
+            for tr in self.transactions: tr.data_source.clear_schema()
+        finally: self.lock.release()
 
 class EntityMeta(type):
     def __init__(cls, name, bases, dict):
@@ -166,6 +157,8 @@ class Entity(object):
     __metaclass__ = EntityMeta
     @classmethod
     def _cls_init_(cls, diagram):
+        if cls.__name__ in diagram.entities:
+            raise DiagramError('Entity %s already exists' % cls.__name__)
         bases = [ c for c in cls.__bases__
                     if issubclass(c, Entity) and c is not Entity ]
         cls._bases_ = bases
@@ -303,112 +296,125 @@ class Entity(object):
     @classmethod
     def _cls_get_info_(cls):
         return diagram.get_entity_info(self)
-        
-class Diagram(object):
-    def __init__(self):
-        self.lock = threading.RLock()
-        self.entities = {} # entity_name -> entity
-        self.schemata = {} # mapping -> schema
-        self.transactions = set()
-    def clear(self):
-        self.lock.acquire()
-        try: self._clear()
-        finally: self.lock.release()
-    def _clear(self):
-        if self.transactions: raise DiagramError(
-            'Cannot change entity diagram '
-            'because it is used by active transaction')
-        self.schemata.clear()
-    def get_schema(self):
-        transaction = local.transaction
-        if transaction is None: raise TransactionError(
-            'There are no active transaction in this thread: %d'
-            % thread.get_ident())
-        mapping = transaction.data_source.mapping
-        self.lock.acquire()
+
+next_id = count().next
+
+class Attribute(object):
+    def __init__(self, py_type, *args, **keyargs):
+        self._id_ = next_id()
+        self.py_type = py_type
+        self.name = None
+        self.entity = None
+        self.args = args
+        self.options = keyargs
+        self.reverse = keyargs.pop('reverse', None)
+        if self.reverse is None: pass
+        elif not isinstance(self.reverse, (basestring, Attribute)):
+            raise TypeError("Value of 'reverse' option must be name of "
+                            "reverse attribute). Got: %r" % self.reverse)
+        elif not (self.py_type, basestring):
+            raise DiagramError('Reverse option cannot be set for this type %r'
+                            % self.py_type)
+    def __str__(self):
+        owner_name = self.entity is None and '?' or self.entity.__name__
+        return '%s.%s' % (owner_name, self.name or '?')
+    def __repr__(self):
+        return '<%s: %s>' % (self, self.__class__.__name__)
+
+class Optional(Attribute):
+    pass
+
+class Required(Attribute):
+    pass
+
+class Unique(Required):
+    def __new__(cls, *args, **keyargs):
+        if not args: raise TypeError('Invalid count of positional arguments')
+        attrs = tuple(a for a in args if isinstance(a, Attribute))
+        non_attrs = [ a for a in args if not isinstance(a, Attribute) ]
+        if attrs and non_attrs: raise TypeError('Invalid arguments')
+        cls_dict = sys._getframe(1).f_locals
+        keys = cls_dict.setdefault('_keys_', set())
+        if issubclass(cls, PrimaryKey): tuple_class = _PrimaryKeyTuple
+        else: tuple_class = tuple
+        if not attrs:
+            result = Required.__new__(cls, *args, **keyargs)
+            keys.add(tuple_class((result,)))
+            return result
+        else: keys.add(tuple_class(attrs))
+
+class PrimaryKey(Unique):
+    pass
+
+class _PrimaryKeyTuple(tuple):
+    pass
+
+class Collection(Attribute):
+    pass
+
+class Set(Collection):
+    pass
+
+##class List(Collection): pass
+##class Dict(Collection): pass
+##class Relation(Collection): pass
+
+class Transaction(object):
+    def __init__(self, data_source, connection=None):
+        if local.transaction is not None: raise TransactionError(
+            'Transaction already started in thread %d' % thread.get_ident())
+        self.data_source = data_source
+        self.connection = connection
+        self.diagrams = set()
+        self.cache = {} # TableInfo -> TableCache
+
+        data_source.lock.acquire()
+        try: data_source.transactions.add(self)
+        finally: data_source.lock.release()
+        local.transaction = self
+    def _close(self):
+        assert local.transaction is self
+        data_source.lock.acquire()
         try:
-            return (self.schemata.get(mapping)
-                    or self.schemata.setdefault(mapping, Schema(self, mapping)))
-        finally:
-            self.lock.release()
+            while self.diagrams:
+                diagram = self.diagrams.pop()
+                diagram.transactions.remove(self)
+            data_source.transactions.remove(self)
+        finally: data_source.lock.release()
+        local.transaction = None
+    def commit(self):
+        self._close()
+        raise NotImplementedError
+    def rollback(self):
+        self._close()
+        raise NotImplementedError
 
-class Schema(object):
-    def __init__(self, diagram, mapping):
-        self.mapping = mapping
-        self.entity_to_tables = {}
-        self.attr_to_columns = {}
+class Local(threading.local):
+    def __init__(self):
+        self.transaction = None
 
-class Table(object):
-    def __init__(self, name):
-        self.name = name
-        self.entities = []
-        self.columns = []
+local = Local()
 
-class Column(object):
-    def __init__(self, table, name):
-        self.name = name
-        self.attrs = []
-        self.table = table
-        table.columns.append(self)
+def get_transaction():
+    return local.transaction
 
-class Mapping(object):
-    _cache = {}
-    def __new__(cls, filename):
-        mapping = cls._cache.get(filename)
-        if mapping is not None: return mapping
-        mapping = object.__new__(cls)
-        mapping._init_(filename)
-        return cls._cache.setdefault(filename, mapping)
-    def _init_(self, filename):
-        self.filename = filename
-        self.tables = {}   # table_name -> TableMapping
-        if not os.path.exists(filename):
-            raise MappingError('File not found: %s' % filename)
-        document = etree.parse(filename)
-        for telement in document.findall('table'):
-            table = TableMapping(telement)
-            if self.tables.setdefault(table.name, table) is not table:
-                raise MappingError('Duplicate table definition: %s'%table.name)
+def begin():
+    if local.transaction is not None: raise TransactionError(
+        'Transaction already started in thread %d' % thread.get_ident())
+    outer_dict = sys._getframe(1).f_locals
+    data_source = outer_dict.get('_data_source_')
+    if data_source is None: raise TransactionError(
+        'Can not start transaction, because default data source is not set')
+    return Transaction(data_source)
 
-class TableMapping(object):
-    def __init__(self, element):
-        self.name = element.get('name')
-        if not self.name:
-            raise MappingError("Table element without 'name' attribute")
-        self.entities = element.get('entity', '').split()
-        self.relations = element.get('relation', '').split()
-        if self.entities and self.relations: raise MappingError(
-            'For table %r both entity name and relations are specified. '
-            'It is not allowed' % table.name)
-        self.columns = []
-        self.cdict = {}
-        for celement in element.findall('column'):
-            col = ColumnMapping(self.name, celement)
-            if self.cdict.setdefault(col.name, col) is not col:
-                raise MappingError('Duplicate column definition: %s.%s'
-                                   % (self.name, col.name))
-            self.columns.append(col)
+def commit():
+    tr = local.transaction
+    if tr is None: raise TransactionError(
+        'Transaction not started in thread %d' % thread.get_ident())
+    tr.commit()
 
-class ColumnMapping(object):
-    def __init__(self, table_name, element):
-        self.name = element.get('name')
-        if not self.name: raise MappingError(
-            'Error in table definition %r: '
-            'Column element without "name" attribute' % table_name)
-        self.domain = element.get('domain')
-        self.attrs = [ attr.split('.')
-                       for attr in element.get('attr', '').split() ]
-        self.kind = element.get('kind')
-        if self.kind not in (None, 'discriminator'): raise MappingError(
-            'Error in column definition %s.%s: invalid column kind: %s'
-            % (table_name, self.name, self.kind))
-        cases = element.findall('case')
-        if cases and self.kind != 'discriminator': raise MappingError(
-            'Non-discriminator column %s.%s contains cases.It is not allowed'
-            % (table_name, self.name))
-        self.cases = [ (case.get('value'), case.get('entity'))
-                       for case in cases ]
-        for value, entity in self.cases:
-            if not value or not entity: raise MappingError(
-                'Invalid discriminator case in column %s.%s'
-                % (table_name, self.name))
+def rollback():
+    tr = local.transaction
+    if tr is None: raise TransactionError(
+        'Transaction not started in thread %d' % thread.get_ident())
+    tr.rollback()
