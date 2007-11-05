2f0968712780963fb492ce0d9922abe7551f23e7
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2007-11-05 18:59:47 +0000

    Fix threading bug in pony.xslt_: "RuntimeError: stylesheet is not usable in this thread"
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@335 698dff77-f410-0410-8918-cf622e150f36

--- pony/xslt_.py
+++ pony/xslt_.py
@@ -1,3 +1,5 @@
+import threading
+
 from lxml import etree
 from pony import layout
 
@@ -28,29 +30,42 @@ xml2html_template = """
 """
 
 doctypes = dict(
-    strict=['-//W3C//DTD HTML 4.01//EN',
-            'http://www.w3.org/TR/html4/strict.dtd'],
-    transitional=['-//W3C//DTD HTML 4.01 Transitional//EN',
-                  'http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd'],
-    frameset=['-//W3C//DTD HTML 4.01 Frameset//EN',
-              'http://www.w3.org/TR/1999/REC-html401-19991224/frameset.dtd'],
+    strict=['-//W3C//DTD HTML 4.01//EN', 'http://www.w3.org/TR/html4/strict.dtd'],
+    transitional=['-//W3C//DTD HTML 4.01 Transitional//EN', 'http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd'],
+    frameset=['-//W3C//DTD HTML 4.01 Frameset//EN', 'http://www.w3.org/TR/1999/REC-html401-19991224/frameset.dtd'],
     )
 doctypes['loose'] = doctypes['transitional']
 
-xml2html_cache = {}
+class Local(threading.local):
+    def __init__(self):
+        self.xml2html_cache = {}
+
+local = Local()
+
+def create_xml2html_transformer(encoding, doctype, indent):
+    if len(doctype) == 2: pyblic, system = doctype
+    else: public, system = doctypes.get(doctype, ['', ''])
+    if public: public = 'doctype-public="%s"' % public
+    if system: system = 'doctype-system="%s"' % system
+    text = xml2html_template % (encoding, indent and 'yes' or 'no', ' '.join((public, system)))
+    xml = etree.XML(text)
+    xslt = etree.XSLT(xml)
+    return xslt
+
+std_encoding = 'UTF-8'
+std_doctype = 'transitional'
+std_indent = True
+
+main_xslt = create_xml2html_transformer(std_encoding, std_doctype, std_indent)
 
 def xml2html(xml, encoding='UTF-8', doctype='transitional', indent=True):
     encoding = encoding.upper().replace('_', '-')
-    xslt = xml2html_cache.get((encoding, doctype, indent))
-    if xslt is None:
-        if len(doctype) == 2: pyblic, system = doctype
-        else: public, system = doctypes.get(doctype, ['', ''])
-        if public: public = 'doctype-public="%s"' % public
-        if system: system = 'doctype-system="%s"' % system
-        text = xml2html_template % (
-            encoding, indent and 'yes' or 'no', ' '.join((public, system)))
-        xslt = etree.XSLT(etree.XML(text))
-        xml2html_cache[(encoding, doctype, indent)] = xslt
+    if encoding != std_encoding or doctype != std_doctype or indent != std_indent:
+        xslt = local.xml2html_cache.get((encoding, doctype, indent))
+        if xslt is None:
+            xslt = create_xml2html_transformer(encoding, doctype, indent)
+            local.xml2html_cache[(encoding, doctype, indent)] = xslt
+    else: xslt = main_xslt
     result = xslt(xml)
     return (str(result).replace('<!--start of IE hack-->', '<!--[if IE]>')
                        .replace('\n<!--end of IE hack-->', '<![endif]-->\n')
