7c2ddaf259bae67d8a3fd3c91642948478dfd317
Alexey Malashkevich <m.alexey@gmail.com>
2007-11-21 22:28:04 +0000

    webgui update
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@358 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/web/webgui.py
+++ pony/examples/web/webgui.py
@@ -6,26 +6,14 @@ from pony.logging import search_log
 
 import pprint, threading, time
 from datetime import timedelta
-from pony.templating import template
 
 use_autoreload()
 
-MAX_RECORD_DISPLAY_COUNT = 1000
+MAX_ROW_COUNT = 30;
 
-@http('/getLastRecords')
-def feed():
-    data = load()
-    records_count = len(data)
-    return template("""
-    {"recordsReturned":$records_count,"records":[
-        $for(r in data) {          
-            {"id":"$(r['id'])", "ts":"$(r['timestamp'][:-7])", "type": "$(r['text'])"} }
-        $separator{,}
-    ]}
-    """)
 
 @http('/getLastRecordsHtml', type='text/html')
-def feed2():
+def feed1():
     data = load()
     records_count = len(data)            
     return template("""
@@ -37,7 +25,7 @@ def feed2():
     <h2>pony web admin</h2>
     <table border="0">
         <tr><th>id</th><th>timestamp</th><th>type</th></tr>
-        $for(r in data) { 
+        $for(r in data) {
         
         <tr class="row">
             <td class="pad"><a href="details?id=$(r['id'])">$(r['id'])</a></td>
@@ -47,77 +35,6 @@ def feed2():
         }
     </table></body></html>
     """)
-
-
-def load(since_last_start=True):
-    start_id = 0
-    if since_last_start:
-        start = search_log(1, None, "type='HTTP:start'")
-        if start: start_id = start[0]['id']
-    data = search_log(MAX_RECORD_DISPLAY_COUNT, None,
-        "type like 'HTTP:%' and type not in ('HTTP:response', 'HTTP:stop') and id >= ?",
-        [ start_id ])
-
-    data.reverse()
-    return data
-
-def load_fragment(frm=None, to=None):
-    print frm
-    print to
-    params = "type like 'HTTP:%' and type not in ('HTTP:response', 'HTTP:stop')"
-    if frm is not None:
-        data = search_log(-MAX_RECORD_DISPLAY_COUNT, frm, params)
-    if to is not None:
-        data = search_log(MAX_RECORD_DISPLAY_COUNT, to, params)
-        data.reverse()
-    return data
-
-def get_details(rec_id):
-    rec_id = int(rec_id)
-
-    records = search_log(-1, None, "id = ?",[ rec_id ])
-    req = records[0]
-    rtype = req['type']
-    if rtype == 'HTTP:start' or rtype == 'HTTP:stop':
-        return ("No details",[],[],"","")
-
-    req_headers = req['headers'].items()    
-    process_id = req['process_id']
-    thread_id = req['thread_id']
-
-    records = search_log(-1, rec_id,
-        "type like 'HTTP:%' and process_id = ? and thread_id = ?",
-        [ process_id, thread_id ])
-
-    if records and records[0]['type'] == 'HTTP:response':
-        resp = records[0]
-
-        rec_id = req['id']
-        
-        dt1 = utils.timestamp2datetime(req['timestamp'])
-        dt2 = utils.timestamp2datetime(resp['timestamp'])
-        delta = dt2 - dt1
-        delta=delta.seconds + 0.000001 * delta.microseconds
-        status_text = ("%s; DELAY: %s; "
-                "PROCESS_ID: %d; THREAD_ID: %d; RECORD_ID: %d" %
-               (resp['text'], delta, process_id, thread_id, rec_id))        
-        if req["user"] is not None: text += "; USER: %s" % req["user"]
-        exceptions = search_log(-10, rec_id,
-                                "type = 'exception' and id < ? "
-                                "and process_id = ? and thread_id = ?",
-                                [ resp['id'], process_id, thread_id ])
-        if exceptions: status_text += "; EXCEPTION: " + exceptions[0]['text']
-
-        resp_headers = resp['headers']
-
-        exc_text_list = []
-        for exc in exceptions:
-            exc_text_list.append(exc['traceback'])
-        exc_text = ('\n'.join(exc_text_list)).replace('\n', '<br/>')
-        
-        session_text = pprint.pformat(req['session'])
-        return (status_text, req_headers, resp_headers, exc_text, session_text)
-    else: return ("Error happened",[],[],"","")
 
 @http('/details?id=$rec_id', type='text/html')
 def show_details(rec_id=None):
@@ -168,51 +85,111 @@ def show_details(rec_id=None):
     </body></html>
         """)
 
+def load(since_last_start=True):
+    start_id = 0
+    if since_last_start:
+        start = search_log(1, None, "type='HTTP:start'")
+        if start: start_id = start[0]['id']
+    data = search_log(MAX_ROW_COUNT + 1, None,
+        "type like 'HTTP:%' and type not in ('HTTP:response', 'HTTP:stop') and id >= ?",
+        [ start_id ])
+    data.reverse()
+    return data
+
+def load_fragment(frm=None, to=None):
+    print frm
+    print to
+    params = "type like 'HTTP:%' and type not in ('HTTP:response', 'HTTP:stop')"
+    if frm is not None:
+        data = search_log(-(MAX_ROW_COUNT + 1), frm, params)
+    if to is not None:
+        data = search_log(MAX_ROW_COUNT + 1, to, params)
+        data.reverse()
+    return data
+
+def get_details(rec_id):
+    rec_id = int(rec_id)
+
+    records = search_log(-1, None, "id = ?",[ rec_id ])
+    req = records[0]
+    rtype = req['type']
+    if rtype == 'HTTP:start' or rtype == 'HTTP:stop':
+        return ("No details",[],[],"","")
+
+    req_headers = req['headers'].items()    
+    process_id = req['process_id']
+    thread_id = req['thread_id']
+
+    records = search_log(-1, rec_id,
+        "type like 'HTTP:%' and process_id = ? and thread_id = ?",
+        [ process_id, thread_id ])
+
+    if records and records[0]['type'] == 'HTTP:response':
+        resp = records[0]
+
+        rec_id = req['id']
+        
+        dt1 = utils.timestamp2datetime(req['timestamp'])
+        dt2 = utils.timestamp2datetime(resp['timestamp'])
+        delta = dt2 - dt1
+        delta=delta.seconds + 0.000001 * delta.microseconds
+        status_text = ("%s; DELAY: %s; "
+                "PROCESS_ID: %d; THREAD_ID: %d; RECORD_ID: %d" %
+               (resp['text'], delta, process_id, thread_id, rec_id))        
+        if req["user"] is not None: text += "; USER: %s" % req["user"]
+        exceptions = search_log(-10, rec_id,
+                                "type = 'exception' and id < ? "
+                                "and process_id = ? and thread_id = ?",
+                                [ resp['id'], process_id, thread_id ])
+        if exceptions: status_text += "; EXCEPTION: " + exceptions[0]['text']
+
+        resp_headers = resp['headers']
+
+        exc_text_list = []
+        for exc in exceptions:
+            exc_text_list.append(exc['traceback'])
+        exc_text = ('\n'.join(exc_text_list)).replace('\n', '<br/>')
+        
+        session_text = pprint.pformat(req['session'])
+        return (status_text, req_headers, resp_headers, exc_text, session_text)
+    else: return ("Error happened",[],[],"","")
+
 
 @http('/details2?id=$rec_id', type='text/html')
 def show_details2(rec_id=None):
     (status_text, req_headers, resp_headers, exc_text, session_text)=get_details(rec_id)
     return template("""
-    <table>
+    <table id='nested'>
+     <tbody>
+      <tr><td colspan='2'><div class="delimeter">status:</div></td></tr>
+      <tr><td colspan='2'> $status_text </td></tr>
 
-      <tr class="delimeter"><td>status:</td></tr>
-      <tr><td class="bordered"> $status_text </td></tr>
-
-      <tr class="delimeter"><td>request:</td></tr> 
-      <tr><td class="bordered"> 
-        <table class="t2">
-          <tr><th class="bordered">HTTP request header</th><th class="bordered">value</th></tr>
+      <tr><td colspan='2'><div class="delimeter">request:</div></td></tr> 
+        
+          <tr><td>HTTP request header</td><td>value</td></tr>
           $for(k, v in sorted(req_headers)) { <tr>
-              <td class="bordered">$k</td><td class="bordered">$v</td>
+              <td>$k</td><td>$v</td>
           </tr>
           }
-        </table>
-      </td></tr>
 
-      <tr class="delimeter"><td>response:</td></tr>
-      <tr><td class="bordered">
-      <table class="t2">
-        <tr><th class="bordered">HTTP response header</th><th class="bordered">value</th></tr>
+      <tr><td colspan='2'><div class="delimeter">response:</div></td></tr>
+      
+        <tr><td>HTTP response header</td><td>value</td></tr>
         $for(k, v in sorted(resp_headers)) { <tr>
-            <td class="bordered">$k</td><td class="bordered">$v</td>
+            <td>$k</td><td>$v</td>
         </tr>
         }
-      </table>
+
+      <tr><td colspan='2'><div class="delimeter">exceptions:</div></td></tr>  
+      <tr><td colspan='2'> $exc_text
       </td></tr>
 
-      <tr class="delimeter"><td>exceptions:</td></tr>  
-      <tr><td class="bordered"> $exc_text
-      </td></tr>
-
-      <tr class="delimeter"><td>session</td></tr>  
-      <tr><td class="bordered"> $session_text </td></tr>
-    
-    </table>
-    
+      <tr><td colspan='2'><div class="delimeter">session</div></td></tr>  
+      <tr><td colspan='2'> $session_text </td></tr>
+     </tbody>
+    </table>    
         """)
-
-
-
+            
 @http('/getUpdates?last_id=$last_id', type='text/html')
 def get_updates(last_id=None):
     data = search_log(-1000, last_id, "type like 'HTTP:%' and type <> 'HTTP:response'")    
@@ -227,11 +204,25 @@ def get_updates(last_id=None):
 
 @http('/getLastRecordsHtml2?frm=$frm&to=$to', type='text/html')
 def feed2(frm=None, to=None):
+    has_next="false"
+    has_prev="false"
     if frm or to:
         data = load_fragment(frm, to)
     else:
         data = load()
-    records_count = len(data)            
+    records_count = len(data)
+    if to is not None:
+        has_next="true"
+    if frm is not None:
+        has_prev="true"
+    if len(data) > MAX_ROW_COUNT:        
+        if to is not None:
+            del data[0]
+            has_prev="true"            
+        else:
+            del data[-1]
+            has_next="true"
+
     return template("""
     <html><head>
     <style type="text/css">
@@ -257,7 +248,9 @@ def feed2(frm=None, to=None):
  var keepUpdating = true;
  var lastRecId = 0;
  var recordsCount;
- var MAX_ROW_COUNT = 30;
+ var MAX_ROW_COUNT = $MAX_ROW_COUNT;
+ var hasNext=$has_next;
+ var hasPrev=$has_prev;
 
  function coll() {
     el = document.getElementById(oldId);
@@ -266,7 +259,7 @@ def feed2(frm=None, to=None):
     currId=null;
  }
 
-var AjaxObject = {
+var GetDetails = {
 	handleSuccess:function(o){
 	  if (oldId != null) {
 	    el = document.getElementById(oldId);
@@ -275,12 +268,11 @@ var AjaxObject = {
 	  oldId = currId;  
 	  el = document.getElementById(currId);
 	  oldValue = el.innerHTML;
-	  document.getElementById(currId).innerHTML = "<td colspan='3' class='expanded'><div onclick='coll()'>"
-	  + oldValue + "<br><div class='scrl'>"
-	  + o.responseText;
+	  document.getElementById(currId).innerHTML = "<div onclick='coll()' class='expanded'>"
+	                                               + oldValue + o.responseText + "</div>"; 
 	},        
 	handleFailure:function(o){
-           alert("failure " + o);
+	  alert("failure " + o);
 	},        
 	startRequest:function(cid) {
 	   YAHOO.util.Connect.asyncRequest('GET', '/details2?id=' + cid, callback, null);
@@ -288,42 +280,42 @@ var AjaxObject = {
 
 };
 var callback = {
-	success:AjaxObject.handleSuccess,
-	failure:AjaxObject.handleFailure,
-	scope: AjaxObject
+	success:GetDetails.handleSuccess,
+	failure:GetDetails.handleFailure,
+	scope: GetDetails
 };
 
  function disp(id) {  
   if (currId == id) return;
   currId=id;
-  AjaxObject.startRequest(id);
+  GetDetails.startRequest(id);
  }
  
  function subscribe() {
-  var divs=document.getElementsByTagName('div');
+  var tbl=document.getElementById("main");
+  var divs=tbl.getElementsByTagName('div');
   for (var i=0; i<divs.length; i++) {
-    if (divs[i].className == 'row') {
       f = "disp('" + divs[i].getAttribute('id') + "');";
       divs[i].onclick= new Function(f);
-    }
   }
  }
  onload=function() {
    subscribe();
    update();
+   showNext(hasNext);
+   showPrev(hasPrev);
  }
 
  function addRow(col1, col2, col3) {
-   var tbl=document.getElementById("main-table");
+   var tbl=document.getElementById("main");
    var lastRow=tbl.rows.length;
    var row=tbl.insertRow(lastRow);
    var td=document.createElement("TD");
-   td.setAttribute("class", "main-row"); 
-   td.innerHTML="<div class='row' onclick='disp(" + col1 + ")' id='" 
-                 + col1 + "'> <div class='col11'>"
-                 + col1 + "</div> <div class='col12'>"
-                 + col2 + "</div> <div class='col13'>"
-                 + col3 + "</div> </div>" ;
+   td.setAttribute("class", "row"); 
+   td.innerHTML="<div onclick='disp(" + col1 + ")' id='" + col1 + "'>"
+                 + " <span class='col1'>" + col1 + "</span>" 
+                 + " <span class='col2'>" + col2 + "</span>"
+                 + " <span class='col3'>" + col3 + "</span> </div>" ;
    row.appendChild(td);                
    lastRecId=col1;
  }
@@ -331,14 +323,13 @@ var callback = {
 
 var TableUpdate = {
 	handleSuccess:function(o){
-           rows=eval(o.responseText);
-           for (var i=0; i< rows.length; i++) {
-             addRow(rows[i].col1, rows[i].col2, rows[i].col3);
-           }  
-
+	       rows=eval(o.responseText);
+	       for (var i=0; i< rows.length; i++) {
+	           addRow(rows[i].col1, rows[i].col2, rows[i].col3);
+	       }                                              
 	},        
 	handleFailure:function(o){
-           alert("failure " + o);
+	    alert("TableUpdate failure " + o);
 	},        
 	startRequest:function(cid) {
 	   YAHOO.util.Connect.asyncRequest('GET', '/getUpdates?last_id=' + cid, callback2, null);
@@ -351,11 +342,43 @@ var callback2 = {
 	scope: TableUpdate
 };
 
+function showNext(hasNext) {
+    var e1=document.getElementById("next1");    
+    if (hasNext) {        
+        e1.style.display="inline";                 
+    } else {
+        e1.style.display="none";                 
+    }
+ }
+ 
+function showPrev(hasPrev) {
+    var e1=document.getElementById("prev1");    
+    if (hasPrev) {        
+        e1.style.display="inline";                 
+    } else {
+        e1.style.display="none";                 
+    }
+}
+
+function getLastRecId() {
+    var tbl=document.getElementById("main");
+    id=tbl.rows[tbl.rows.length-1].cells[0].getElementsByTagName("DIV")[0].id;
+    return id;
+}
+
+function getFirstRecId() {
+    var tbl=document.getElementById("main");
+    id=tbl.rows[1].cells[0].getElementsByTagName("DIV")[0].id;
+    return id;
+}
+
 function update() {
   try {
-    var tbl=document.getElementById("main-table");
-    if (tbl.rows.length > MAX_ROW_COUNT) 
+    var tbl=document.getElementById("main");
+    if (tbl.rows.length > MAX_ROW_COUNT) {
+        showNext(true);
         return;
+    }
     lastRecId=tbl.rows[tbl.rows.length-1].cells[0].getElementsByTagName("DIV")[0].id;
     TableUpdate.startRequest(lastRecId);
   } finally {
@@ -364,33 +387,42 @@ function update() {
   }
 }
 
+function test() {
+        el=document.getElementById("next2");
+        el.style.display="inline"; 
+}
 
    </script>  
 
 
     </head><body>
     <h2>pony web admin</h2>
-    <table id="main-table" border="1">
+<table><tr><td>
+  <div class="prevnext">
+    <a href="/getLastRecordsHtml2" id="prev1" onClick="this.href='/getLastRecordsHtml2?to=' + getFirstRecId()">prev</a>
+    <a href="/getLastRecordsHtml2" id="next1" onClick="this.href='/getLastRecordsHtml2?frm=' + getLastRecId()">next</a>
+  </div>
+</td></tr><tr><td>                       
+    <table id="main" border="1">
      <thead>
       <tr><td>
-         <span class="col11">id</span> <span class="col12">timestamp</span> <span class="col13">type</span>
+         <span class="col1">id</span> <span class="col2">timestamp</span> <span class="col3">type</span>
       </td></tr>     
      </thead>
      <tbody> 
         $for( r in data ) { 
         
-        <tr><td class="main-row">
-          <div class="row" id=$(r['id'])> 
-            <span class="col11">$(r['id'])</span>
-            <span class="col12">$(r['timestamp'][:-7])</span>
-            <span class="col13">$(r['text'])</span>
+        <tr><td class="row">
+          <div id=$(r['id'])> 
+            <span class="col1">$(r['id'])</span>
+            <span class="col2">$(r['timestamp'][:-7])</span>
+            <span class="col3">$(r['text'])</span>
           </div>
         </td></tr>
         }
       </tbody>
     </table>
-    <div id="prevnext"></div>
-    <input type="button" onclick="update()">
+</td></tr></table>
     </body></html>
     """)
 
