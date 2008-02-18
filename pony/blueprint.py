7fee23e03a787f383dbd2df1d5c556b96a04c6a7
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-02-18 22:45:49 +0000

    Small changes, nothing important
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@494 698dff77-f410-0410-8918-cf622e150f36

--- pony/blueprint.py
+++ pony/blueprint.py
@@ -1,5 +1,9 @@
 from cStringIO import StringIO
 
+try: import Image, ImageDraw
+except ImportError: PIL = False
+else: PIL = True
+
 from pony.templating import template, cycle
 from pony.web import http
 
@@ -12,8 +16,7 @@ def grid(column_count=24, column_width=30, gutter_width=10):
 @http('/blueprint/grid.png', type='image/png')
 @http('/blueprint/$column_count/$column_width/$gutter_width/grid.png', type='image/png')
 def grid_background(column_count=24, column_width=30, gutter_width=10):
-    try: import Image, ImageDraw
-    except ImportError: raise http.NotFound
+    if not PIL: raise http.NotFound
     im = Image.new('RGB', (column_width+gutter_width, 18), (255, 255, 255))
     draw = ImageDraw.Draw(im)
     draw.rectangle((0, 0, column_width, 17), fill=(232, 239, 251))
