3f5f33b421d14e9703d9367dd02b41e924a48158
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-02-20 16:54:58 +0000

    Blueprint layout (first attempt)
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@511 698dff77-f410-0410-8918-cf622e150f36

--- pony/blueprint.py
+++ pony/blueprint.py
@@ -7,14 +7,14 @@ else: PIL = True
 from pony.templating import template, cycle
 from pony.web import http
 
-@http('/blueprint/grid.css', type='text/css')
-@http('/blueprint/$column_count/$column_width/$gutter_width/grid.css', type='text/css')
+@http('/pony/blueprint/grid.css', type='text/css')
+@http('/pony/blueprint/$column_count/$column_width/$gutter_width/grid.css', type='text/css')
 def grid(column_count=24, column_width=30, gutter_width=10):
     page_width = column_count*(column_width+gutter_width) - gutter_width
     return template()
 
-@http('/blueprint/grid.png', type='image/png')
-@http('/blueprint/$column_count/$column_width/$gutter_width/grid.png', type='image/png')
+@http('/pony/blueprint/grid.png', type='image/png')
+@http('/pony/blueprint/$column_count/$column_width/$gutter_width/grid.png', type='image/png')
 def grid_background(column_count=24, column_width=30, gutter_width=10):
     if not PIL: raise http.NotFound
     im = Image.new('RGB', (column_width+gutter_width, 18), (255, 255, 255))
---
+++ pony/layout/blueprint.py
@@ -0,0 +1,33 @@
+from lxml.etree import SubElement, Comment
+from pony.layout import move_content
+
+pony_static_dir = '/pony/static'
+
+def transform(html):
+    head = html.find('head')
+    body = html.find('body')
+    links = head.findall('link')
+    css_links = [ link for link in links
+                       if link.get('rel') == 'stylesheet'
+                       and link.get('type') == 'text/css' ]
+    favicon_links = [ link for link in links if 'icon' in link.get('rel', '').split() ]
+    if not favicon_links:
+        for rel in ('shortcut icon', 'icon'):
+            SubElement(head, 'link', rel=rel, type='image/vnd.microsoft.icon',
+                       href=pony_static_dir + '/favicon.ico')
+    styles = head.findall('style')
+    if not css_links and not styles:
+          SubElement(head, 'link', rel='stylesheet', type='text/css', media="screen, projection",
+                     href= pony_static_dir + '/blueprint/screen.css')
+          SubElement(head, 'link', rel='stylesheet', type='text/css', media="print",
+                     href= pony_static_dir + '/blueprint/print.css')
+          head.append(Comment('start of IE hack'))
+          SubElement(head, 'link', rel='stylesheet', type='text/css', media="screen, projection",
+                     href= pony_static_dir + '/blueprint/ie.css')
+          head.append(Comment('end of IE hack'))
+          content = html.makeelement('div')
+          content.set('class', 'content')
+          move_content(content, [ body ])
+          body[:] = [ content ]
+          body.text = None
+    return html
