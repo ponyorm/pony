e325af397e36580acc5b88d1a6113b8b7aa89ce6
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-05-04 09:43:53 +0000

    Whitespace normalization
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@623 698dff77-f410-0410-8918-cf622e150f36

--- pony/i18n.py
+++ pony/i18n.py
@@ -33,48 +33,49 @@ def parse(lines):
                 check_params(params_list, lineno, lstr, lineno2, langkey)
                 lstr = transform_string(lstr)
                 ld[langkey] = (get_params_order(t, lstr), lstr)
-            else: raise ParseError("No language selector found in line %d (line=%s)" % (lineno2, s))            
-        d[norm_key] = ld                    
+            else: raise ParseError("No language selector found in line %d (line=%s)" % (lineno2, s))
+        d[norm_key] = ld
     return d
 
 def get_params_order(key, lstr):
     pkey, plstr = [], []
-    for (flag, value) in key:
+    for flag, value in key:
         if flag: pkey.append(value)
-    for (flag, value) in lstr:
+    for flag, value in lstr:
         if flag: plstr.append(value)
     result = []
     for v in plstr:
         result.append(pkey.index(v))
     return result
-        
+
 def read_phrases(lines):
    kstr, lstr_list = None, []
    for lineno, line in izip(count(1), lines):
        if line.isspace(): continue
        elif line[0].isspace():
-           if kstr is None: raise ParseError("Translation string found but key string was expected in line %d" % lineno)
+           if kstr is None: raise ParseError(
+               "Translation string found but key string was expected in line %d" % lineno)
            lstr_list.append((lineno, line))
-       elif kstr is None: kstr = (lineno, line)
+       elif kstr is None: kstr = lineno, line
        else:
            yield kstr, lstr_list
            kstr, lstr_list = (lineno, line), []
    if kstr is not None:
        yield kstr, lstr_list
 
-def check_params(params_list, lineno, lstr, lineno2, langkey):    
+def check_params(params_list, lineno, lstr, lineno2, langkey):
     params_list2 = []
     for match in param_re.finditer(lstr):
         if match.group() != '$$': params_list2.append(match.group())
-    if len(params_list) != len(params_list2):
-        raise ParseError("Parameters count in line %d doesn't match with line %d" % (lineno, lineno2))
+    if len(params_list) != len(params_list2): raise ParseError(
+        "Parameters count in line %d doesn't match with line %d" % (lineno, lineno2))
     params_list.sort()
     params_list2.sort()
     for a, b in zip(params_list, params_list2):
-        if a != b:
-            raise ParseError("Unknown parameter in line %d: %s (translation for %s)" % (lineno2, b, langkey))
+        if a != b: raise ParseError(
+            "Unknown parameter in line %d: %s (translation for %s)" % (lineno2, b, langkey))
 
-def transform_string(s):    
+def transform_string(s):
     result = []
     pos = 0
     for match in param_re.finditer(s):
@@ -85,7 +86,7 @@ def transform_string(s):
     result.append((False, s[pos:]))
     prevf = None
     result2 = []
-    for (flag, value) in result:
+    for flag, value in result:
         if flag == prevf == False: result2[-1] = (flag, result2[-1][1] + value)
         elif value: result2.append((flag, value))
         prevf = flag
@@ -94,33 +95,32 @@ def transform_string(s):
 def translate(key, params, lang_list):
     for lang in lang_list:
         while True:
-            try:                
-                (params_order, lstr) = translations[key][lang]
+            try: params_order, lstr = translations[key][lang]
             except KeyError:
                 try:
                     lang = lang[:lang.rindex('-')]
                     continue
                 except ValueError: break
             result = []
-            for (flag, value) in lstr:
+            for flag, value in lstr:
                 if flag: result.append(params[params_order.pop(0)])
                 else: result.append(value)
             return ''.join(result)
     else: return None
-        
+
 if __name__ == "__main__":
     #lines = open("c:\\temp\\trans.txt").readlines()
-    lines = []
-    lines.append('Hello,  $name\n')
-    lines.append(' ru Privet, $name\n')
-    lines.append(' de Guten Tag, 		$name\n')        
-    lines.append('Date dd/mm/yy, $dd/$mm/$yy\n')
-    lines.append(' ru Data dd/mm/yy,	$dd/$mm/$yy\n')
-    lines.append(' de Datai yy/mm/dd,	$yy/$mm/$dd\n')
-    lines.append(' en Date mm/dd/yy,	$mm/$dd/$yy\n')
-    lines.append('Hey\n') 
-    lines.append(' ru\n') 
-
+    import textwrap
+    lines = textwrap.dedent('''Hello,  $name
+      ru Privet, $name
+      de Guten Tag,      $name
+    Date dd/mm/yy, $dd/$mm/$yy
+      ru Data dd/mm/yy,  $dd/$mm/$yy
+      de Datai yy/mm/dd, $yy/$mm/$dd
+      en Date mm/dd/yy,  $mm/$dd/$yy
+    Hey
+      ru
+    ''').split('\n')
     translations = parse(lines)
     print translate('Hello, $#', ['Peter'], ['en-ca', 'ru'])
     print translate('Hello, $#', ['Peter'], ['de'])
@@ -129,3 +129,4 @@ if __name__ == "__main__":
     print translate('Date dd/mm/yy, $#/$#/$#', ['25', '04', '77'], ['de'])
     print translate('Date dd/mm/yy, $#/$#/$#', ['25', '04', '77'], ['en-us'])
     print translate('Hey', [], ['ru'])
+    raw_input()
