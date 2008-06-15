8ef11d49096ff71a6d60138dd88b46f3d9a1947c
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-06-15 16:17:32 +0000

    Small improvement
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@671 698dff77-f410-0410-8918-cf622e150f36

--- pony/bayes.py
+++ pony/bayes.py
@@ -35,7 +35,7 @@ class Filter(object):
             self.all[token] = self.all.get(token, 0) + 1
             if is_spam: self.spam[token] = self.spam.get(token, 0) + 1
     def estimate(self, message):
-        all_count = self.all_count
+        all_count = self.all_count + 1
         if not all_count: return 1
         spam_count = self.spam_count
         result = spam_count / all_count
@@ -45,6 +45,6 @@ class Filter(object):
             if token_count < 2: continue
             tokenspam_count = getspam(token, 0)
             numerator = (tokenspam_count or 0.1) * all_count
-            denominator = (token_count) * spam_count
+            denominator = token_count * spam_count
             result *= numerator / denominator
         return result
