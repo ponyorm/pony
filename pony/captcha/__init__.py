06f64efda1231631c7dfffe31302b4d9ca450732
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-07-18 14:23:24 +0000

    Changes in captcha generation: load pre-generated map om startup
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@754 698dff77-f410-0410-8918-cf622e150f36

--- pony/captcha/__init__.py
+++ pony/captcha/__init__.py
@@ -5,18 +5,45 @@ from math import sin, cos, sqrt, exp
 from random import randint, random, sample, choice
 
 try: import Image, ImageDraw, ImageFont, ImageChops
-except ImportError: PIL = False
-else: PIL = True
+except ImportError: raise ImportError('Cannot import pony.capthca because PIL does not installed')
 
 import pony
 
-font_fnames = glob.glob(os.path.join(pony.PONY_DIR, 'fonts', '*.ttf'))
+if pony.MODE.startswith('GAE-'): raise ImportError('Module pony.capthca does not work inside GAE')
 
-fonts = [ ImageFont.truetype(fname, 60) for fname in font_fnames ]
+dir = os.path.dirname(__file__)
+main = __name__ == '__main__'
 
 numbers = '123456789'
 vovels = 'aeiou'
 consonants = 'bsdfghjklmnpqrstvwxz'
+
+def generate_text():
+    letter_pairs_count = choice([3, 4])
+    s = ''.join(choice(consonants) + choice(vovels) for i in range(letter_pairs_count))
+    if letter_pairs_count == 3: s += ''.join(sample(numbers, 2))
+    elif randint(0, 1): s = s[:-1]
+    return s
+
+font_fnames = glob.glob(os.path.join(dir, '*.ttf'))
+
+fonts = [ ImageFont.truetype(fname, 60) for fname in font_fnames ]
+
+def get_letter_glyph(letter, font):
+    text = ' ' + letter + ' '
+    size = font.getsize(text)
+    img = Image.new(MODE, size, BLACK)
+    draw = ImageDraw.ImageDraw(img)
+    draw.text((0, 0), text, fill=WHITE, font=font)
+    x1, y1, x2, y2 = img.getbbox()
+    return img.crop((x1, 0, x2, size[1]))
+
+maps = []
+
+if __name__ != '__main__':
+    fname = os.path.join(dir, 'map.dat')
+    map = file(fname, 'rb').read().decode('zip')
+    maps.append(map)
 
 QUALITY = 3
 
@@ -29,95 +56,6 @@ HEIGHT = 110
 
 TEXT_X = 15
 TEXT_Y = 30
-
-xmin = ymin = 1000
-xmax = ymax = 0
-
-def generate_text():
-    letter_pairs_count = choice([3, 4])
-    s = ''.join(choice(consonants) + choice(vovels) for i in range(letter_pairs_count))
-    if letter_pairs_count == 3: s += ''.join(sample(numbers, 2))
-    elif randint(0, 1): s = s[:-1]
-    return s
-
-def get_letter_glyph(letter, font):
-    text = ' ' + letter + ' '
-    size = font.getsize(text)
-    img = Image.new(MODE, size, BLACK)
-    draw = ImageDraw.ImageDraw(img)
-    draw.text((0, 0), text, fill=WHITE, font=font)
-    x1, y1, x2, y2 = img.getbbox()
-    return img.crop((x1, 0, x2, size[1]))
-
-def generate_map():
-    X_SCALE = randint(15, 25)
-    Y_SCALE = randint(15, 25)
-    X_AMP = randint(3, 6)
-    Y_AMP = randint(3, 6)
-    X_PHASE = 6.28*random()
-    Y_PHASE = 6.28*random()
-    
-    SWIRL_X = randint(60, 100)
-    SWIRL_Y = randint(70, 80)
-    SWIRL_ANGLE = (random() - 0.5)
-    if SWIRL_ANGLE > 0: SWIRL_ANGLE += 0.4
-    else: SWIRL_ANGLE -= 0.4
-    
-    SWIRL2_X = randint(120, 160)
-    SWIRL2_Y = randint(80, 90)
-    SWIRL2_ANGLE = (random() - 0.5)
-    if SWIRL2_ANGLE > 0: SWIRL2_ANGLE += 0.2
-    else: SWIRL2_ANGLE -= 0.2
-
-    qrange = range(QUALITY)
-    ylist = [   (Y, [   (y, sin(y/Y_SCALE + Y_PHASE)*Y_AMP)
-                        for y in (Y + dy/QUALITY for dy in qrange)   ])
-                for Y in range(HEIGHT)   ]
-    xlist = [   (X, [   (x, sin(x/X_SCALE + X_PHASE)*X_AMP)
-                        for x in (X + dx/QUALITY for dx in qrange)   ])
-                for X in range(WIDTH)   ]
-
-    map = []
-    
-    for Y, ylist2 in ylist:
-        time.sleep(.01)
-        for X, xlist2 in xlist:
-            for y, xshift in ylist2:
-                for x, yshift in xlist2:
-                    x2 = x + xshift
-                    y2 = y + yshift
-
-                    delta_x = x2 - SWIRL_X
-                    delta_y = y2 - SWIRL_Y
-                    radius = sqrt(delta_x**2 + delta_y**2)
-                    angle = SWIRL_ANGLE * exp(-(radius/30)**2)
-                    c = cos(angle)
-                    s = sin(angle)
- 
-                    x3 = SWIRL_X + delta_x*c - delta_y*s
-                    y3 = SWIRL_Y + delta_x*s + delta_y*c
-
-                    delta_x = x3 - SWIRL2_X
-                    delta_y = y3 - SWIRL2_Y
-                    radius = sqrt(delta_x**2 + delta_y**2)
-                    angle = SWIRL2_ANGLE * exp(-(radius/30)**2)
-                    c = cos(angle)
-                    s = sin(angle)
-
-                    x4 = SWIRL2_X + delta_x*c - delta_y*s
-                    y4 = SWIRL2_Y + delta_x*s + delta_y*c
-
-                    x_result = int(x4)
-                    y_result = int(y4)
-                    
-                    if x_result < 0 or x_result >= WIDTH or y_result < 0 or y_result >= HEIGHT:
-                        map.append('\x00\x00')
-                    else:
-                        map.append(chr(x_result - X + 128))
-                        map.append(chr(y_result - Y + 128))
-    return ''.join(map)
-
-maps = []
 
 def generate_captcha():
     text = generate_text()
@@ -160,23 +98,96 @@ def generate_captcha():
     img = img.crop((x1-2, y1-2, x2+2, y2+2))
     return text, img
 
+def generate_map():
+    X_SCALE = randint(15, 25)
+    Y_SCALE = randint(15, 25)
+    X_AMP = randint(3, 6)
+    Y_AMP = randint(3, 6)
+    X_PHASE = 6.28*random()
+    Y_PHASE = 6.28*random()
+    
+    SWIRL_X = randint(60, 100)
+    SWIRL_Y = randint(70, 80)
+    SWIRL_ANGLE = (random() - 0.5)
+    if SWIRL_ANGLE > 0: SWIRL_ANGLE += 0.4
+    else: SWIRL_ANGLE -= 0.4
+    
+    SWIRL2_X = randint(120, 160)
+    SWIRL2_Y = randint(80, 90)
+    SWIRL2_ANGLE = (random() - 0.5)
+    if SWIRL2_ANGLE > 0: SWIRL2_ANGLE += 0.2
+    else: SWIRL2_ANGLE -= 0.2
+
+    qrange = range(QUALITY)
+    ylist = [   (Y, [   (y, sin(y/Y_SCALE + Y_PHASE)*Y_AMP)
+                        for y in (Y + dy/QUALITY for dy in qrange)   ])
+                for Y in range(HEIGHT)   ]
+    xlist = [   (X, [   (x, sin(x/X_SCALE + X_PHASE)*X_AMP)
+                        for x in (X + dx/QUALITY for dx in qrange)   ])
+                for X in range(WIDTH)   ]
+
+    map = []
+    
+    for Y, ylist2 in ylist:
+        if pony.shutdown: return
+        if not (Y % 10) and not main: time.sleep(.1)
+        for X, xlist2 in xlist:
+            for y, xshift in ylist2:
+                for x, yshift in xlist2:
+                    x2 = x + xshift
+                    y2 = y + yshift
+
+                    delta_x = x2 - SWIRL_X
+                    delta_y = y2 - SWIRL_Y
+                    radius = sqrt(delta_x**2 + delta_y**2)
+                    angle = SWIRL_ANGLE * exp(-(radius/30)**2)
+                    c = cos(angle)
+                    s = sin(angle)
+ 
+                    x3 = SWIRL_X + delta_x*c - delta_y*s
+                    y3 = SWIRL_Y + delta_x*s + delta_y*c
+
+                    delta_x = x3 - SWIRL2_X
+                    delta_y = y3 - SWIRL2_Y
+                    radius = sqrt(delta_x**2 + delta_y**2)
+                    angle = SWIRL2_ANGLE * exp(-(radius/30)**2)
+                    c = cos(angle)
+                    s = sin(angle)
+
+                    x4 = SWIRL2_X + delta_x*c - delta_y*s
+                    y4 = SWIRL2_Y + delta_x*s + delta_y*c
+
+                    x_result = int(x4)
+                    y_result = int(y4)
+                    
+                    if x_result < 0 or x_result >= WIDTH or y_result < 0 or y_result >= HEIGHT:
+                        map.append('\x00\x00')
+                    else:
+                        map.append(chr(x_result - X + 128))
+                        map.append(chr(y_result - Y + 128))
+    return ''.join(map)
+
 class CaptchaThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self, name="CaptchaThread")
         self.setDaemon(True)
     def run(self):
+        if not main: time.sleep(5)
         for i in range(10):
-            time.sleep(1)
-            if pony.shutdown: break
             map = generate_map()
+            if pony.shutdown: return
             maps.append(map)
+            if main: print i+1
+            # fname = os.path.join(dir, 'map-%d' % (i+1))
+            # file(fname, 'wb').write(map.encode('zip'))
+        maps.pop(0)
 
-if not pony.MODE.startswith('GAE-'):
-    captcha_thread = CaptchaThread()
-    captcha_thread.start()
+captcha_thread = CaptchaThread()
+captcha_thread.start()
 
-    if __name__ == '__main__':
-        captcha_thread.join()
-        for i in range(99):
-            text, img = generate_captcha()
-            img.save('captcha-%02d-%s.jpg' % (i+1, text))
+if __name__ == '__main__':
+    captcha_thread.join()
+    for i in range(20):
+        print '.',
+        text, img = generate_captcha()
+        img.save('captcha-%02d-%s.jpg' % (i+1, text))
