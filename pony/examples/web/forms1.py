fc7bc14b2abe16449595c7062bab8941176f21ec
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-11-07 19:26:52 +0000

    Forgot to commit...
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@930 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/web/forms1.py
+++ pony/examples/web/forms1.py
@@ -2,16 +2,18 @@ from pony.main import *
 
 use_autoreload()
 
-@http('/?a=$a&b=$b')
-def index(a=None, b=None):
+@http('/')
+def index():
+    a = http['a']
+    b = http['b']
     return html("""
-    <form method="GET">
+    <form method="POST">
       <p><input type="text" name="a">
       <p><input type="text" name="b">
       <p><input type="submit" value="send">
     </form>
     <p>a = $a
-    <p>b = $b
+    <p>b = $b  
     """)
 
 http.start()
