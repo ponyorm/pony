76e430892afe0957950d3eabdfae6f2bdbdd13b2
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-11-21 13:01:21 +0000

    use cElementTree in Python 2.5 instead of ElementTree
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@943 698dff77-f410-0410-8918-cf622e150f36

--- pony/thirdparty/etree.py
+++ pony/thirdparty/etree.py
@@ -1,6 +1,6 @@
 try: from lxml.etree import *
 except ImportError:
-    try: from xml.etree.ElementTree import *
+    try: from xml.etree.cElementTree import *
     except ImportError:
         try: from cElementTree import *
         except ImportError:
