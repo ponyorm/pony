75d075b2637066e7e9c7c9285232973c316bc743
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2008-11-23 14:34:06 +0000

    Indented xml output by default
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@947 698dff77-f410-0410-8918-cf622e150f36

--- pony/syndication.py
+++ pony/syndication.py
@@ -7,20 +7,11 @@ ns = 'http://www.w3.org/2005/Atom'
 nsmap = getattr(etree, '_namespace_map', {})
 nsmap.setdefault(ns, 'atom')
 
-def set_atom_text(element, text):
-    if isinstance(text, (Html, StrHtml)):
-        element.set('{http://www.w3.org/2005/Atom}type', 'html')
-        element.text = text[:]
-    elif isinstance(text, basestring):
-        element.set('{http://www.w3.org/2005/Atom}type', 'text')
-        element.text = text
-    elif hasattr(text, 'makeelement') and getattr(text, 'tag') == '{http://www.w3.org/1999/xhtml}div':
-        element.set('{http://www.w3.org/2005/Atom}type', 'xhtml')
-        element.append(text)
-    else: raise TypeError('Inappropriate text value: %r' % text)
+nsmap_keyargs = {}
+if getattr(etree, 'LXML_VERSION', (0, 0, 0, 0)) >= (1, 3, 7, 0):
+    nsmap_keyargs = {'nsmap': {'atom':ns}}
 
 def atom_date(date):
-    if isinstance(date, basestring): return date
     return rfc3339.rfc3339(date)
 
 def atom_author(author):
@@ -36,6 +27,18 @@ def atom_author(author):
     if email: SubElement(result, '{http://www.w3.org/2005/Atom}email').text = email
     return result
 
+def set_atom_text(element, text):
+    if isinstance(text, (Html, StrHtml)):
+        element.set('{http://www.w3.org/2005/Atom}type', 'html')
+        element.text = text[:]
+    elif isinstance(text, basestring):
+        element.set('{http://www.w3.org/2005/Atom}type', 'text')
+        element.text = text
+    elif hasattr(text, 'makeelement') and getattr(text, 'tag') == '{http://www.w3.org/1999/xhtml}div':
+        element.set('{http://www.w3.org/2005/Atom}type', 'xhtml')
+        element.append(text)
+    else: raise TypeError('Inappropriate text value: %r' % text)
+
 class Feed(object):
     def __init__(self, link, title, updated,
                  id=None, subtitle=None, feed_link=None, author=None, rights=None, icon=None, logo=None):
@@ -50,30 +53,48 @@ class Feed(object):
         self.entries = []
     def add(self, entry):
         self.entries.append(entry)
-    def atom(self):
-        if getattr(etree, 'LXML_VERSION', (0, 0, 0, 0)) >= (1, 3, 7, 0):
-              feed = Element('{http://www.w3.org/2005/Atom}feed', nsmap={'atom':ns})
-        else: feed = Element('{http://www.w3.org/2005/Atom}feed')
+    def __str__(self):
+        return tostring(self.atom())
+    def atom(self, pretty_print=True):
+        if pretty_print: indent = '\n  '
+        else: indent = ''     
+        feed = Element('{http://www.w3.org/2005/Atom}feed', **nsmap_keyargs)
+        feed.text = indent
         link = SubElement(feed, '{http://www.w3.org/2005/Atom}link')
         link.set('{http://www.w3.org/2005/Atom}href', self.link)
+        link.tail = indent
         title = SubElement(feed, '{http://www.w3.org/2005/Atom}title')
         set_atom_text(title, self.title)
+        title.tail = indent
         updated = SubElement(feed, '{http://www.w3.org/2005/Atom}updated')
         updated.text = atom_date(self.updated)
+        updated.tail = indent
         id = SubElement(feed, '{http://www.w3.org/2005/Atom}id')
         id.text = self.id
+        id.tail = indent
         if self.subtitle:
             subtitle = SubElement(feed, '{http://www.w3.org/2005/Atom}summary')
             set_atom_text(subtitle, self.subtitle)
-        if self.author: feed.append(atom_author(self.author))
+            subtitle.tail = indent
+        if self.author:
+            author = atom_author(self.author)
+            feed.append(author)
+            author.tail = indent
         if self.feed_link:
             feed_link = SubElement(feed, '{http://www.w3.org/2005/Atom}link')
             feed_link.set('{http://www.w3.org/2005/Atom}rel', 'self')
             feed_link.set('{http://www.w3.org/2005/Atom}href', self.feed_link)
+            feed_link.tail = indent
         if self.rights:
             rights = SubElement(feed, '{http://www.w3.org/2005/Atom}rights')
             set_atom_text(rights, self.rights)
-        for entry in self.entries: feed.append(entry.atom())
+            rights.tail = indent
+        for entry in self.entries:
+            entry = entry.atom()
+            entry[-1].tail = indent
+            feed.append(entry)
+            entry.tail = indent
+        feed[-1].tail = indent and '\n' or ''
         return feed
 
 class Entry(object):
@@ -89,31 +110,49 @@ class Entry(object):
         self.author = author
         self.rights = rights
         self.published = published
-    def atom(self):
-        entry = Element('{http://www.w3.org/2005/Atom}entry')
+    def __str__(self):
+        return tostring(self.atom())
+    def atom(self, pretty_print=True):
+        if pretty_print: indent = '\n    '
+        else: indent = ''
+        entry = Element('{http://www.w3.org/2005/Atom}entry', **nsmap_keyargs)
+        entry.text = indent
         link = SubElement(entry, '{http://www.w3.org/2005/Atom}link')
         link.set('{http://www.w3.org/2005/Atom}href', self.link)
+        link.tail = indent
         title = SubElement(entry, '{http://www.w3.org/2005/Atom}title')
         set_atom_text(title, self.title)
+        title.tail = indent
         updated = SubElement(entry, '{http://www.w3.org/2005/Atom}updated')
         updated.text = atom_date(self.updated)
+        updated.tail = indent
         id = SubElement(entry, '{http://www.w3.org/2005/Atom}id')
         id.text = self.id
+        id.tail = indent
         if self.summary:
             summary = SubElement(entry, '{http://www.w3.org/2005/Atom}summary')
             set_atom_text(summary, self.summary)
+            summary.tail = indent
         if self.content:
             content = SubElement(entry, '{http://www.w3.org/2005/Atom}content')
             set_atom_text(content, self.content)
+            content.tail = indent
         if self.enclosure:
             enclosure = SubElement(entry, '{http://www.w3.org/2005/Atom}link')
             enclosure.set('{http://www.w3.org/2005/Atom}rel', 'enclosure')
             enclosure.set('{http://www.w3.org/2005/Atom}href', self.enclosure)
-        if self.author: entry.append(atom_author(self.author))
+            enclosure.tail = indent
+        if self.author:
+            author = atom_author(self.author)
+            entry.append(author)
+            author.tail = indent
         if self.rights:
             rights = SubElement(entry, '{http://www.w3.org/2005/Atom}rights')
             set_atom_text(rights, self.rights)
+            rights.tail = indent
         if self.published:
             published = SubElement(entry, '{http://www.w3.org/2005/Atom}published')
             published.text = atom_date(self.published)
-        return entry    
+            published.tail = indent
+        entry[-1].tail = indent and '\n' or ''
+        return entry
