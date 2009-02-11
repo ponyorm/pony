75e646a10bc79ea1ec3b0a13e425f860a19481f5
Alexey Malashkevich <m.alexey@gmail.com>
2009-02-11 02:05:43 +0000

    bdist_wininst fix
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1038 698dff77-f410-0410-8918-cf622e150f36

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
 from glob import glob
+import sys
 
 name = "Pony"
 pony_version = "0.0.1a1"
@@ -34,35 +35,35 @@ packages=[
     "pony.thirdparty.wsgiref" 
 ]
 download_url="http://download.justpony.org/"
-data_files = [ ('pony', ['pony/welcome.html', 'pony/notfound.html',
-                         'pony/htmltb.format_exc.html', 'pony/translations.txt']), 
-               ('pony/captcha', ['pony/captcha/map.dat', 'pony/captcha/VeraSe.ttf']),
-               ('pony/doc', glob('pony/doc/*.txt') ),               
-               ('pony/examples/orm', ['pony/examples/orm/mapping1.xml']),
-               ('pony/examples/web', glob('pony/examples/web/*.html')),
-               ('pony/static', glob('pony/static/*.*')),
-               ('pony/static/blueprint', glob('pony/static/blueprint/*.*')),
-               ('pony/static/css', glob('pony/static/css/*.*')),
-               ('pony/static/img', glob('pony/static/img/*.*')),
-               ('pony/static/jquery', glob('pony/static/jquery/*.*')),
-               ('pony/static/js', glob('pony/static/js/*.*')), 
-               ('pony/static/blueprint/src', glob('pony/static/blueprint/src/*.*')),
-               ('pony/static/css/layouts', glob('pony/static/css/layouts/*.*')),
-               ('pony/static/img/doc', glob('pony/static/img/doc/*.*')),
-               ('pony/static/yui/reset-fonts-grids', glob('pony/static/yui/reset-fonts-grids/*.*')),
-               ('pony/static/blueprint/plugins/buttons', glob('pony/static/blueprint/plugins/buttons/*.*')),
-               ('pony/static/blueprint/plugins/css-classes', glob('pony/static/blueprint/plugins/css-classes/*.*')),
-               ('pony/static/blueprint/plugins/fancy-type', glob('pony/static/blueprint/plugins/fancy-type/*.*')),
-               ('pony/static/blueprint/plugins/link-icons', glob('pony/static/blueprint/plugins/link-icons/*.*')),
-               ('pony/static/blueprint/plugins/rtl', glob('pony/static/blueprint/plugins/rtl/*.*')), 
-               ('pony/static/blueprint/plugins/buttons/icons', glob('pony/static/blueprint/plugins/buttons/icons/*.*')),
-               ('pony/static/blueprint/plugins/link-icons/icons', glob('pony/static/blueprint/plugins/link-icons/icons/*.*')),
-               ('pony/patches', glob('pony/patches/*.*')), 
-               ('pony/patches/pywin', glob('pony/patches/pywin/*.*')), 
-               ('pony/patches/pywin/framework', ['pony/patches/pywin/framework/interact.py']),
-               ('pony/patches/pywin/scintilla', ['pony/patches/pywin/scintilla/view.py']),
-               ('pony/layouts', glob('pony/layouts/*.txt')),
-               ('pony/text', glob('pony/text/*.txt'))
+data_files = [ ['pony', ['pony/welcome.html', 'pony/notfound.html',
+                         'pony/htmltb.format_exc.html', 'pony/translations.txt']], 
+               ['pony/captcha', ['pony/captcha/map.dat', 'pony/captcha/VeraSe.ttf']],
+               ['pony/doc', glob('pony/doc/*.txt')],
+               ['pony/examples/orm', ['pony/examples/orm/mapping1.xml']],
+               ['pony/examples/web', glob('pony/examples/web/*.html')],
+               ['pony/static', glob('pony/static/*.*')],
+               ['pony/static/blueprint', glob('pony/static/blueprint/*.*')],
+               ['pony/static/css', glob('pony/static/css/*.*')],
+               ['pony/static/img', glob('pony/static/img/*.*')],
+               ['pony/static/jquery', glob('pony/static/jquery/*.*')],
+               ['pony/static/js', glob('pony/static/js/*.*')], 
+               ['pony/static/blueprint/src', glob('pony/static/blueprint/src/*.*')],
+               ['pony/static/css/layouts', glob('pony/static/css/layouts/*.*')],
+               ['pony/static/img/doc', glob('pony/static/img/doc/*.*')],
+               ['pony/static/yui/reset-fonts-grids', glob('pony/static/yui/reset-fonts-grids/*.*')],
+               ['pony/static/blueprint/plugins/buttons', glob('pony/static/blueprint/plugins/buttons/*.*')],
+               ['pony/static/blueprint/plugins/css-classes', glob('pony/static/blueprint/plugins/css-classes/*.*')],
+               ['pony/static/blueprint/plugins/fancy-type', glob('pony/static/blueprint/plugins/fancy-type/*.*')],
+               ['pony/static/blueprint/plugins/link-icons', glob('pony/static/blueprint/plugins/link-icons/*.*')],
+               ['pony/static/blueprint/plugins/rtl', glob('pony/static/blueprint/plugins/rtl/*.*')], 
+               ['pony/static/blueprint/plugins/buttons/icons', glob('pony/static/blueprint/plugins/buttons/icons/*.*')],
+               ['pony/static/blueprint/plugins/link-icons/icons', glob('pony/static/blueprint/plugins/link-icons/icons/*.*')],
+               ['pony/patches', glob('pony/patches/*.*')], 
+               ['pony/patches/pywin', glob('pony/patches/pywin/*.*')], 
+               ['pony/patches/pywin/framework', ['pony/patches/pywin/framework/interact.py']],
+               ['pony/patches/pywin/scintilla', ['pony/patches/pywin/scintilla/view.py']],
+               ['pony/layouts', glob('pony/layouts/*.txt')],
+               ['pony/text', glob('pony/text/*.txt')]
              ]
  
 
@@ -78,6 +79,10 @@ def main():
         print s % (name, pony_version, python_version.split(' ', 1)[0])
         exit(1)
     
+    if sys.argv[1] == 'bdist_wininst':
+        for fileInfo in data_files:
+            fileInfo[0] = '\\PURELIB\\%s' % fileInfo[0]
+
     setup(
         name=name,
         version=pony_version,
