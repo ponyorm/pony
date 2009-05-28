eb1e926d0e2576cdfd9a23df4a297e467b08891e
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2009-05-28 10:09:53 +0000

    DebugThread
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1062 698dff77-f410-0410-8918-cf622e150f36

--- pony/debugging.py
+++ pony/debugging.py
@@ -1,3 +1,48 @@
 
-def debug_app(app, environ):
-    return app(environ)
+import threading
+
+import pony
+
+if pony.MODE.startswith('GAE-'):
+    
+    def debug_app(app, environ):
+        return app(environ)
+
+else:
+
+    class Local(threading.local):
+        def __init__(self):
+            self.lock = threading.Lock()
+            self.lock.acquire()
+
+    local = Local()
+
+    def debug_app(app, environ):
+        result_holder = []
+        queue.put((local.lock, app, environ, result_holder))
+        local.lock.acquire()
+        return result_holder[0]
+
+    from Queue import Queue
+    queue = Queue()
+
+    class DebugThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self, name="DebugThread")
+            self.setDaemon(True)
+        def run(self):
+            while True:
+                x = queue.get()
+                if x is None: break
+                lock, app, environ, result_holder = x
+                status, headers, result = app(environ)
+                result_holder.append((status, headers, result))
+                lock.release()
+
+    @pony.on_shutdown
+    def do_shutdown():
+        queue.put(None)
+        debug_thread.join()
+
+    debug_thread = DebugThread()
+    debug_thread.start()
