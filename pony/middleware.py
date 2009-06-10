6f8205c68f0aa968c5350d414e472ae112c00273
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2009-06-10 19:50:26 +0000

    Proper middleware autoreloading
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1087 698dff77-f410-0410-8918-cf622e150f36

--- pony/middleware.py
+++ pony/middleware.py
@@ -1,10 +1,11 @@
 
 from pony import db, debugging
 from pony.utils import simple_decorator
+from pony.autoreload import on_reload
 
 wsgi_middleware_list = []
-pony_middleware_list = [ debugging.debug_middleware ]
-decorator_list = [ debugging.middleware_decorator, db.middleware_decorator ]
+pony_middleware_list = []
+decorator_list = []
 
 def wsgi_wrap(application):
     for m in wsgi_middleware_list: application = m(application)
@@ -17,3 +18,11 @@ def pony_wrap(app):
 def decorator_wrap(func):
     for d in decorator_list: func = d(func)
     return func
+
+@on_reload
+def init():
+    wsgi_middleware_list[:] = []
+    pony_middleware_list[:] = [ debugging.debug_middleware ]
+    decorator_list[:] = [ debugging.middleware_decorator, db.middleware_decorator ]
+
+init()
