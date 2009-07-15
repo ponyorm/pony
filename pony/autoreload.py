3a8b6ff9a519fbcc3103cdb21684a8bf6f6a4937
Alexey Malashkevich <m.alexey@gmail.com>
2009-07-15 23:27:45 +0000

    small refactoring
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1119 698dff77-f410-0410-8918-cf622e150f36

--- pony/autoreload.py
+++ pony/autoreload.py
@@ -49,37 +49,40 @@ def reload(modules, changed_module, filename):
 
 reloading_exception = None
 
+def check_files():
+    global reloading_exception
+    if not reloading_exception:
+        modules = [ m for name, m in sys.modules.items()
+                    if getattr(m, 'USE_AUTORELOAD', False)
+                       and (name.startswith('pony.examples.')
+                            or not name.startswith('pony.')) ]
+    try:
+        for m in modules:
+            filename = abspath(m.__file__)
+            if filename.endswith(".pyc") or filename.endswith(".pyo"):
+                filename = filename[:-1]
+            if not exists(filename): continue
+            mtime = get_mtime(filename)
+            if mtimes.setdefault(filename, mtime) != mtime:
+                try: reload(modules, m, filename)
+                except Exception:
+                    # Запоминаем traceback так что мы можем отобразить его
+                    # на веб-странице позднее, когда поступит какой-либо HTTP запрос
+                    reloading_exception = sys.exc_info()
+                else: reloading_exception = None
+                break
+    except:
+        log_exc()
+        sys.exit()
+
 def use_autoreload():
     if pony.MODE != 'CHERRYPY' or pony.mainloop_counter.next(): return
     try: load_main()
     except Exception, e:
         pony.exception_in_main = e
         raise
-    if not pony._do_mainloop: sys.exit()
-    global reloading_exception
+    if not pony._do_mainloop: sys.exit()    
     while True:
         if pony.shutdown: sys.exit()
-        if not reloading_exception:
-            modules = [ m for name, m in sys.modules.items()
-                        if getattr(m, 'USE_AUTORELOAD', False)
-                           and (name.startswith('pony.examples.')
-                                or not name.startswith('pony.')) ]
-        try:
-            for m in modules:
-                filename = abspath(m.__file__)
-                if filename.endswith(".pyc") or filename.endswith(".pyo"):
-                    filename = filename[:-1]
-                if not exists(filename): continue
-                mtime = get_mtime(filename)
-                if mtimes.setdefault(filename, mtime) != mtime:
-                    try: reload(modules, m, filename)
-                    except Exception:
-                        # Запоминаем traceback так что мы можем отобразить его
-                        # на веб-странице позднее, когда поступит какой-либо HTTP запрос
-                        reloading_exception = sys.exc_info()
-                    else: reloading_exception = None
-                    break
-            time.sleep(1)
-        except:
-            log_exc()
-            sys.exit()
+        check_files()
+        time.sleep(1)
