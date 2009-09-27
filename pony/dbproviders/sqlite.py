6b9e69b45a8250f4942319626546f722fca59962
Alexey Malashkevich <m.alexey@gmail.com>
2009-09-27 20:03:03 +0000

    one connection for each thread for sqlite provider
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1207 698dff77-f410-0410-8918-cf622e150f36

--- pony/dbproviders/sqlite.py
+++ pony/dbproviders/sqlite.py
@@ -5,14 +5,23 @@ from pony.thirdparty.sqlite import (Warning, Error, InterfaceError, DatabaseErro
                                     ProgrammingError, NotSupportedError)
 
 from pony import dbapiprovider
+from pony.utils import localbase
 
 paramstyle = 'qmark'
+
+class Local(localbase):
+    def __init__(self):
+        self.connection = None
+
+local = Local()
 
 def quote_name(connection, name):
     return dbapiprovider.quote_name(name)
 
 def connect(filename):
-    return sqlite.connect(filename)
+    if local.connection is None:
+        local.connection = sqlite.connect(filename)
+    return local.connection
 
 def release(connection):
     connection.close()
