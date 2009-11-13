053b7598dbb133f67a5ed036d178fc889f9649b5
Alexey Malashkevich <m.alexey@gmail.com>
2009-11-13 00:20:02 +0000

    gallery small improvement
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1229 698dff77-f410-0410-8918-cf622e150f36

--- pony/examples/web/gallery/gallery.py
+++ pony/examples/web/gallery/gallery.py
@@ -6,7 +6,7 @@ use_autoreload()
 @http("/")
 def index():
     IMAGES_PATH = "static/images/demo/"
-    imagelist = [ IMAGES_PATH + s for s in os.listdir(IMAGES_PATH)]
+    imagelist = [ IMAGES_PATH + s for s in os.listdir(IMAGES_PATH) if s.endswith(".jpg")]
     return html()
 
 	
