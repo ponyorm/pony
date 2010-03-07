e21016de91dc7326395c6bf9cd9fb21bf5a0f32f
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2010-03-07 00:56:40 +0000

    Improved locking for get_multi, set_multi, etc.
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1318 698dff77-f410-0410-8918-cf622e150f36

--- pony/caching.py
+++ pony/caching.py
@@ -55,6 +55,8 @@ from threading import Lock
 from time import time as gettime
 from weakref import ref
 
+from pony.utils import simple_decorator
+
 class Node(object):
     __slots__ = 'prev', 'next', 'key', 'value', 'expire', 'access', '__weakref__'
 
@@ -78,6 +80,12 @@ def normalize(key, value="", expire=None):
         elif expire <= MONTH: expire = int(gettime()) + expire
         elif expire <= MONTH * 100: raise ValueError('Invalid expire value: %d' % expire)
     return key, value, expire
+
+@simple_decorator
+def with_lock(func, self, *args, **keyargs):
+    self._lock.acquire()
+    try: return func(self, *args, **keyargs)
+    finally: self._lock.release()
 
 class Memcache(object):
     def __init__(self, max_data_size=64*1024*1024):
@@ -179,92 +187,87 @@ class Memcache(object):
             if node is not None and expire == node.expire: new_heap.append(item)
         heapify(new_heap)
         self._heap = new_heap
-    def get(self, key):
+    def _get(self, key):
         self._get_count += 1
         key, _, _ = normalize(key)
-        self._lock.acquire()
-        try:
-            node = self._find_node(key)
-            if node is None:
-                self._misses += 1
-                return None
-            self._place_on_top(node)
-            self._hits += 1
-            return node.value
-        finally: self._lock.release()
+        node = self._find_node(key)
+        if node is None:
+            self._misses += 1
+            return None
+        self._place_on_top(node)
+        self._hits += 1
+        return node.value
+    get = with_lock(_get)
+    @with_lock
     def get_multi(self, keys, key_prefix=''):
         result = {}
         for key in keys:
-            val = self.get(key_prefix + key)
+            val = self._get(key_prefix + key)
             if val is not None: result[key] = val
         return result
-    def set(self, key, value, time=None):
+    def _set(self, key, value, time=None):
         self._set_count += 1
         key, value, expire = normalize(key, value, time)
-        self._lock.acquire()
-        try:
-            node = self._find_node(key)
-            if node is None: node = self._create_node(key)
-            self._place_on_top(node)
-            self._set_node_value(node, value, expire)
-        finally: self._lock.release()
+        node = self._find_node(key)
+        if node is None: node = self._create_node(key)
+        self._place_on_top(node)
+        self._set_node_value(node, value, expire)
         return True
+    set = with_lock(_set)
+    @with_lock
     def set_multi(self, mapping, time=None, key_prefix=''):
         for key, value in mapping.iteritems():
-            self.set(key_prefix + key, value, time)
+            self._set(key_prefix + key, value, time)
         return []
-    def add(self, key, value, time=None):
+    def _add(self, key, value, time=None):
         self._add_count += 1
         key, value, expire = normalize(key, value, time)
-        self._lock.acquire()
-        try:
-            node = self._find_node(key)
-            if node is not None:
-                self._place_on_top(node)
-                return False
-            node = self._create_node(key)
+        node = self._find_node(key)
+        if node is not None:
             self._place_on_top(node)
-            self._set_node_value(node, value, expire)
-        finally: self._lock.release()
+            return False
+        node = self._create_node(key)
+        self._place_on_top(node)
+        self._set_node_value(node, value, expire)
         return True
+    add = with_lock(_add)
+    @with_lock
     def add_multi(self, mapping, time=None, key_prefix=''):
         result = []
         for key, value in mapping.iteritems():
-            if not self.add(key_prefix + key, value, time):
+            if not self._add(key_prefix + key, value, time):
                 result.append(key)
         return result
-    def replace(self, key, value, time=None):
+    def _replace(self, key, value, time=None):
         self._replace_count += 1
         key, value, expire = normalize(key, value, time)
-        self._lock.acquire()
-        try:
-            node = self._find_node(key)
-            if node is None: return False
-            self._place_on_top(node)
-            if node.value is None: return False
-            self._set_node_value(node, value, expire)
-        finally: self._lock.release()
+        node = self._find_node(key)
+        if node is None: return False
+        self._place_on_top(node)
+        if node.value is None: return False
+        self._set_node_value(node, value, expire)
         return True
+    replace = with_lock(_replace)
+    @with_lock
     def replace_multi(self, mapping, time=None, key_prefix=''):
         result = []
         for key, value in mapping.iteritems():
-            if not self.replace(key_prefix + key, value, time):
+            if not self._replace(key_prefix + key, value, time):
                 result.append(key)
         return result
-    def delete(self, key, seconds=None):
+    def _delete(self, key, seconds=None):
         self._delete_count += 1
         key, _, seconds = normalize(key, "", seconds)
-        self._lock.acquire()
-        try:
-            node = self._find_node(key)
-            if node is None or node.value is None: return 1
-            if seconds is None: self._delete_node(node)
-            else: self._set_node_value(node, None, seconds)
-        finally: self._lock.release()
+        node = self._find_node(key)
+        if node is None or node.value is None: return 1
+        if seconds is None: self._delete_node(node)
+        else: self._set_node_value(node, None, seconds)
         return 2
+    delete = with_lock(_delete)
+    @with_lock
     def delete_multi(self, keys, seconds=None, key_prefix=''):
         for key in keys:
-            self.delete(key_prefix + key, seconds)
+            self._delete(key_prefix + key, seconds)
         return []
     def incr(self, key, delta=1):
         self._incr_count += 1
