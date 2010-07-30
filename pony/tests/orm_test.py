be00ce97dad4c494f73030925894d7b85425cd8f
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2010-07-30 06:48:23 +0000

    Mapping information was added in orm_test.py
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1352 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm_test.py
+++ pony/tests/orm_test.py
@@ -1,27 +1,34 @@
 from pony.orm3 import *
+
 class Student(Entity):
+    _table_ = "Students"
     zach = PrimaryKey(int)
-    name = Required(unicode)
+    name = Required(unicode, column="fio")
     group = Required("Group")
     stipendy = Required(int, default=0)
     marks = Set("Mark")
 
 class Group(Entity):
+    _table_ = "Groups"
     number = PrimaryKey(str)
     kaf = Required(int)
     students = Set("Student")
     subjects = Set("Subject")
 
 class Subject(Entity):
+    _table_ = "Subjects"
     name = PrimaryKey(unicode)
     groups = Set("Group")
     marks = Set("Mark")
 
 class Mark(Entity):
-    student = Required(Student)
+    _table_ = "Exams"
+    student = Required(Student, column="zach")
     subject = Required(Subject)
     value = Required(int)
-    Unique(student, subject)
+    PrimaryKey(student, subject)
+
+generate_mapping()
 
 g1 = Group.create(number='4142', kaf=44)
 g2 = Group.create(number='3137', kaf=33)
@@ -34,4 +41,6 @@ subj3 = Subject.create('Chemistry')
 m1 = Mark.create(student=s1, subject=subj1, value=4)
 m2 = Mark.create(student=s2, subject=subj1, value=5)
 m3 = Mark.create(student=s1, subject=subj2, value=3)
+
 trans = get_trans()
+
