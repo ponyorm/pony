7bb0cbcbc60113fcc0abfbf89b90d659062e3c31
Alexey Malashkevich <m.alexey@gmail.com>
2010-10-08 14:41:08 +0000

    orm test updated
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1413 698dff77-f410-0410-8918-cf622e150f36

---
+++ pony/tests/orm/model1.py
@@ -0,0 +1,40 @@
+import os.path
+from pony.db import Database
+from pony.orm3 import *
+
+class Student(Entity):
+    _table_ = "Students"
+    record = PrimaryKey(int)
+    name = Required(unicode, column="fio")
+    group = Required("Group")
+    scholarship = Required(int, default=0)
+    marks = Set("Mark")
+
+class Group(Entity):
+    _table_ = "Groups"
+    number = PrimaryKey(str)
+    department = Required(int)
+    students = Set("Student")
+    subjects = Set("Subject")
+
+class Subject(Entity):
+    _table_ = "Subjects"
+    name = PrimaryKey(unicode)
+    groups = Set("Group")
+    marks = Set("Mark")
+
+class Mark(Entity):
+    _table_ = "Exams"
+    student = Required(Student, column="student")
+    subject = Required(Subject, column="subject")
+    value = Required(int)
+    PrimaryKey(student, subject)
+
+db = Database('sqlite', ':memory:')
+path = os.path.split(__file__)[0]
+script_filename = os.path.join(path, 'model1-database.sql')
+script_sql = file(script_filename).read()
+db.get_connection().executescript(script_sql)
+db.commit()
+generate_mapping(db, check_tables=True)
+
