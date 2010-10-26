e3f2ca47665ae4afb5a55e2623904b7db3f50b1a
Alexey Malashkevich <m.alexey@gmail.com>
2010-10-26 15:55:49 +0000

    oops
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1442 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/testutils.py
+++ pony/tests/orm/testutils.py
@@ -6,7 +6,7 @@ def raises_exception(exc_class, msg):
                 self.assert_(False, "expected exception %s wasn't raised" % exc_class.__name__)
             except exc_class, e:
                 self.assertEqual(e.args[0], msg, "incorrect exception message. expected '%s', got '%s'"
-                % (msg, e.message))
+                % (msg, e.args[0]))
         wrapper.__name__ = func.__name__
         return wrapper
     return decorator
