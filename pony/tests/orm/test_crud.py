5243da8673431947d32327b435aa48fed5c88600
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2010-12-18 16:26:20 +0000

    test_crud now checks Entity._cached_create_sql_
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1484 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/test_crud.py
+++ pony/tests/orm/test_crud.py
@@ -10,13 +10,21 @@ class TestCRUD(unittest.TestCase):
 
     def test_create(self):
         g1 = Group.create(number=1, department=2)
+
+        self.assertEqual(Student._cached_create_sql_, None)
         s1 = Student.create(record=3, name='A', group=g1)
         s2 = Student.create(record=4, name='B', group=g1, scholarship=500)
+
         get_trans().commit()
+        sql = 'INSERT INTO "Students" ("record", "fio", "group", "scholarship") VALUES (?, ?, ?, ?)'
+        self.assertEqual(Student._cached_create_sql_[0], sql)
+
         g1_row = db.get('* from Groups where number = 1')
         self.assertEqual(g1_row, ('1', 2))
+
         s1_row = db.get('* from Students where record = 3')
         self.assertEqual(s1_row, (3, 'A', '1', 0))
+
         s2_row = db.get('* from Students where record = 4')
         self.assertEqual(s2_row, (4, 'B', '1', 500))
         
