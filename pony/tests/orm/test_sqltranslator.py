34e08e61c30c1290c60854e33542c483e19e0e25
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-01-15 18:50:31 +0000

    test_sqltranslator.py was extended
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1556 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/test_sqltranslator.py
+++ pony/tests/orm/test_sqltranslator.py
@@ -15,8 +15,13 @@ class TestSQLTranslator(unittest.TestCase):
         class Student(Entity):
             _table_ = 'Students'
             name = Required(unicode)
+            group = Required('Group')
             scholarship = Required(int, default=0)
             grades = Set('Grade')
+        class Group(Entity):
+            _table_ = 'Groups'
+            id = PrimaryKey(int)
+            students = Set(Student)
         class Course(Entity):
             _table_ = 'Courses'
             name = Required(unicode)
@@ -34,7 +39,12 @@ class TestSQLTranslator(unittest.TestCase):
             create table Students(
               id integer primary key,
               name varchar(50) not null,
+              "group" integer not null,
               scholarship integer not null default 0
+            );
+            drop table if exists Groups;
+            create table Groups(
+              id integer primary key
             );
             drop table if exists Courses;
             create table Courses(
@@ -48,9 +58,11 @@ class TestSQLTranslator(unittest.TestCase):
               value varchar(2) not null,
               primary key(student, course)
             );
-            insert into Students values (1, 'S1', 0);
-            insert into Students values (2, 'S2', 100);
-            insert into Students values (3, 'S3', 500);
+            insert into Students values (1, 'S1', 1, 0);
+            insert into Students values (2, 'S2', 1, 100);
+            insert into Students values (3, 'S3', 2, 500);
+            insert into Groups values (1);
+            insert into Groups values (2);
             insert into Courses values (10, 'Math');
             insert into Courses values (11, 'Economics');
             insert into Courses values (12, 'Physics');
@@ -95,7 +107,81 @@ class TestSQLTranslator(unittest.TestCase):
         grades = [ grade.value for grade in result ]
         grades.sort()
         self.assertEquals(grades, ['B', 'C'])
-        
-
+    def test_function_max1(self):
+        Student = self.diagram.entities["Student"]
+        result = select(s for s in Student if max(s.grades.value) == 'C')
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S1'])
+    def test_function_max2(self):
+        Grade = self.diagram.entities["Grade"]
+        Student = self.diagram.entities["Student"]
+        Course = self.diagram.entities["Course"]
+        grade1 = Grade(Student(1), Course(12))
+        try:
+            result = select(s for s in Student if max(s.grades) == grade1)
+            self.assert_(False)
+        except TypeError:
+            self.assert_(True)
+    def test_function_min(self):
+        Student = self.diagram.entities["Student"]
+        result = select(s for s in Student if min(s.grades.value) == 'B')
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S2'])
+    def test_function_min2(self):
+        Grade = self.diagram.entities["Grade"]
+        Student = self.diagram.entities["Student"]
+        Course = self.diagram.entities["Course"]
+        grade1 = Grade(Student(1), Course(12))
+        try:
+            result = select(s for s in Student if min(s.grades) == grade1)
+            self.assert_(False)
+        except TypeError:
+            self.assert_(True)
+    def test_function_len1(self):
+        Student = self.diagram.entities["Student"]
+        result = select(s for s in Student if len(s.grades) == 1)
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S2'])
+    def test_function_len2(self):
+        Student = self.diagram.entities["Student"]
+        result = select(s for s in Student if max(s.grades.value) == 'C')
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S1'])
+    def test_function_sum1(self):
+        Group = self.diagram.entities["Group"]
+        result = select(g for g in Group if sum(g.students.scholarship) == 100)
+        group_ids = [ group.id for group in result ]
+        self.assertEquals(group_ids, [1])
+    def test_function_sum2(self):
+        Group = self.diagram.entities["Group"]
+        try:
+            result = select(g for g in Group if sum(g.students) == 100)
+            self.assert_(False)
+        except TypeError:
+            self.assert_(True)
+    def test_function_sum3(self):
+        Group = self.diagram.entities["Group"]
+        try:
+            result = select(g for g in Group if sum(g.students.name) == 100)
+            self.assert_(False)
+        except TypeError:
+            self.assert_(True)
+    def test_function_abs(self):
+        Student = self.diagram.entities["Student"]
+        result = select(s for s in Student if abs(s.scholarship) == 100)
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S2'])
+    def test_builtin_in_locals(self):
+        x = max
+        Student = self.diagram.entities["Student"]
+        gen = (s for s in Student if x(s.grades.value) == 'C')
+        result = select(gen)
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, ['S1'])
+        x = min
+        result = select(gen)
+        names = [ stud.name for stud in result ]
+        self.assertEquals(names, [])
+       
 if __name__ == "__main__":
     unittest.main()
