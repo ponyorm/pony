f7ecb9c062bba3f9cf5d04f0361bf517487bfb01
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-03-12 17:31:49 +0000

    remove debug print
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1629 698dff77-f410-0410-8918-cf622e150f36

--- pony/sqltranslator.py
+++ pony/sqltranslator.py
@@ -387,7 +387,6 @@ class SQLTranslator(ASTTranslator):
                 func_monad_class = special_functions[func]
                 node.monad = func_monad_class(translator)
             else:
-                print name, value_type
                 if name in ('True', 'False') and issubclass(value_type, int):
                     node.monad = ConstMonad(translator, name == 'True' and 1 or 0)
                 elif value_type is NoneType: node.monad = ConstMonad(translator, None)
