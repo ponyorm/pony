9ba2d861e16a14065669a5d7eccacca188047595
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-03-24 12:31:13 +0000

    Renaming: self -> estimator
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1653 698dff77-f410-0410-8918-cf622e150f36

--- pony/text/bayes.py
+++ pony/text/bayes.py
@@ -3,31 +3,31 @@ from __future__ import division
 from pony.text import tokenize
 
 class Estimator(object):
-    def __init__(self):
-        self.all = {}
-        self.spam = {}
-        self.all_count = self.spam_count = 0
-    def tokenize(self, message, prefix=''):
+    def __init__(estimator):
+        estimator.all = {}
+        estimator.spam = {}
+        estimator.all_count = estimator.spam_count = 0
+    def tokenize(estimator, message, prefix=''):
         return tokenize(message, prefix)
-    def train(self, good, message, **keyargs):
-        self.all_count += 1
-        if good: self.spam_count += 1
+    def train(estimator, good, message, **keyargs):
+        estimator.all_count += 1
+        if good: estimator.spam_count += 1
         keyargs['']=message
         for prefix, s in keyargs.items():
-            for token in set(self.tokenize(message, prefix)):
-                self.all[token] = self.all.get(token, 0) + 1
-                if good: self.spam[token] = self.spam.get(token, 0) + 1
-    def good(self, message, **keyargs):
-        self.train(True, message, **keyargs)
-    def bad(self, message, **keyargs):
-        self.train(False, message, **keyargs)
-    def estimate(self, message):
-        all_count = self.all_count + 1
+            for token in set(estimator.tokenize(message, prefix)):
+                estimator.all[token] = estimator.all.get(token, 0) + 1
+                if good: estimator.spam[token] = estimator.spam.get(token, 0) + 1
+    def good(estimator, message, **keyargs):
+        estimator.train(True, message, **keyargs)
+    def bad(estimator, message, **keyargs):
+        estimator.train(False, message, **keyargs)
+    def estimate(estimator, message):
+        all_count = estimator.all_count + 1
         if not all_count: return 1
-        spam_count = self.spam_count
+        spam_count = estimator.spam_count
         result = spam_count / all_count
-        getall, getspam = self.all.get, self.spam.get
-        for token in set(self.tokenize(message)):
+        getall, getspam = estimator.all.get, estimator.spam.get
+        for token in set(estimator.tokenize(message)):
             token_count = getall(token, 0)
             if token_count < 2: continue
             tokenspam_count = getspam(token, 0)
