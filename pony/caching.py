0de95b68dcf7e2310228746a300ba71c38b62edd
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-03-24 14:23:35 +0000

    Renaming: self -> cache
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1658 698dff77-f410-0410-8918-cf622e150f36

--- pony/caching.py
+++ pony/caching.py
@@ -84,88 +84,88 @@ def normalize(key, value="", expire=None):
     return key, value, expire
 
 @simple_decorator
-def with_lock(func, self, *args, **keyargs):
-    self._lock.acquire()
-    try: return func(self, *args, **keyargs)
-    finally: self._lock.release()
+def with_lock(func, cache, *args, **keyargs):
+    cache._lock.acquire()
+    try: return func(cache, *args, **keyargs)
+    finally: cache._lock.release()
 
 DEFAULT_MAX_DATA_SIZE = 64*1024*1024  # 64 Megabytes
 
 class Memcache(object):
-    def __init__(self, max_data_size=DEFAULT_MAX_DATA_SIZE):
-        self._lock = Lock()
-        self._heap = []
-        self._dict = {}
-        self._list = list = Node()
+    def __init__(cache, max_data_size=DEFAULT_MAX_DATA_SIZE):
+        cache._lock = Lock()
+        cache._heap = []
+        cache._dict = {}
+        cache._list = list = Node()
         list.prev = list.next = list.expire = list.key = list.value = None
         list.prev = list.next = list
         list.access = int(gettime())
-        self._data_size = 0
+        cache._data_size = 0
         if not isinstance(max_data_size, int): raise TypeError, 'Max data size must be int. Got: %s' % type(max_data_size).__name__
-        self.max_data_size = max_data_size
-        self._stat_get_hits = self._stat_get_misses = self._stat_evictions = 0
-        self._stat_cmd_get = self._stat_cmd_set = 0
-        self._stat_total_items = 0
-        self._start_time = int(gettime())
-    def __len__(self):
-        return len(self._dict)
-    def __iter__(self):
-        return iter(self.items())
+        cache.max_data_size = max_data_size
+        cache._stat_get_hits = cache._stat_get_misses = cache._stat_evictions = 0
+        cache._stat_cmd_get = cache._stat_cmd_set = 0
+        cache._stat_total_items = 0
+        cache._start_time = int(gettime())
+    def __len__(cache):
+        return len(cache._dict)
+    def __iter__(cache):
+        return iter(cache.items())
     @with_lock
-    def items(self):
+    def items(cache):
         now = int(gettime())
         result = []
         append = result.append
-        list = self._list
+        list = cache._list
         node = list.next
         while node is not list:
             expire = node.expire
-            if expire is not None and expire <= now: self._delete_node(node)
+            if expire is not None and expire <= now: cache._delete_node(node)
             elif node.value is not None: append((node.key, node.value))
             node = node.next
         return result
-    def _delete_node(self, node, unlink=True):
+    def _delete_node(cache, node, unlink=True):
         if unlink:
             prev, next = node.prev, node.next
             prev.next = next
             next.prev = prev
-        del self._dict[node.key]
-        self._data_size -= len(node.key)
-        if node.value is not None: self._data_size -= len(node.value)
-    def _find_node(self, key):
-        node = self._dict.get(key)
+        del cache._dict[node.key]
+        cache._data_size -= len(node.key)
+        if node.value is not None: cache._data_size -= len(node.value)
+    def _find_node(cache, key):
+        node = cache._dict.get(key)
         if node is None: return None
         prev, next = node.prev, node.next
         prev.next = next
         next.prev = prev
         expire = node.expire
         if expire is None or expire > int(gettime()): return node
-        self._delete_node(node, unlink=False)
+        cache._delete_node(node, unlink=False)
         return None
-    def _create_node(self, key):
-        self._dict[key] = node = Node()
+    def _create_node(cache, key):
+        cache._dict[key] = node = Node()
         node.key = key
         node.value = None
-        self._data_size += len(node.key)
-        self._stat_total_items += 1
+        cache._data_size += len(node.key)
+        cache._stat_total_items += 1
         return node
-    def _place_on_top(self, node):
-        list = self._list
+    def _place_on_top(cache, node):
+        list = cache._list
         old_top = list.next
         node.prev, node.next = list, old_top
         list.next = old_top.prev = node
         node.access = int(gettime())
-    def _set_node_value(self, node, value, expire):
-        if node.value is not None: self._data_size -= len(node.value)
-        if value is not None: self._data_size += len(value)
+    def _set_node_value(cache, node, value, expire):
+        if node.value is not None: cache._data_size -= len(node.value)
+        if value is not None: cache._data_size += len(value)
         node.value, node.expire = value, expire
-        if expire is not None: heappush(self._heap, (expire, ref(node)))
-        self._delete_expired_nodes()
-        self._conform_to_limits()
-        if len(self._heap) > len(self._dict) * 2: self._pack_heap()
-    def _delete_expired_nodes(self):
+        if expire is not None: heappush(cache._heap, (expire, ref(node)))
+        cache._delete_expired_nodes()
+        cache._conform_to_limits()
+        if len(cache._heap) > len(cache._dict) * 2: cache._pack_heap()
+    def _delete_expired_nodes(cache):
         now = int(gettime())
-        heap = self._heap
+        heap = cache._heap
         while heap:
             expire, node_ref = heap[0]
             if expire > now: break
@@ -173,112 +173,112 @@ class Memcache(object):
             node = node_ref()
             if node is not None:
                 expire = node.expire
-                if expire is not None and expire <= now: self._delete_node(node)
-    def _conform_to_limits(self):
-        list = self._list
-        while self._data_size > self.max_data_size:
+                if expire is not None and expire <= now: cache._delete_node(node)
+    def _conform_to_limits(cache):
+        list = cache._list
+        while cache._data_size > cache.max_data_size:
             bottom = list.prev
-            self._delete_node(bottom)
-            self._stat_evictions += 1
-    def _pack_heap(self):
+            cache._delete_node(bottom)
+            cache._stat_evictions += 1
+    def _pack_heap(cache):
         new_heap = []
-        for item in self._heap:
+        for item in cache._heap:
             expire, node_ref = item
             node = node_ref()
             if node is not None and expire == node.expire: new_heap.append(item)
         heapify(new_heap)
-        self._heap = new_heap
-    def _get(self, key):
-        self._stat_cmd_get += 1
+        cache._heap = new_heap
+    def _get(cache, key):
+        cache._stat_cmd_get += 1
         key, _, _ = normalize(key)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None:
-            self._stat_get_misses += 1
+            cache._stat_get_misses += 1
             return None
-        self._place_on_top(node)
-        self._stat_get_hits += 1
+        cache._place_on_top(node)
+        cache._stat_get_hits += 1
         return node.value
     get = with_lock(_get)
     @with_lock
-    def get_multi(self, keys, key_prefix=''):
+    def get_multi(cache, keys, key_prefix=''):
         result = {}
         for key in keys:
-            val = self._get(key_prefix + key)
+            val = cache._get(key_prefix + key)
             if val is not None: result[key] = val
         return result
-    def _set(self, key, value, time=0):
-        self._stat_cmd_set += 1
+    def _set(cache, key, value, time=0):
+        cache._stat_cmd_set += 1
         key, value, expire = normalize(key, value, time)
-        node = self._find_node(key)
-        if node is None: node = self._create_node(key)
-        self._place_on_top(node)
-        self._set_node_value(node, value, expire)
+        node = cache._find_node(key)
+        if node is None: node = cache._create_node(key)
+        cache._place_on_top(node)
+        cache._set_node_value(node, value, expire)
         return True
     set = with_lock(_set)
     @with_lock
-    def set_multi(self, mapping, time=0, key_prefix=''):
+    def set_multi(cache, mapping, time=0, key_prefix=''):
         for key, value in mapping.iteritems():
-            self._set(key_prefix + key, value, time)
+            cache._set(key_prefix + key, value, time)
         return []
-    def _add(self, key, value, time=0):
-        self._stat_cmd_set += 1
+    def _add(cache, key, value, time=0):
+        cache._stat_cmd_set += 1
         key, value, expire = normalize(key, value, time)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is not None:
-            self._place_on_top(node)
+            cache._place_on_top(node)
             return False
-        node = self._create_node(key)
-        self._place_on_top(node)
-        self._set_node_value(node, value, expire)
+        node = cache._create_node(key)
+        cache._place_on_top(node)
+        cache._set_node_value(node, value, expire)
         return True
     add = with_lock(_add)
     @with_lock
-    def add_multi(self, mapping, time=0, key_prefix=''):
+    def add_multi(cache, mapping, time=0, key_prefix=''):
         result = []
         for key, value in mapping.iteritems():
-            if not self._add(key_prefix + key, value, time):
+            if not cache._add(key_prefix + key, value, time):
                 result.append(key)
         return result
-    def _replace(self, key, value, time=0):
-        self._stat_cmd_set += 1
+    def _replace(cache, key, value, time=0):
+        cache._stat_cmd_set += 1
         key, value, expire = normalize(key, value, time)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None: return False
-        self._place_on_top(node)
+        cache._place_on_top(node)
         if node.value is None: return False
-        self._set_node_value(node, value, expire)
+        cache._set_node_value(node, value, expire)
         return True
     replace = with_lock(_replace)
     @with_lock
-    def replace_multi(self, mapping, time=0, key_prefix=''):
+    def replace_multi(cache, mapping, time=0, key_prefix=''):
         result = []
         for key, value in mapping.iteritems():
-            if not self._replace(key_prefix + key, value, time):
+            if not cache._replace(key_prefix + key, value, time):
                 result.append(key)
         return result
-    def _delete(self, key, seconds=0):
-        self._stat_cmd_set += 1
+    def _delete(cache, key, seconds=0):
+        cache._stat_cmd_set += 1
         key, _, seconds = normalize(key, "", seconds)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None or node.value is None: return 1
-        if seconds is None: self._delete_node(node)
+        if seconds is None: cache._delete_node(node)
         else:
-            self._place_on_top(node)
-            self._set_node_value(node, None, seconds)
+            cache._place_on_top(node)
+            cache._set_node_value(node, None, seconds)
         return 2
     delete = with_lock(_delete)
     @with_lock
-    def delete_multi(self, keys, seconds=0, key_prefix=''):
+    def delete_multi(cache, keys, seconds=0, key_prefix=''):
         for key in keys:
-            self._delete(key_prefix + key, seconds)
+            cache._delete(key_prefix + key, seconds)
         return []
     @with_lock
-    def incr(self, key, delta=1):
-        self._stat_cmd_set += 1
+    def incr(cache, key, delta=1):
+        cache._stat_cmd_set += 1
         key, _, _ = normalize(key)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None: return None
-        self._place_on_top(node)
+        cache._place_on_top(node)
         value = node.value
         if value is None: return None
         try: value = int(value) + delta
@@ -286,48 +286,48 @@ class Memcache(object):
         if value < 0: value = 0
         node.value = str(value)
         return value
-    def decr(self, key, delta=1):
-        return self.incr(key, -delta)
+    def decr(cache, key, delta=1):
+        return cache.incr(key, -delta)
     @with_lock
-    def append(self, key, value):
+    def append(cache, key, value):
         key, value, _ = normalize(key, value)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None: return False
-        self._place_on_top(node)
+        cache._place_on_top(node)
         if node.value is None: return False
-        self._data_size += len(value)
+        cache._data_size += len(value)
         node.value += value
-        self._delete_expired_nodes()
-        self._conform_to_limits()
+        cache._delete_expired_nodes()
+        cache._conform_to_limits()
         return True
     @with_lock
-    def prepend(self, key, value):
+    def prepend(cache, key, value):
         key, value, _ = normalize(key, value)
-        node = self._find_node(key)
+        node = cache._find_node(key)
         if node is None: return False
-        self._place_on_top(node)
+        cache._place_on_top(node)
         if node.value is None: return False
-        self._data_size += len(value)
+        cache._data_size += len(value)
         node.value = value + node.value
-        self._delete_expired_nodes()
-        self._conform_to_limits()
+        cache._delete_expired_nodes()
+        cache._conform_to_limits()
         return True
     @with_lock
-    def flush_all(self):
-        self._dict.clear()
-        self._heap = []
-        self._list.prev = self._list.next = self._list
-        for node in self._dict.itervalues():
+    def flush_all(cache):
+        cache._dict.clear()
+        cache._heap = []
+        cache._list.prev = cache._list.next = cache._list
+        for node in cache._dict.itervalues():
             node.prev = node.next = None
     @with_lock
-    def get_stats(self):
+    def get_stats(cache):
         now = int(gettime())
-        return dict(curr_items=len(self._dict), total_items=self._stat_total_items,
-                    bytes=self._data_size, limit_maxbytes=self.max_data_size,
-                    get_hits=self._stat_get_hits, get_misses=self._stat_get_misses, evictions=self._stat_evictions,
-                    oldest_item_age=int(gettime())-self._list.prev.access,
-                    cmd_get=self._stat_cmd_get, cmd_set=self._stat_cmd_set,
-                    time=now, uptime=now-self._start_time)
+        return dict(curr_items=len(cache._dict), total_items=cache._stat_total_items,
+                    bytes=cache._data_size, limit_maxbytes=cache.max_data_size,
+                    get_hits=cache._stat_get_hits, get_misses=cache._stat_get_misses, evictions=cache._stat_evictions,
+                    oldest_item_age=int(gettime())-cache._list.prev.access,
+                    cmd_get=cache._stat_cmd_get, cmd_set=cache._stat_cmd_set,
+                    time=now, uptime=now-cache._start_time)
 
 if pony.MODE.startswith('GAE-'):
     import google.appengine.api.memcache
