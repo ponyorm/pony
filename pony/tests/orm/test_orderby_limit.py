86b22bf15ab3094b2e423ca657283182eeafaa45
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-04-19 09:38:00 +0000

    Refactoring of test_orderby_limit.py
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1732 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/test_orderby_limit.py
+++ pony/tests/orm/test_orderby_limit.py
@@ -2,107 +2,103 @@ import unittest
 from pony.orm import *
 from testutils import *
 
+class Student(Entity):
+    name = Required(unicode)
+    scholarship = Optional(int)
+    group = Required(int)
+
+db = Database('sqlite', ':memory:')
+db.generate_mapping(create_tables=True)
+
+@with_transaction
+def populate_db():
+    Student.create(1, name="B", scholarship=None, group=41)
+    Student.create(2, name="C", scholarship=700, group=41)
+    Student.create(3, name="A", scholarship=500, group=42)
+    Student.create(4, name="D", scholarship=500, group=43)
+    Student.create(5, name="E", scholarship=700, group=42)
+populate_db()
+
 class TestOrderbyLimit(unittest.TestCase):
     def setUp(self):
-        _diagram_ = Diagram()
-        self.diagram = _diagram_
-        class Student(Entity):
-            name = Required(unicode)
-            scholarship = Optional(int)
-            group = Required(int)
-        self.Student = Student
-        self.db = Database('sqlite', ':memory:')
-        con = self.db.get_connection()
-        con.executescript("""
-        drop table if exists Student;
-        create table Student(
-            id integer primary key,
-            name varchar(20),
-            scholarship integer,
-            [group] integer
-        );
-        insert into Student values (1, "B", null, 41);
-        insert into Student values (2, "C", 700, 41);
-        insert into Student values (3, "A", 500, 42);
-        insert into Student values (4, "D", 500, 43);
-        insert into Student values (5, "E", 700, 42);
-        """)
-        self.db.generate_mapping(check_tables=True)
+        rollback()
+    def tearDown(self):
+        rollback()
 
     def test1(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.name))
         self.assertEqual(students, set([Student(3), Student(1), Student(2), Student(4), Student(5)]))
+        
     def test2(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.name.asc))
         self.assertEqual(students, set([Student(3), Student(1), Student(2), Student(4), Student(5)]))
+        
     def test3(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id.desc))
         self.assertEqual(students, set([Student(5), Student(4), Student(3), Student(2), Student(1)]))
+        
     def test4(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.scholarship.asc, Student.group.desc))
         self.assertEqual(students, set([Student(1), Student(4), Student(3), Student(5), Student(2)]))
+        
     def test5(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.name).limit(3))
         self.assertEqual(students, set([Student(3), Student(1), Student(2)]))
+        
     def test6(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.name).limit(3, 1))
         self.assertEqual(students, set([Student(1), Student(2), Student(4)]))
+        
     def test7(self):
-        Student = self.Student
         query = select(s for s in Student).orderby(Student.name).limit(3, 1)
         students = set(query)
         self.assertEqual(students, set([Student(1), Student(2), Student(4)]))
         students = set(query)
         self.assertEqual(students, set([Student(1), Student(2), Student(4)]))
+        
     @raises_exception(TypeError, "query.orderby() arguments must be attributes. Got: 'name'")
     def test8(self):
-        Student = self.Student
-        students = select(s for s in Student).orderby("name").fetch()     
+        students = select(s for s in Student).orderby("name").fetch()
+        
     def test9(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[1:4])
         self.assertEqual(students, set([Student(2), Student(3), Student(4)]))
+        
     def test10(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[:4])
         self.assertEqual(students, set([Student(1), Student(2), Student(3), Student(4)]))
+        
     @raises_exception(TypeError, "Parameter 'stop' of slice object should be specified")
     def test11(self):
-        Student = self.Student
         students = select(s for s in Student).orderby(Student.id)[4:]
+        
     @raises_exception(TypeError, "Parameter 'start' of slice object cannot be negative")
     def test12(self):
-        Student = self.Student
         students = select(s for s in Student).orderby(Student.id)[-3:2]
+        
     def test13(self):
-        Student = self.Student
         students = select(s for s in Student).orderby(Student.id)[3]
         self.assertEqual(students, Student(4))
+        
     @raises_exception(TypeError, "Incorrect argument type: 'a'")
     def test14(self):
-        Student = self.Student
         students = select(s for s in Student).orderby(Student.id)["a"]
+        
     def test15(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[0:4][1:3])
         self.assertEqual(students, set([Student(2), Student(3)]))
+        
     def test16(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[0:4][1:])
-        self.assertEqual(students, set([Student(2), Student(3), Student(4)]))               
+        self.assertEqual(students, set([Student(2), Student(3), Student(4)]))
+        
     def test17(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[:4][1:])
-        self.assertEqual(students, set([Student(2), Student(3), Student(4)]))               
+        self.assertEqual(students, set([Student(2), Student(3), Student(4)]))
+        
     def test18(self):
-        Student = self.Student
         students = set(select(s for s in Student).orderby(Student.id)[:])
-        self.assertEqual(students, set([Student(1), Student(2), Student(3), Student(4), Student(5)]))  
+        self.assertEqual(students, set([Student(1), Student(2), Student(3), Student(4), Student(5)]))
+        
 if __name__ == "__main__":
     unittest.main()
