c391c9ef05ceffd41bb37b037e300dd4d28f2b27
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-04-19 09:42:59 +0000

    Refactoring of test_query_set_monad.py
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1733 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/test_query_set_monad.py
+++ pony/tests/orm/test_query_set_monad.py
@@ -2,98 +2,82 @@ import unittest
 from pony.orm import *
 from testutils import *
 
+class Student(Entity):
+    name = Required(unicode)
+    group = Required('Group')
+    scholarship = Required(int, default=0)
+
+class Group(Entity):
+    id = PrimaryKey(int)
+    students = Set(Student)
+
+db = Database('sqlite', ':memory:')
+db.generate_mapping(create_tables=True)
+
+@with_transaction
+def populate_db():
+    g1 = Group.create(1)
+    g2 = Group.create(2)
+    s1 = Student.create(1, name='S1', group=g1, scholarship=0)
+    s2 = Student.create(2, name='S2', group=g1, scholarship=100)
+    s3 = Student.create(3, name='S3', group=g2, scholarship=500)
+populate_db()
+
 class TestQuerySetMonad(unittest.TestCase):
     def setUp(self):
-        _diagram_ = Diagram()
-        self.diagram = _diagram_
-        self.db = Database('sqlite', ':memory:')
-        class Student(Entity):
-            name = Required(unicode)
-            group = Required('Group')
-            scholarship = Required(int, default=0)
-        class Group(Entity):
-            id = PrimaryKey(int)
-            students = Set(Student)
-        conn = self.db.get_connection()
-        conn.executescript("""
-            drop table if exists Student;
-            create table Student(
-              id integer primary key,
-              name varchar(50) not null,
-              "group" integer not null,
-              scholarship integer default 0 
-            );
-            drop table if exists "Group";
-            create table "Group"(
-              id integer primary key
-            );            
-            insert into Student values (1, 'S1', 1, 0);
-            insert into Student values (2, 'S2', 1, 100);
-            insert into Student values (3, 'S3', 2, 500);
-            insert into "Group" values (1);
-            insert into "Group" values (2);
-        """)
-        self.db.generate_mapping(check_tables=True)
+        rollback()
+
+    def tearDown(self):
+        rollback()
+
     def test_len(self):
-        Group = self.diagram.entities["Group"]
         result = set(select(g for g in Group if len(g.students) > 1))
         self.assertEquals(result, set([Group(1)]))
+        
     def test_len2(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if len(s for s in Student if s.group == g) > 1))
         self.assertEquals(result, set([Group(1)]))
+
     def test_len3(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if len(s.name for s in Student if s.group == g) > 1))
         self.assertEquals(result, set([Group(1)]))
+
     @raises_exception(TypeError)
     def test_sum1(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if sum(s for s in Student if s.group == g) > 1))
         self.assertEquals(result, set([]))
+
     @raises_exception(TypeError)
     def test_sum2(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         select(g for g in Group if sum(s.name for s in Student if s.group == g) > 1)
+
     def test_sum3(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if sum(s.scholarship for s in Student if s.group == g) > 500))
         self.assertEquals(result, set([]))
+
     def test_min1(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if min(s.name for s in Student if s.group == g) == 'S1'))
         self.assertEquals(result, set([Group(1)]))
+
     @raises_exception(TypeError)
     def test_min2(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         select(g for g in Group if min(s for s in Student if s.group == g) == None)
+
     def test_max1(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if max(s.scholarship for s in Student if s.group == g) > 100))
         self.assertEquals(result, set([Group(2)]))
+
     @raises_exception(TypeError)    
     def test_max2(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         select(g for g in Group if max(s for s in Student if s.group == g) == None)
+
     def test_negate(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         result = set(select(g for g in Group if not(s.scholarship for s in Student if s.group == g)))
         self.assertEquals(result, set([]))
+
     def test_no_conditions(self):
-        Group = self.diagram.entities["Group"]
-        Student = self.diagram.entities["Student"]
         students = set(select(s for s in Student if s.group in (g for g in Group)))
         self.assertEqual(students, set([Student(1), Student(2), Student(3)]))    
+
 if __name__ == "__main__":
     unittest.main()
-        
