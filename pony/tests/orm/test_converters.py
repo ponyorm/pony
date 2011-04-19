ae4e41e86baac466b0ebf0a73cd63319646a12a2
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-04-19 09:10:36 +0000

    Refactoring of test_converters.py
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1729 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/test_converters.py
+++ pony/tests/orm/test_converters.py
@@ -3,97 +3,68 @@ from pony.orm import *
 from decimal import Decimal
 from datetime import date
 
+class Student(Entity):
+    name = Required(unicode)
+    scholarship = Required(Decimal, 5, 2)
+    group = Required("Group")
+    marks = Set("Mark")
+
+class Group(Entity):
+    number = PrimaryKey(int)
+    department = Required(int)
+    students = Set(Student)
+    subjects = Set("Subject")
+
+class Subject(Entity):
+    name = PrimaryKey(unicode)
+    groups = Set(Group)
+    marks = Set("Mark")
+
+class Mark(Entity):
+    value = Required(int)
+    student = Required(Student)
+    subject = Required(Subject)
+    date = Required(date)
+    PrimaryKey(student, subject)
+
+db = Database('sqlite', ':memory:')
+db.generate_mapping(create_tables=True)
+
+@with_transaction
+def populate_db():
+    Math = Subject.create("Math")
+    Physics = Subject.create("Physics")
+    History = Subject.create("History")
+
+    g41 = Group.create(41, department=101, subjects=[ Math, Physics, History ])
+    g42 = Group.create(42, department=102, subjects=[ Math, Physics ])
+    g43 = Group.create(43, department=102, subjects=[ Physics ])
+    
+    s1 = Student.create(1, name="Joe", scholarship=Decimal('99.9'), group=g41)
+    s2 = Student.create(2, name="Bob", scholarship=Decimal('100.0'), group=g41)
+    s3 = Student.create(3, name="Beth", scholarship=Decimal('500.5'), group=g41)
+    s4 = Student.create(4, name="Jon", scholarship=Decimal('500.6'), group=g42)
+    s5 = Student.create(5, name="Pete", scholarship=Decimal('700.1'), group=g42)
+
+    Mark.create(value=5, student=s1, subject=Math,    date=date(2010, 10, 01))
+    Mark.create(value=4, student=s2, subject=Physics, date=date(2010, 10, 02))
+    Mark.create(value=3, student=s2, subject=Math,    date=date(2010, 10, 03))
+    Mark.create(value=2, student=s2, subject=History, date=date(2010, 10, 04))
+    Mark.create(value=1, student=s3, subject=History, date=date(2010, 10, 05))
+    Mark.create(value=2, student=s3, subject=Math,    date=date(2010, 10, 06))
+    Mark.create(value=2, student=s4, subject=Math,    date=date(2010, 10, 07))
+populate_db()
+
 class TestConverters(unittest.TestCase):
     def setUp(self):
-        _diagram_ = Diagram()
-        self.diagram = _diagram_        
-        class Student(Entity):
-            name = Required(unicode)
-            scholarship = Required(Decimal, 5, 2)
-            group = Required("Group")
-            marks = Set("Mark")
-        class Group(Entity):
-            number = PrimaryKey(int)
-            department = Required(int)
-            students = Set(Student)
-            subjects = Set("Subject")
-        class Subject(Entity):
-            name = PrimaryKey(unicode)
-            groups = Set(Group)
-            marks = Set("Mark")
-        class Mark(Entity):
-            value = Required(int)
-            student = Required(Student)
-            subject = Required(Subject)
-            date = Required(date)
-            PrimaryKey(student, subject)
-        self.Student = Student
-        self.Group = Group
-        self.Subject = Subject
-        self.Mark = Mark
-        self.db = Database('sqlite', ':memory:')
-        con = self.db.get_connection()
-        con.executescript("""
-        drop table if exists Student;
-        create table Student(
-            id integer primary key,
-            name varchar(20),
-            scholarship integer,
-            [group] integer
-        );
-        drop table if exists [Group];
-        create table [Group](
-            number integer primary key,
-            department integer
-        );
-        drop table if exists Subject;
-        create table Subject(
-            name varchar(20) primary key
-        );
-        drop table if exists Group_Subject;
-        create table Group_Subject(
-            [group] integer,
-            subject varchar(20),
-            primary key ([group], subject)
-        );
-        drop table if exists Mark;
-        create table Mark(
-            value integer,
-            student integer,
-            subject varchar(20),
-            date datetime,
-            primary key (student, subject)
-        );
-        insert into Student values (1, "Joe", 99.9, 41);
-        insert into Student values (2, "Bob", 100.0, 41);
-        insert into Student values (3, "Beth", 500.5, 41);
-        insert into Student values (4, "Jon", 500.6, 42);
-        insert into Student values (5, "Pete", 700.1, 42);
-        insert into [Group] values (41, 101);
-        insert into [Group] values (42, 102);
-        insert into [Group] values (43, 102);
-        insert into Group_Subject values (41, "Math");
-        insert into Group_Subject values (41, "Physics");
-        insert into Group_Subject values (41, "History");
-        insert into Group_Subject values (42, "Math");
-        insert into Group_Subject values (42, "Physics");
-        insert into Group_Subject values (43, "Physics");
-        insert into Subject values ("Math");
-        insert into Subject values ("Physics");
-        insert into Subject values ("History");
-        insert into Mark values (5, 1, "Math", "2010-10-01");
-        insert into Mark values (4, 2, "Physics", "2010-10-02");
-        insert into Mark values (3, 2, "Math", "2010-10-03");
-        insert into Mark values (2, 2, "History", "2010-10-04");
-        insert into Mark values (1, 3, "History", "2010-10-05");
-        insert into Mark values (2, 3, "Math", "2010-10-06");
-        insert into Mark values (2, 4, "Math", "2010-10-07");
-        """)
-        self.db.generate_mapping(check_tables=True)
+        rollback()
+    def tearDown(self):
+        rollback()
+        
     def test1(self):
-        Student = self.Student
         result = set(select(s.scholarship for s in Student if min(s.marks.value) < 2))
         self.assertEquals(result, set([Decimal("500.5")]))
+
 ##    def test2(self):
 ##        Group = self.Group
 ##        Student = self.Student
