bc41fde99063e83bfe1bba7d7946305c99af6c30
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2011-05-15 11:28:16 +0000

    Improved @raises_exception decorator (does not check exception text if 'msg' decorator parameter is not set)
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@1776 698dff77-f410-0410-8918-cf622e150f36

--- pony/tests/orm/testutils.py
+++ pony/tests/orm/testutils.py
@@ -6,9 +6,8 @@ def raises_exception(exc_class, msg=None):
                 self.assert_(False, "expected exception %s wasn't raised" % exc_class.__name__)
             except exc_class, e:
                 if not e.args: self.assertEqual(msg, None)
-                else:                    
-                    self.assertEqual(e.args[0], msg, "incorrect exception message. expected '%s', got '%s'"
-                    % (msg, e.args[0]))
+                elif msg is not None:
+                    self.assertEqual(e.args[0], msg, "incorrect exception message. expected '%s', got '%s'" % (msg, e.args[0]))
         wrapper.__name__ = func.__name__
         return wrapper
     return decorator
