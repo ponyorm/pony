3aa7b112f0f54016a70b44e02841cb94057a7136
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2012-09-20 12:38:34 +0000

    replace "assert False" to throw NotImplementedError('Expression is too complex to decompile, try to pass query as string, e.g. fetch("x for x in Something")')
    git-svn-id: http://pony.tigris.org/svn/pony/trunk@2032 698dff77-f410-0410-8918-cf622e150f36

--- pony/decompiling.py
+++ pony/decompiling.py
@@ -239,7 +239,7 @@ class Decompiler(object):
                 if hasattr(top, 'endpos'):
                     top2.endpos = top.endpos
                     if decompiler.targets.get(top.endpos) is top: decompiler.targets[top.endpos] = top2
-            else: assert False
+            else: throw(NotImplementedError('Expression is too complex to decompile, try to pass query as string, e.g. fetch("x for x in Something")'))
             top2.endpos = max(top2.endpos, getattr(top, 'endpos', 0))
             top = decompiler.stack.pop()
         decompiler.stack.append(top)
