718f2d627693e23edbb4bfbe1be1ab21314a9f6d
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2013-02-24 19:06:28 +0400

    eStore example added

---
+++ pony/orm/examples/estore.py
@@ -0,0 +1,52 @@
+from decimal import Decimal
+from datetime import datetime
+from pony.orm import *
+
+db = Database("sqlite", "estore.sqlite", create_db=True)
+
+class Customer(db.Entity):
+    email = Unique(unicode)
+    password = Required(unicode)
+    name = Required(unicode)
+    address = Required(unicode)
+    cart_items = Set("CartItem")
+    orders = Set("Order")
+
+class Product(db.Entity):
+    id = PrimaryKey(int)
+    name = Required(unicode)
+    categories = Set("Category")
+    description = Optional(unicode)
+    picture = Optional(buffer)
+    price = Required(Decimal)
+    quantity = Required(int)
+    car_items = Set("CartItem")
+    order_items = Set("OrderItem")
+
+class CartItem(db.Entity):
+    quantity = Required(int)
+    customer = Required(Customer)
+    product = Required(Product)
+
+class OrderItem(db.Entity):
+    quantity = Required(int)
+    item_price = Required(Decimal)
+    order = Required("Order")
+    product = Required(Product)
+    PrimaryKey(order, product)
+
+class Order(db.Entity):
+    id = PrimaryKey(int)
+    state = Required(unicode)
+    date_created = Required(datetime)
+    date_shipped = Optional(datetime)
+    date_delivered = Optional(datetime)
+    total_price = Required(Decimal)
+    customer = Required(Customer)
+    order_items = Set(OrderItem)
+
+class Category(db.Entity):
+    name = Required(unicode)
+    products = Set(Product)
+
+db.generate_mapping(create_tables=True)
