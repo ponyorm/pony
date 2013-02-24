c7e27b31ec75783d37aa3ee359b1831a3d59410b
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2013-02-24 23:17:48 +0400

    Fixed imports

--- pony/main.py
+++ pony/main.py
@@ -21,7 +21,4 @@ except ImportError: pass  # may happen if pony.options.log_to_sqlite = False
 import pony.layouts.blueprint
 import pony.images
 
-from pony.orm import (
-    Database, RowNotFound, MultipleRowsFound,
-    Entity, Optional, Required, Unique, PrimaryKey, Set
-    )
+from pony.orm import *
