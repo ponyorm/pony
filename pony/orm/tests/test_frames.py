ponyorm/pony

commit 227727a9717a46642f4d78211a5a4d747a2eabec

Improved decorators

pony/orm/core.py
@@ -20,9 +20,8 @@ from pony.orm.dbapiprovider import (
     OperationalError, IntegrityError, InternalError, ProgrammingError, NotSupportedError
     )
 from pony.utils import (
-    localbase, simple_decorator, cut_traceback, throw, deprecated,
+    localbase, decorator, cut_traceback, throw, deprecated,
     import_module, parse_expr, is_ident, count, avg as _avg, tostring, strjoin,
-    copy_func_attrs
     )
 
 __all__ = '''
@@ -3607,15 +3606,15 @@ class DBSessionContextManager(object):
         if len(args) == 1:
             if kwargs: throw(TypeError)
             self.is_decorator = True
-            old_func = args[0]
-            def new_func(*args, **kwargs):
+            func = args[0]
+            def new_func(func, *args, **kwargs):
                 for i in xrange(self.retry):
                     try:
-                        with self: return old_func(*args, **kwargs)
+                        with self: return func(*args, **kwargs)
                     except Exception, e:
                         if not isinstance(e, self.retry_exceptions): raise
                 raise
-            return copy_func_attrs(new_func, old_func, 'db_session')
+            return decorator(new_func, func)
         return self.__class__(**kwargs)
     def __enter__(self):
         if not self.is_decorator and self.retry != 1: throw(TypeError,
@@ -3642,7 +3641,7 @@ def with_transaction(*args, **kwargs):
     deprecated(3, "@with_transaction decorator is deprecated, use @db_session decorator instead")
     return db_session(*args, **kwargs)
 
-@simple_decorator
+@decorator
 def db_decorator(func, *args, **kwargs):
     web = sys.modules.get('pony.web')
     allowed_exceptions = web and [ web.HttpRedirect ] or []
@@ -3680,8 +3679,8 @@ def select(gen, frame_depth=0, left_join=False):
         tree = string2ast(query_string)
         if not isinstance(tree, ast.GenExpr): throw(TypeError)
         code_key = query_string
-        globals = sys._getframe(frame_depth+2).f_globals
-        locals = sys._getframe(frame_depth+2).f_locals
+        globals = sys._getframe(frame_depth+3).f_globals
+        locals = sys._getframe(frame_depth+3).f_locals
     else: throw(TypeError)
     return Query(code_key, tree.code, globals, locals, left_join)
 

pony/auth.py
@@ -9,7 +9,7 @@ from pony.thirdparty import simplejson
 
 import pony
 from pony import options, httputils
-from pony.utils import compress, decompress, simple_decorator, localbase
+from pony.utils import compress, decompress, decorator, localbase
 from pony.logging2 import log_exc
 
 hash = pony.options.HASH_ALGORITHM
@@ -279,14 +279,14 @@ if not pony.MODE.startswith('GAE-'):
     from Queue import Queue
     queue = Queue()
 
-    @simple_decorator
+    @decorator
     def exec_in_auth_thread(f, *args, **kwargs):
         result_holder = []
         queue.put((local.lock, f, args, kwargs, result_holder))
         local.lock.acquire()
         return result_holder[0]
 
-    @simple_decorator
+    @decorator
     def exec_async(f, *args, **kwargs):
         queue.put((None, f, args, kwargs, None))
 

pony/caching.py
@@ -55,7 +55,7 @@ from weakref import ref
 
 import pony
 from pony import options
-from pony.utils import simple_decorator
+from pony.utils import decorator
 
 class Node(object):
     __slots__ = 'prev', 'next', 'key', 'value', 'expire', 'access', '__weakref__'
@@ -83,7 +83,7 @@ def normalize(key, value="", expire=None):
         elif expire <= MONTH * 100: raise ValueError('Invalid expire value: %d' % expire)
     return key, value, expire
 
-@simple_decorator
+@decorator
 def with_lock(func, cache, *args, **kwargs):
     cache._lock.acquire()
     try: return func(cache, *args, **kwargs)

pony/compression.py
@@ -4,7 +4,7 @@ from gzip import GzipFile
 from zlib import compress
 
 import pony
-from pony.utils import simple_decorator
+from pony.utils import decorator
 
 compressable_mime_types = set("""
     application/javascript
@@ -19,7 +19,7 @@ compressable_mime_types = set("""
 
 min_compressed_length = 500
 
-@simple_decorator
+@decorator
 def compression_middleware(app, environ):
     status, headers, content = app(environ)
 

pony/debugging.py
@@ -6,7 +6,7 @@ from urllib import unquote_plus
 
 import pony
 from pony import options, utils, httputils
-from pony.utils import detect_source_encoding, is_ident, tostring, pos2lineno, getlines, getlines2, simple_decorator
+from pony.utils import detect_source_encoding, is_ident, tostring, pos2lineno, getlines, getlines2, decorator
 from pony.templating import html, cycle, htmljoin, Html, StrHtml, ParseError
 
 def restore_escapes(s):
@@ -248,7 +248,7 @@ else:
 
     import bdb
 
-    @simple_decorator
+    @decorator
     def debugging_decorator(func, *args, **kwargs):
         if options.DEBUG:
             web = sys.modules.get('pony.web')
@@ -267,7 +267,7 @@ else:
     debug_re = re.compile(r'(?:(?<=\?|&)|^)debug(?:=([^&]*))?&?')
     expr_re = re.compile(r'(?:(?<=\?|&)|^)expr(?:=([^&]*))?&?')
 
-    @simple_decorator
+    @decorator
     def debugging_middleware(app, environ):
         if not options.DEBUG: return app(environ)
         query = environ.get('QUERY_STRING', '')

pony/forms.py
@@ -25,19 +25,17 @@ class FormMeta(type):
         return super(FormMeta, meta).__new__(meta, name, bases, dict)
 
 @decorator
-def _form_init_decorator(__init__):
-    def new_init(form, *args, **kwargs):
-        try: init_counter = form._init_counter
-        except AttributeError:
-            if form.__class__ is not Form: Form.__init__.original_func(form)
-            object.__setattr__(form, '_init_args', cPickle.dumps((args, kwargs), 2))
-            object.__setattr__(form, '_init_counter', 1)
-        else: object.__setattr__(form, '_init_counter', init_counter+1)
-        try: __init__(form, *args, **kwargs)
-        finally:
-            init_counter = form._init_counter
-            object.__setattr__(form, '_init_counter', init_counter-1)
-    return new_init
+def _form_init_decorator(__init__, form, *args, **kwargs):
+    try: init_counter = form._init_counter
+    except AttributeError:
+        if form.__class__ is not Form: Form.__init__.original_func(form)
+        object.__setattr__(form, '_init_args', cPickle.dumps((args, kwargs), 2))
+        object.__setattr__(form, '_init_counter', 1)
+    else: object.__setattr__(form, '_init_counter', init_counter+1)
+    try: __init__(form, *args, **kwargs)
+    finally:
+        init_counter = form._init_counter
+        object.__setattr__(form, '_init_counter', init_counter-1)
 
 http_303_incompatible_browsers = []
 

pony/orm/asttranslation.py
@@ -1,6 +1,7 @@
 from compiler import ast
+from functools import update_wrapper
 
-from pony.utils import copy_func_attrs, throw
+from pony.utils import throw
 
 class TranslationError(Exception): pass
 
@@ -42,7 +43,7 @@ def priority(p):
             for child in node.getChildNodes():
                 if getattr(child, 'priority', 0) >= p: child.src = '(%s)' % child.src
             return func(translator, node)
-        return copy_func_attrs(new_func, func)
+        return update_wrapper(new_func, func)
     return decorator
 
 def binop_src(op, node):

pony/orm/dbapiprovider.py
@@ -3,7 +3,7 @@ from datetime import datetime, date, time
 from uuid import uuid4, UUID
 import re
 
-from pony.utils import is_utf8, simple_decorator, throw, localbase
+from pony.utils import is_utf8, decorator, throw, localbase
 from pony.converting import str2date, str2datetime
 from pony.orm.ormtypes import LongStr, LongUnicode
 
@@ -36,7 +36,7 @@ class     InternalError(DatabaseError): pass
 class     ProgrammingError(DatabaseError): pass
 class     NotSupportedError(DatabaseError): pass
 
-@simple_decorator
+@decorator
 def wrap_dbapi_exceptions(func, provider, *args, **kwargs):
     dbapi_module = provider.dbapi_module
     try: return func(provider, *args, **kwargs)

commit 707c7ddc0e5ecd5e6f1504af603d4d43cafaceaa

Fixes #30 issue caused by commit 227727a9 (improved decorators)

pony/orm/core.py
@@ -483,7 +483,7 @@ class Database(object):
     @cut_traceback
     def select(database, sql, globals=None, locals=None, frame_depth=0):
         if not select_re.match(sql): sql = 'select ' + sql
-        cursor = database._exec_raw_sql(sql, globals, locals, frame_depth + 2)
+        cursor = database._exec_raw_sql(sql, globals, locals, frame_depth + 3)
         max_fetch_count = options.MAX_FETCH_COUNT
         if max_fetch_count is not None:
             result = cursor.fetchmany(max_fetch_count)
@@ -501,7 +501,7 @@ class Database(object):
         return result
     @cut_traceback
     def get(database, sql, globals=None, locals=None):
-        rows = database.select(sql, globals, locals, 2)
+        rows = database.select(sql, globals, locals, frame_depth=3)
         if not rows: throw(RowNotFound)
         if len(rows) > 1: throw(MultipleRowsFound)
         row = rows[0]

pony/orm/tests/test_frames.py
@@ -129,5 +129,17 @@ class TestFrames(unittest.TestCase):
         result = Person.select('lambda p: p.age > x').filter('p.name.startswith(y)')[:]
         self.assertEqual(result, [Person[3]])
 
+    @db_session
+    def test_db_select(self):
+        x = 20
+        result = db.select('name from Person where age > $x order by name')
+        self.assertEqual(result, ['John', 'Mike'])
+
+    @db_session
+    def test_db_get(self):
+        x = 18
+        result = db.get('name from Person where age = $x')
+        self.assertEqual(result, 'Mary')
+
 if __name__ == '__main__':
     unittest.main()