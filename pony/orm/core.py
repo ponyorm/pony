52c88b6e5b702b44687e9c713506784809dd2321
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2014-01-22 12:57:50 +0400

    Forcing of n+1 query optimization during cascade delete

--- pony/orm/core.py
+++ pony/orm/core.py
@@ -1557,7 +1557,8 @@ class Set(Collection):
 
         counter = cache.collection_statistics.setdefault(attr, 0)
         nplus1_threshold = attr.nplus1_threshold
-        prefetching = not attr.lazy and nplus1_threshold is not None and counter >= nplus1_threshold
+        prefetching = not attr.lazy and nplus1_threshold is not None \
+                      and (counter >= nplus1_threshold or cache.noflush_counter)
 
         if items:
             if not reverse.is_collection:
