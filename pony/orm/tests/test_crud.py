6131a1d5b2c12b7b70bcc474233457bbb0749615
Alexey Malashkevich <m.alexey@gmail.com>
2014-05-24 20:08:59 +0400

    Update tests

--- pony/orm/tests/test_crud.py
+++ pony/orm/tests/test_crud.py
@@ -15,16 +15,29 @@ class Student(db.Entity):
     name = Required(unicode)
     scholarship = Required(Decimal, default=0)
     picture = Optional(buffer, lazy=True)
+    email = Required(unicode, unique=True)
+    phone = Optional(unicode, unique=True)
+    courses = Set('Course')
     group = Optional('Group')
+
+class Course(db.Entity):
+    name = Required(unicode)
+    semester = Required(int)
+    students = Set(Student)
+    composite_key(name, semester)
 
 db.generate_mapping(create_tables=True)
 
 with db_session:
     g1 = Group(id=1, major='Math')
     g2 = Group(id=2, major='Physics')
-    s1 = Student(id=1, name='S1', group=g1)
-    s2 = Student(id=2, name='S2', group=g1)
-    s3 = Student(id=3, name='S3', group=g2)
+    s1 = Student(id=1, name='S1', email='s1@example.com', group=g1)
+    s2 = Student(id=2, name='S2', email='s2@example.com', group=g1)
+    s3 = Student(id=3, name='S3', email='s3@example.com', group=g2)
+    c1 = Course(name='Math', semester=1)
+    c2 = Course(name='Math', semester=2)
+    c3 = Course(name='Physics', semester=1)
+
 
 class TestCRUD(unittest.TestCase):
     def setUp(self):
@@ -43,3 +56,9 @@ class TestCRUD(unittest.TestCase):
         s3 = Student[3]
         g1.set(students=[s3])
         self.assertEquals(s3.group, Group[1])
+    def test_set3(self):
+        c1 = Course[1]
+        c1.set(name='Algebra', semester=3)
+    def test_set4(self):
+        s1 = Student[1]
+        s1.set(name='New name', email='new_email@example.com')
