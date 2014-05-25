650d226e665c75e7b1b537d836f3c54d72b0b7e4
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2014-05-25 20:50:13 +0400

    Change Pony ORM version: 0.5-beta -> 0.5rc1

--- pony/__init__.py
+++ pony/__init__.py
@@ -2,7 +2,7 @@ import sys, time, threading, random
 from os.path import dirname
 from itertools import count
 
-__version__ = '0.5-beta'
+__version__ = '0.5rc1'
 
 uid = str(random.randint(1, 1000000))
 
