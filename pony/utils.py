520e236fea2e9407fa5dccb44dab5423422bfc6b
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2014-07-28 14:24:08 +0400

    cleanup of pony.utils

--- pony/utils.py
+++ pony/utils.py
@@ -1,21 +1,15 @@
-#coding: cp1251
-
 from __future__ import absolute_import, print_function
 from pony.py23compat import imap
 
-import re, os, os.path, sys, datetime, types, linecache, warnings, json
+import re, os.path, sys, types, warnings
 
+from datetime import datetime
 from itertools import count as _count
-from inspect import isfunction, ismethod, getargspec
+from inspect import isfunction, getargspec
 from time import strptime
-from os import urandom
-from codecs import BOM_UTF8, BOM_LE, BOM_BE
-from locale import getpreferredencoding
-from bisect import bisect
 from collections import defaultdict
 from copy import deepcopy, _deepcopy_dispatch
 from functools import update_wrapper
-from pony.thirdparty.compiler import ast
 from xml.etree import cElementTree
 
 # deepcopy instance method patch for Python < 2.7:
@@ -27,6 +21,7 @@ if types.MethodType not in _deepcopy_dispatch:
 import pony
 from pony import options
 
+from pony.thirdparty.compiler import ast
 from pony.thirdparty.decorator import decorator as _decorator
 
 if pony.MODE.startswith('GAE-'): localbase = object
@@ -55,24 +50,6 @@ def decorator(caller, func=None):
     if isfunction(caller):
         update_wrapper(new_decorator, caller)
     return new_decorator
-
-##def simple_decorator(dec):
-##    def new_dec(func):
-##        def pony_wrapper(*args, **kwargs):
-##            return dec(func, *args, **kwargs)
-##        return copy_func_attrs(pony_wrapper, func, dec.__name__)
-##    return copy_func_attrs(new_dec, dec, 'simple_decorator')
-
-##@simple_decorator
-##def decorator_with_params(dec, *args, **kwargs):
-##    if len(args) == 1 and not kwargs:
-##        func = args[0]
-##        new_func = dec(func)
-##        return copy_func_attrs(new_func, func, dec.__name__)
-##    def parameterized_decorator(old_func):
-##        new_func = dec(func, *args, **kwargs)
-##        return copy_func_attrs(new_func, func, dec.__name__)
-##    return parameterized_decorator
 
 def decorator_with_params(dec):
     def parameterized_decorator(*args, **kwargs):
@@ -135,17 +112,6 @@ def get_lambda_args(func):
     if defaults: throw(TypeError, 'Defaults are not supported')
     lambda_args_cache[func] = names
     return names
-
-_cache = {}
-MAX_CACHE_SIZE = 1000
-
-@decorator
-def cached(f, *args, **kwargs):
-    key = (f, args, tuple(sorted(kwargs.items())))
-    value = _cache.get(key)
-    if value is not None: return value
-    if len(_cache) == MAX_CACHE_SIZE: _cache.clear()
-    return _cache.setdefault(key, f(*args, **kwargs))
 
 def error_method(*args, **kwargs):
     raise TypeError
@@ -218,62 +184,8 @@ def absolutize_path(filename, frame_depth):
     code_path = os.path.dirname(code_filename)
     return os.path.join(code_path, filename)
 
-def shortened_filename(filename):
-    if pony.MAIN_DIR is None: return filename
-    maindir = pony.MAIN_DIR + os.sep
-    if filename.startswith(maindir): return filename[len(maindir):]
-    return filename
-
-def get_mtime(filename):
-    stat = os.stat(filename)
-    mtime = stat.st_mtime
-    if sys.platform == "win32": mtime -= stat.st_ctime
-    return mtime
-
-coding_re = re.compile(r'coding[:=]\s*([-\w.]+)')
-
-def detect_source_encoding(filename):
-    for i, line in enumerate(linecache.getlines(filename)):
-        if i == 0 and line.startswith(BOM_UTF8): return 'utf-8'
-        if not line.lstrip().startswith('#'): continue
-        match = coding_re.search(line)
-        if match is not None: return match.group(1)
-    else: return options.SOURCE_ENCODING or getpreferredencoding()
-
-escape_re = re.compile(r'''
-    (?<!\\)\\         # single backslash
-    (?:
-        x[0-9a-f]{2}  # byte escaping
-    |   u[0-9a-f]{4}  # unicode escaping
-    |   U[0-9a-f]{8}  # long unicode escaping
-    )
-    ''', re.VERBOSE)
-
-def restore_escapes(s, console_encoding=None, source_encoding=None):
-    if not options.RESTORE_ESCAPES: return s
-    if source_encoding is None:
-        source_encoding = options.SOURCE_ENCODING or getpreferredencoding()
-    if console_encoding is None:
-        try: console_encoding = getattr(sys.stderr, 'encoding', None)
-        except: console_encoding = None  # workaround for PythonWin win32ui.error "The MFC object has died."
-        console_encoding = console_encoding or options.CONSOLE_ENCODING
-        console_encoding = console_encoding or getpreferredencoding()
-    try: s = s.decode(source_encoding).encode(console_encoding)
-    except (UnicodeDecodeError, UnicodeEncodeError): pass
-    def f(match):
-        esc = match.group()
-        code = int(esc[2:], 16)
-        if esc.startswith('\\x'):
-            if code < 32: return esc
-            try: return chr(code).decode(source_encoding).encode(console_encoding)
-            except (UnicodeDecodeError, UnicodeEncodeError): return esc
-        char = unichr(code)
-        try: return char.encode(console_encoding)
-        except UnicodeEncodeError: return esc
-    return escape_re.sub(f, s)
-
 def current_timestamp():
-    return datetime2timestamp(datetime.datetime.now())
+    return datetime2timestamp(datetime.now())
 
 def datetime2timestamp(d):
     result = d.isoformat(' ')
@@ -283,72 +195,7 @@ def datetime2timestamp(d):
 def timestamp2datetime(t):
     time_tuple = strptime(t[:19], '%Y-%m-%d %H:%M:%S')
     microseconds = int((t[20:26] + '000000')[:6])
-    return datetime.datetime(*(time_tuple[:6] + (microseconds,)))
-
-def read_text_file(fname, encoding=None):
-    text = file(fname).read()
-    for bom, enc in [ (BOM_UTF8, 'utf8'), (BOM_LE, 'utf-16le'), (BOM_BE, 'utf-16be') ]:
-        if text[:len(bom)] == bom: return text[len(bom):].decode(enc)
-    try: return text.decode('utf8')
-    except UnicodeDecodeError:
-        try: return text.decode(encoding or getpreferredencoding())
-        except UnicodeDecodeError:
-            return text.decode('ascii', 'replace')
-
-def compress(s):
-    zipped = s.encode('zip')
-    if len(zipped) < len(s): return 'Z' + zipped
-    return 'N' + s
-
-def decompress(s):
-    first = s[0]
-    if first == 'N': return s[1:]
-    elif first == 'Z': return s[1:].decode('zip')
-    raise ValueError('Incorrect data')
-
-nbsp_re = re.compile(ur"\s+(и|с|в|от)\s+")
-
-def markdown(s):
-    from pony.templating import Html, quote
-    from pony.thirdparty.markdown import markdown
-    s = quote(s)[:]
-    result = markdown(s, html4tags=True)
-    result = nbsp_re.sub(r" \1&nbsp;", result)
-    return Html(result)
-
-class JsonString(unicode): pass
-
-def json_result(obj, **kwargs):
-    result = JsonString(json.dumps(obj, **kwargs))
-    result.media_type = 'application/json'
-    if 'encoding' in kwargs: result.charset = kwargs['encoding']
-    return result
-
-def new_guid():
-    'new_guid() -> new_binary_guid'
-    return buffer(urandom(16))
-
-def guid2str(guid):
-    """guid_binary2str(binary_guid) -> string_guid
-
-    >>> guid2str(unxehlify('ff19966f868b11d0b42d00c04fc964ff'))
-    '6F9619FF-8B86-D011-B42D-00C04FC964FF'
-    """
-    assert isinstance(guid, buffer) and len(guid) == 16
-    guid = str(guid)
-    return '%s-%s-%s-%s-%s' % tuple(imap(hexlify, (
-        guid[3::-1], guid[5:3:-1], guid[7:5:-1], guid[8:10], guid[10:])))
-
-def str2guid(s):
-    """guid_str2binary(str_guid) -> binary_guid
-
-    >>> unhexlify(str2guid('6F9619FF-8B86-D011-B42D-00C04FC964FF'))
-    'ff19966f868b11d0b42d00c04fc964ff'
-    """
-    assert isinstance(s, basestring) and len(s) == 36
-    a, b, c, d, e = imap(unhexlify, (s[:8],s[9:13],s[14:18],s[19:23],s[24:]))
-    reverse = slice(-1, None, -1)
-    return buffer(''.join((a[reverse], b[reverse], c[reverse], d, e)))
+    return datetime(*(time_tuple[:6] + (microseconds,)))
 
 expr1_re = re.compile(r'''
         ([A-Za-z_]\w*)  # identifier (group 1)
@@ -429,47 +276,6 @@ def strjoin(sep, strings, source_encoding='ascii', dest_encoding=None):
     result = sep.join(strings)
     if dest_encoding is None: return result
     return result.encode(dest_encoding, replace)
-
-def make_offsets(s):
-    offsets = [ 0 ]
-    si = -1
-    try:
-        while True:
-            si = s.index('\n', si + 1)
-            offsets.append(si + 1)
-    except ValueError: pass
-    offsets.append(len(s))
-    return offsets
-
-def pos2lineno(pos, offsets):
-    line = bisect(offsets, pos, 0, len(offsets)-1)
-    if line == 1: offset = pos
-    else: offset = pos - offsets[line - 1]
-    return line, offset
-
-def getline(text, offsets, lineno):
-    return text[offsets[lineno-1]:offsets[lineno]]
-
-def getlines(text, offsets, lineno, context=1):
-    if context <= 0: return [], None
-    start = max(0, lineno - 1 - context//2)
-    end = min(len(offsets)-1, start + context)
-    start = max(0, end - context)
-    lines = []
-    for i in range(start, end): lines.append(text[offsets[i]:offsets[i+1]])
-    index = lineno - 1 - start
-    return lines, index
-
-def getlines2(filename, lineno, context=1):
-    if context <= 0: return [], None
-    lines = linecache.getlines(filename)
-    if not lines: return [], None
-    start = max(0, lineno - 1 - context//2)
-    end = min(len(lines), start + context)
-    start = max(0, end - context)
-    lines = lines[start:start+context]
-    index = lineno - 1 - start
-    return lines, index
 
 def count(*args, **kwargs):
     if kwargs: return _count(*args, **kwargs)
