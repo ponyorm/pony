d1f1178445c2b721b131abc597a04229dc5d88d4
Alexey Malashkevich <m.alexey@gmail.com>
2014-09-30 16:56:38 +0400

    Small refactoring

--- pony/py23compat.py
+++ pony/py23compat.py
@@ -5,28 +5,24 @@ PY2 = sys.version_info[0] == 2
 if PY2:
     from future_builtins import zip as izip, map as imap
     xrange = xrange
-else:
-    izip, imap, xrange = zip, map, range
+    basestring = basestring
+    cmp = cmp
 
-if PY2:
     def iteritems(dict):
         return dict.iteritems()
 
     def itervalues(dict):
         return dict.itervalues()
 
-    cmp = cmp
-
-    basestring = basestring
-
 else:
+    izip, imap, xrange = zip, map, range
+    basestring = str
+
+    def cmp(a, b):
+        return (a > b) - (a < b)
+
     def iteritems(dict):
         return iter(dict.items())
 
     def itervalues(dict):
         return iter(dict.values())
-
-    def cmp(a, b):
-        return (a > b) - (a < b)
-
-    basestring = str
