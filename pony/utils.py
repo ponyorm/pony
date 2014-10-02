6bd8dd66ea60f5b6bc3f14fa6e05e8822e64a4b4
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2014-10-02 13:18:41 +0400

    Remove obsolete code

--- pony/utils.py
+++ pony/utils.py
@@ -315,16 +315,6 @@ def decompress(s):
     elif first == 'Z': return s[1:].decode('zip')
     raise ValueError('Incorrect data')
 
-nbsp_re = re.compile(ur"\s+(и|с|в|от)\s+")
-
-def markdown(s):
-    from pony.templating import Html, quote
-    from pony.thirdparty.markdown import markdown
-    s = quote(s)[:]
-    result = markdown(s, html4tags=True)
-    result = nbsp_re.sub(r" \1&nbsp;", result)
-    return Html(result)
-
 class JsonString(unicode): pass
 
 def json_result(obj, **kwargs):
@@ -332,32 +322,6 @@ def json_result(obj, **kwargs):
     result.media_type = 'application/json'
     if 'encoding' in kwargs: result.charset = kwargs['encoding']
     return result
-
-def new_guid():
-    'new_guid() -> new_binary_guid'
-    return buffer(urandom(16))
-
-def guid2str(guid):
-    """guid_binary2str(binary_guid) -> string_guid
-
-    >>> guid2str(unxehlify('ff19966f868b11d0b42d00c04fc964ff'))
-    '6F9619FF-8B86-D011-B42D-00C04FC964FF'
-    """
-    assert isinstance(guid, buffer) and len(guid) == 16
-    guid = str(guid)
-    return '%s-%s-%s-%s-%s' % tuple(imap(hexlify, (
-        guid[3::-1], guid[5:3:-1], guid[7:5:-1], guid[8:10], guid[10:])))
-
-def str2guid(s):
-    """guid_str2binary(str_guid) -> binary_guid
-
-    >>> unhexlify(str2guid('6F9619FF-8B86-D011-B42D-00C04FC964FF'))
-    'ff19966f868b11d0b42d00c04fc964ff'
-    """
-    assert isinstance(s, basestring) and len(s) == 36
-    a, b, c, d, e = imap(unhexlify, (s[:8],s[9:13],s[14:18],s[19:23],s[24:]))
-    reverse = slice(-1, None, -1)
-    return buffer(''.join((a[reverse], b[reverse], c[reverse], d, e)))
 
 expr1_re = re.compile(r'''
         ([A-Za-z_]\w*)  # identifier (group 1)
