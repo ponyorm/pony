ponyorm/pony

commit ab6f977b5ed89c011137d1ae590d497f02c50426

An optional real_type parameter added to raw_sql()
Conflicts:
	pony/orm/tests/test_raw_sql.py

Conflicts:
	pony/orm/tests/test_raw_sql.py

pony/orm/sqltranslation.py
@@ -1054,7 +1054,9 @@ class Monad(with_metaclass(MonadMeta)):
 
 class RawSQLMonad(Monad):
     def __init__(monad, translator, rawtype, varkey):
-        Monad.__init__(monad, translator, rawtype)
+        if rawtype.result_type is None: type = rawtype
+        else: type = normalize_type(rawtype.result_type)
+        Monad.__init__(monad, translator, type)
         monad.rawtype = rawtype
         monad.varkey = varkey
     def contains(monad, item, not_in=False):

pony/orm/core.py
@@ -4803,10 +4803,10 @@ def desc(expr):
         return 'desc(%s)' % expr
     return expr
 
-def raw_sql(sql):
+def raw_sql(sql, result_type=None):
     globals = sys._getframe(1).f_globals
     locals = sys._getframe(1).f_locals
-    return RawSQL(sql, globals, locals)
+    return RawSQL(sql, globals, locals, result_type)
 
 def extract_vars(extractors, globals, locals, cells=None):
     if cells:

pony/orm/ormtypes.py
@@ -98,21 +98,23 @@ def parse_raw_sql(sql):
 class RawSQL(object):
     def __deepcopy__(self, memo):
         assert False  # should not attempt to deepcopy RawSQL instances, because of locals/globals
-    def __init__(self, sql, globals=None, locals=None):
+    def __init__(self, sql, globals=None, locals=None, result_type=None):
         self.sql = sql
         self.items, self.codes = parse_raw_sql(sql)
         self.values = tuple(eval(code, globals, locals) for code in self.codes)
         self.types = tuple(get_normalized_type_of(value) for value in self.values)
+        self.result_type = result_type
     def _get_type_(self):
-        return RawSQLType(self.sql, self.items, self.types)
+        return RawSQLType(self.sql, self.items, self.types, self.result_type)
 
 class RawSQLType(object):
     def __deepcopy__(self, memo):
         return self  # RawSQLType instances are "immutable"
-    def __init__(self, sql, items, types):
+    def __init__(self, sql, items, types, result_type):
         self.sql = sql
         self.items = items
         self.types = types
+        self.result_type = result_type
     def __hash__(self):
         return hash(self.sql) ^ hash(self.types)
     def __eq__(self, other):

pony/orm/tests/test_raw_sql.py
@@ -92,27 +92,33 @@ class TestRawSQL(unittest.TestCase):
 
     @db_session
     def test_11(self):
+        # it is possible to specify raw_sql type manually
+        dates = select(raw_sql('(p.dob)', result_type=date) for p in Person).order_by(lambda: p.id)[:]
+        self.assertEqual(dates, [date(1985, 1, 1), date(1983, 5, 20), date(1995, 2, 15)])
+
+    @db_session
+    def test_12(self):
         # raw_sql can be used in lambdas
         x = 25
         persons = Person.select(lambda p: p.age > raw_sql('$x'))[:]
         self.assertEqual(set(persons), set([Person[1], Person[2]]))
 
     @db_session
-    def test_12(self):
+    def test_13(self):
         # raw_sql in filter()
         x = 25
         persons = select(p for p in Person).filter(lambda p: p.age > raw_sql('$x'))[:]
         self.assertEqual(set(persons), set([Person[1], Person[2]]))
 
     @db_session
-    def test_13(self):
+    def test_14(self):
         # raw_sql in filter() without using lambda
         x = 25
         persons = Person.select().filter(raw_sql('p.age > $x'))[:]
         self.assertEqual(set(persons), set([Person[1], Person[2]]))
 
     @db_session
-    def test_14(self):
+    def test_15(self):
         # several raw_sql expressions in a single query
         x = '123'
         y = 'John'
@@ -120,7 +126,7 @@ class TestRawSQL(unittest.TestCase):
         self.assertEqual(set(persons), set([Person[1]]))
 
     @db_session
-    def test_15(self):
+    def test_16(self):
         # the same param name can be used several times with different types & values
         x = 10
         y = 31
@@ -132,14 +138,14 @@ class TestRawSQL(unittest.TestCase):
         self.assertEqual(set(persons), set([Person[1]]))
 
     @db_session
-    def test_16(self):
+    def test_17(self):
         # raw_sql in order_by() section
         x = 9
         persons = Person.select().order_by(lambda p: raw_sql('SUBSTR(p.dob, $x)'))[:]
         self.assertEqual(persons, [Person[1], Person[3], Person[2]])
 
     @db_session
-    def test_17(self):
+    def test_18(self):
         # raw_sql in order_by() section without using lambda
         x = 9
         persons = Person.select().order_by(raw_sql('SUBSTR(p.dob, $x)'))[:]
@@ -147,13 +153,13 @@ class TestRawSQL(unittest.TestCase):
 
     @db_session
     @raises_exception(TypeError, "raw_sql(p.name)")
-    def test_18(self):
+    def test_19(self):
         # raw_sql argument cannot depend on iterator variables
         select(p for p in Person if raw_sql(p.name))[:]
 
     @db_session
     @raises_exception(ExprEvalError, "raw_sql('p.dob < $x') raises NameError: name 'x' is not defined")
-    def test_19(self):
+    def test_20(self):
         # testing for situation where parameter variable is missing
         select(p for p in Person if raw_sql('p.dob < $x'))[:]
 

commit f74997be92031bba46f9c40beea44d2ad6d25958

Fix bug in translating random() to SQL introduced in commit ab6f977b

pony/orm/sqltranslation.py
@@ -2014,8 +2014,8 @@ registered_functions[JOIN] = JoinMonad
 
 class FuncRandomMonad(FuncMonad):
     func = random
-    def __init__(monad, translator):
-        FuncMonad.__init__(monad, translator)
+    def __init__(monad, translator, type):
+        FuncMonad.__init__(monad, translator, type)
         translator.query_result_is_cacheable = False
     def __call__(monad):
         return NumericExprMonad(monad.translator, float, [ 'RANDOM' ])

pony/orm/tests/test_random.py
@@ -21,6 +21,15 @@ with db_session:
 class TestRandom(unittest.TestCase):
     @db_session
     def test_1(self):
+        persons = Person.select().random(2)
+        self.assertEqual(len(persons), 2)
+        p1, p2 = persons
+        self.assertNotEqual(p1.id, p2.id)
+        self.assertTrue(p1.id in range(1, 6))
+        self.assertTrue(p2.id in range(1, 6))
+
+    @db_session
+    def test_2(self):
         persons = Person.select_random(2)
         self.assertEqual(len(persons), 2)
         p1, p2 = persons