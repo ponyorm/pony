fedcb70f0be1c36a826bf7a5e475b4422c850024
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2016-07-28 12:33:47 +0300

    Oracle bug fixed

--- pony/orm/dbproviders/oracle.py
+++ pony/orm/dbproviders/oracle.py
@@ -498,13 +498,14 @@ def output_type_handler(cursor, name, defaultType, size, precision, scale):
 class OraPool(object):
     forked_pools = []
     def __init__(pool, **kwargs):
+        pool.kwargs = kwargs
         pool.cx_pool = cx_Oracle.SessionPool(**kwargs)
         pool.pid = os.getpid()
     def connect(pool):
         pid = os.getpid()
         if pool.pid != pid:
             pool.forked_pools.append((pool.cx_pool, pool.pid))
-            pool.cx_pool = cx_Oracle.SessionPool(**kwargs)
+            pool.cx_pool = cx_Oracle.SessionPool(**pool.kwargs)
             pool.pid = os.getpid()
         if core.debug: log_orm('GET CONNECTION')
         con = pool.cx_pool.acquire()
