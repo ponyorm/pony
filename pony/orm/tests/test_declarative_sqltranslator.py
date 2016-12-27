ponyorm/pony

commit ca3113c97a0bcd225a0001937366243e817d551f

Change a way aliases in SQL query are generated in order to fix a problem when a subquery alias mask a base query alias (see test_lambda_4 in test_declarative_sqltranslator.py)

pony/orm/sqltranslation.py
@@ -843,7 +843,7 @@ class Subquery(object):
             subquery.alias_counters = {}
             subquery.expr_counter = itertools.count(1)
         else:
-            subquery.alias_counters = parent_subquery.alias_counters.copy()
+            subquery.alias_counters = parent_subquery.alias_counters
             subquery.expr_counter = parent_subquery.expr_counter
         subquery.used_from_subquery = False
     def get_tableref(subquery, name_path, from_subquery=False):
@@ -862,14 +862,10 @@ class Subquery(object):
         tableref = JoinedTableRef(subquery, name_path, parent_tableref, attr)
         tablerefs[name_path] = tableref
         return tableref
-    def get_short_alias(subquery, name_path, entity_name):
-        if name_path:
-            if is_ident(name_path): return name_path
-            if not options.SIMPLE_ALIASES and len(name_path) <= max_alias_length:
-                return name_path
-        name = entity_name[:max_alias_length-3].lower()
+    def make_alias(subquery, name):
+        name = name[:max_alias_length-3].lower()
         i = subquery.alias_counters.setdefault(name, 0) + 1
-        alias = '%s-%d' % (name, i)
+        alias = name if i == 1 and name != 't' else '%s-%d' % (name, i)
         subquery.alias_counters[name] = i
         return alias
     def join_table(subquery, parent_alias, alias, table_name, join_cond):
@@ -886,7 +882,8 @@ class Subquery(object):
 class TableRef(object):
     def __init__(tableref, subquery, name, entity):
         tableref.subquery = subquery
-        tableref.alias = tableref.name_path = name
+        tableref.name_path = name
+        tableref.alias = subquery.make_alias(name)
         tableref.entity = entity
         tableref.joined = False
         tableref.can_affect_distinct = True
@@ -907,6 +904,7 @@ class JoinedTableRef(object):
     def __init__(tableref, subquery, name_path, parent_tableref, attr):
         tableref.subquery = subquery
         tableref.name_path = name_path
+        tableref.var_name = name_path if is_ident(name_path) else None
         tableref.alias = None
         tableref.optimized = None
         tableref.parent_tableref = parent_tableref
@@ -933,7 +931,7 @@ class JoinedTableRef(object):
                 assert reverse.columns and not reverse.is_collection
                 rentity = reverse.entity
                 pk_columns = rentity._pk_columns_
-                alias = subquery.get_short_alias(tableref.name_path, rentity.__name__)
+                alias = subquery.make_alias(tableref.var_name or rentity.__name__)
                 join_cond = join_tables(parent_alias, alias, left_pk_columns, reverse.columns)
             else:
                 if attr.pk_offset is not None:
@@ -946,16 +944,16 @@ class JoinedTableRef(object):
                     tableref.optimized = True
                     tableref.joined = True
                     return parent_alias, left_columns
-                alias = subquery.get_short_alias(tableref.name_path, entity.__name__)
+                alias = subquery.make_alias(tableref.var_name or entity.__name__)
                 join_cond = join_tables(parent_alias, alias, left_columns, pk_columns)
         elif not attr.reverse.is_collection:
-            alias = subquery.get_short_alias(tableref.name_path, entity.__name__)
+            alias = subquery.make_alias(tableref.var_name or entity.__name__)
             join_cond = join_tables(parent_alias, alias, left_pk_columns, attr.reverse.columns)
         else:
             right_m2m_columns = attr.reverse_columns if attr.symmetric else attr.columns
             if not tableref.joined:
                 m2m_table = attr.table
-                m2m_alias = subquery.get_short_alias(None, 't')
+                m2m_alias = subquery.make_alias('t')
                 reverse_columns = attr.columns if attr.symmetric else attr.reverse.columns
                 m2m_join_cond = join_tables(parent_alias, m2m_alias, left_pk_columns, reverse_columns)
                 subquery.join_table(parent_alias, m2m_alias, m2m_table, m2m_join_cond)
@@ -968,7 +966,7 @@ class JoinedTableRef(object):
             elif tableref.optimized:
                 assert not pk_only
                 m2m_alias = tableref.alias
-            alias = subquery.get_short_alias(tableref.name_path, entity.__name__)
+            alias = subquery.make_alias(tableref.var_name or entity.__name__)
             join_cond = join_tables(m2m_alias, alias, right_m2m_columns, pk_columns)
         if not pk_only and entity._discriminator_attr_:
             discr_criteria = entity._construct_discriminator_criteria_(alias)
@@ -2422,7 +2420,7 @@ class AttrSetMonad(SetMixin, Monad):
         assert len({alias for _, alias, column in groupby_columns}) == 1
 
         if extra_grouping:
-            inner_alias = translator.subquery.get_short_alias(None, 't')
+            inner_alias = translator.subquery.make_alias('t')
             inner_columns = [ 'DISTINCT' ]
             col_mapping = {}
             col_names = set()
@@ -2462,7 +2460,7 @@ class AttrSetMonad(SetMixin, Monad):
             subquery_ast.append([ 'WHERE' ] + inner_conditions)
         subquery_ast.append([ 'GROUP_BY' ] + groupby_columns)
 
-        alias = translator.subquery.get_short_alias(None, 't')
+        alias = translator.subquery.make_alias('t')
         for cond in outer_conditions: cond[2][1] = alias
         translator.subquery.from_ast.append([ alias, 'SELECT', subquery_ast, sqland(outer_conditions) ])
         expr_ast = [ 'COLUMN', alias, expr_name ]
@@ -2603,7 +2601,7 @@ class QuerySetMonad(SetMixin, Monad):
                 new_names.append(new_name)
                 select_ast[i] = [ 'AS', column_ast, new_name ]
 
-            alias = subquery.get_short_alias(None, 't')
+            alias = subquery.make_alias('t')
             outer_conditions = [ [ 'EQ', item_column, [ 'COLUMN', alias, new_name ] ]
                                     for item_column, new_name in izip(item_columns, new_names) ]
             subquery.from_ast.append([ alias, 'SELECT', subquery_ast[1:], sqland(outer_conditions) ])
@@ -2666,7 +2664,7 @@ class QuerySetMonad(SetMixin, Monad):
                         [ 'AGGREGATES', [ 'COUNT', 'DISTINCT', [ 'COLUMN', alias, 'ROWID' ] ] ],
                         from_ast, where_ast ]
                 else:
-                    alias = translator.subquery.get_short_alias(None, 't')
+                    alias = translator.subquery.make_alias('t')
                     sql_ast = [ 'SELECT', [ 'AGGREGATES', [ 'COUNT', 'ALL' ] ],
                                 [ 'FROM', [ alias, 'SELECT', [
                                   [ 'DISTINCT' ] + sub.expr_columns, from_ast, where_ast ] ] ] ]

pony/orm/tests/test_declarative_sqltranslator.py
@@ -361,6 +361,10 @@ class TestSQLTranslator(unittest.TestCase):
         q = select(s for s in Student)
         q = q.filter(lambda stud: exists(x for x in Student if stud.name < x.name))
         self.assertEqual(set(q), {Student[1], Student[2]})
+    def test_lambda_4(self):
+        q = select(s for s in Student)
+        q = q.filter(lambda stud: exists(s for s in Student if stud.name < s.name))
+        self.assertEqual(set(q), {Student[1], Student[2]})
 
 
 if __name__ == "__main__":

pony/orm/tests/test_declarative_join_optimization.py
@@ -69,7 +69,7 @@ class TestM2MOptimization(unittest.TestCase):
         q = select(s for s in Student if sum(c.credits for c in Course if s.group.dept == c.dept) > 10)
         objects = q[:]
         self.assertEqual(str(q._translator.subquery.from_ast),
-            "['FROM', ['s', 'TABLE', 'Student'], ['group-1', 'TABLE', 'Group', ['EQ', ['COLUMN', 's', 'group'], ['COLUMN', 'group-1', 'number']]]]")
+            "['FROM', ['s', 'TABLE', 'Student'], ['group', 'TABLE', 'Group', ['EQ', ['COLUMN', 's', 'group'], ['COLUMN', 'group', 'number']]]]")
 
 
 if __name__ == '__main__':

pony/orm/tests/test_declarative_sqltranslator2.py
@@ -114,12 +114,12 @@ class TestSQLTranslator2(unittest.TestCase):
         self.assertEqual(result, {Student[1], Student[2], Student[3], Student[4], Student[5], Student[6], Student[7]})
     def test_not_null1(self):
         q = select(g for g in Group if '123-45-67' not in g.students.tel and g.dept == Department[1])
-        not_null = "IS_NOT_NULL COLUMN student-1 tel" in (" ".join(str(i) for i in flatten(q._translator.conditions)))
+        not_null = "IS_NOT_NULL COLUMN student tel" in (" ".join(str(i) for i in flatten(q._translator.conditions)))
         self.assertEqual(not_null, True)
         self.assertEqual(q[:], [Group[101]])
     def test_not_null2(self):
         q = select(g for g in Group if 'John' not in g.students.name and g.dept == Department[1])
-        not_null = "IS_NOT_NULL COLUMN student-1 name" in (" ".join(str(i) for i in flatten(q._translator.conditions)))
+        not_null = "IS_NOT_NULL COLUMN student name" in (" ".join(str(i) for i in flatten(q._translator.conditions)))
         self.assertEqual(not_null, False)
         self.assertEqual(q[:], [Group[101]])
     def test_chain_of_attrs_inside_for1(self):

pony/orm/tests/test_relations_one2one3.py
@@ -38,9 +38,9 @@ class TestOneToOne3(unittest.TestCase):
         sql = self.db.last_sql
         self.assertEqual(sql, '''SELECT "p"."id", "p"."name"
 FROM "Person" "p"
-  LEFT JOIN "Passport" "passport-1"
-    ON "p"."id" = "passport-1"."person"
-WHERE "passport-1"."id" IS NULL''')
+  LEFT JOIN "Passport" "passport"
+    ON "p"."id" = "passport"."person"
+WHERE "passport"."id" IS NULL''')
 
     @db_session
     def test_3(self):
@@ -48,9 +48,9 @@ WHERE "passport-1"."id" IS NULL''')
         sql = self.db.last_sql
         self.assertEqual(sql, '''SELECT "p"."id", "p"."name"
 FROM "Person" "p"
-  LEFT JOIN "Passport" "passport-1"
-    ON "p"."id" = "passport-1"."person"
-WHERE "passport-1"."id" IS NULL''')
+  LEFT JOIN "Passport" "passport"
+    ON "p"."id" = "passport"."person"
+WHERE "passport"."id" IS NULL''')
 
     @db_session
     def test_4(self):
@@ -58,9 +58,9 @@ WHERE "passport-1"."id" IS NULL''')
         sql = self.db.last_sql
         self.assertEqual(sql, '''SELECT "p"."id", "p"."name"
 FROM "Person" "p"
-  LEFT JOIN "Passport" "passport-1"
-    ON "p"."id" = "passport-1"."person"
-WHERE "passport-1"."id" IS NOT NULL''')
+  LEFT JOIN "Passport" "passport"
+    ON "p"."id" = "passport"."person"
+WHERE "passport"."id" IS NOT NULL''')
 
     @db_session
     def test_5(self):

commit 5d29397e0462a4c93dc1a81a272ad6482460fb19

Fix bug introduced in ca3113c9: query optimization lead to invalid query

pony/orm/sqltranslation.py
@@ -843,7 +843,7 @@ class Subquery(object):
             subquery.alias_counters = {}
             subquery.expr_counter = itertools.count(1)
         else:
-            subquery.alias_counters = parent_subquery.alias_counters
+            subquery.alias_counters = parent_subquery.alias_counters.copy()
             subquery.expr_counter = parent_subquery.expr_counter
         subquery.used_from_subquery = False
     def get_tableref(subquery, name_path, from_subquery=False):

pony/orm/tests/test_declarative_sqltranslator.py
@@ -77,6 +77,7 @@ with db_session:
     g1.rooms = [ r1, r2 ]
     g2.rooms = [ r2, r3 ]
     c1.students.add(s1)
+    c1.students.add(s2)
     c2.students.add(s2)
 
 db2 = Database('sqlite', ':memory:')
@@ -283,13 +284,13 @@ class TestSQLTranslator(unittest.TestCase):
         self.assertEqual(result, {Teacher.get(name='T1')})
     def test_composite_key2(self):
         result = set(select(s for s in Student if Course['Math', 1] in s.courses))
-        self.assertEqual(result, {Student[1]})
+        self.assertEqual(result, {Student[1], Student[2]})
     def test_composite_key3(self):
         result = set(select(s for s in Student if Course['Math', 1] not in s.courses))
-        self.assertEqual(result, {Student[2], Student[3]})
+        self.assertEqual(result, {Student[3]})
     def test_composite_key4(self):
         result = set(select(s for s in Student if len(c for c in Course if c not in s.courses) == 2))
-        self.assertEqual(result, {Student[1], Student[2]})
+        self.assertEqual(result, {Student[1]})
     def test_composite_key5(self):
         result = set(select(s for s in Student if not (c for c in Course if c not in s.courses)))
         self.assertEqual(result, set())
@@ -332,7 +333,7 @@ class TestSQLTranslator(unittest.TestCase):
         self.assertEqual(result, {Student[2]})
     def test_hint_join2(self):
         result = set(select(c for c in Course if JOIN(len(c.students) == 1)))
-        self.assertEqual(result, {Course['Math', 1], Course['Economics', 1]})
+        self.assertEqual(result, {Course['Economics', 1]})
     def test_tuple_param(self):
         x = Student[1], Student[2]
         result = set(select(s for s in Student if s not in x))
@@ -365,6 +366,12 @@ class TestSQLTranslator(unittest.TestCase):
         q = select(s for s in Student)
         q = q.filter(lambda stud: exists(s for s in Student if stud.name < s.name))
         self.assertEqual(set(q), {Student[1], Student[2]})
+    def test_optimized_1(self):
+        q = select((g, count(g.students)) for g in Group if count(g.students) > 1)
+        self.assertEqual(set(q), {(Group[1], 2)})
+    def test_optimized_2(self):
+        q = select((s, count(s.courses)) for s in Student if count(s.courses) > 1)
+        self.assertEqual(set(q), {(Student[2], 2)})
 
 
 if __name__ == "__main__":