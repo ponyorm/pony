1fc96e5623a32221647db12d4cfe9f80531a2dae
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2017-08-16 14:07:55 +0300

    Remove Executor.renamed_tables and Executor.renamed_columns, use obj.prev and obj.new instead

--- pony/migrate/executor.py
+++ pony/migrate/executor.py
@@ -5,7 +5,6 @@ from pony.orm.dbschema import Table, Column, Trigger, DBIndex, ForeignKey
 from .operations import Op, OperationBatch, alter_table
 from .diagram_ops import RenameEntity, RenameAttr, AddAttr, ModifyAttr
 
-
 class Executor(object):
 
     def __init__(self, prev_db, new_db, entity_ops):
@@ -17,46 +16,58 @@ class Executor(object):
         self.prev_objects = self.prev_schema.objects_to_create()
         self.new_objects = self.new_schema.objects_to_create()
 
-        self.renamed_tables = renamed_tables = {}
-        self.renamed_columns = renamed_columns = defaultdict(dict)
-        renamed_entities = {}
+        entities = prev_db.entities.copy()
+        new_entities = new_db.entities
 
         for op in self.entity_ops:
             if isinstance(op, RenameEntity):
-                prev_entity = prev_db.entities[op.old_name]
-                new_entity = new_db.entities[op.new_name]
+                assert op.new_name not in entities
+                new_entity = new_entities[op.new_name]
+                prev_entity = entities.pop(op.old_name)
+                entities[op.new_name] = prev_entity
                 prev_entity._new_entity_ = new_entity
                 new_entity._prev_entity_ = prev_entity
 
-                renamed_entities[prev_entity] = new_entity
-                renamed_tables[new_entity._table_] = prev_entity._table_
+        for entity_name, prev_entity in entities.items():
+            if prev_entity._new_entity_ is None:
+                new_entity = new_entities.get(entity_name)
+                if new_entity is not None and new_entity._prev_entity_ is None:
+                    prev_entity._new_entity_ = new_entity
+                    new_entity._prev_entity_ = prev_entity
+            new_entity = prev_entity._new_entity_
+            if new_entity is not None:
+                prev_entity._table_object_.new = new_entity._table_object_
+                new_entity._table_object_.prev = prev_entity._table_object_
+
+        entities = prev_db.entities.copy()
 
         for op in self.entity_ops:
-            if isinstance(op, RenameAttr):
-                prev_entity = prev_db.entities[op.entity_name]
-                prev_table_name = prev_entity._table_
-                entity = renamed_entities.get(prev_entity, new_db.entities.get(op.entity_name))
+            if isinstance(op, RenameEntity):
+                prev_entity = entities.pop(op.old_name)
+                entities[op.new_name] = prev_entity
+            elif isinstance(op, RenameAttr):
+                prev_entity = entities[op.entity_name]
+                new_entity = new_entities[op.entity_name]  # fixme
+                assert prev_entity._table_object_.new is new_entity._table_object_
+                assert new_entity._table_object_.prev is prev_entity._table_object_
                 prev_attr = prev_entity._adict_[op.old_name]
-                new_attr = entity._adict_[op.new_name]
+                new_attr = new_entity._adict_[op.new_name]
                 prev_attr.new_attr = new_attr
                 new_attr.prev_attr = prev_attr
-                assert len(prev_attr.columns) == len(new_attr.columns)
-                assert len(prev_attr.column_objects) == len(new_attr.column_objects)
-                for prev_col, new_col in zip(prev_attr.column_objects, new_attr.column_objects):
-                    prev_col.new = new_col
-                    new_col.prev = prev_col
 
-                for prev_col, col in zip(prev_attr.columns, new_attr.columns):
-                    if prev_col != col:
-                        renamed_columns[prev_table_name][col] = prev_col
-
-        self.initials = initials = {}
-        for op in self.entity_ops:
-            if isinstance(op, (AddAttr, ModifyAttr)):
-                entity = self.new_db.entities[op.entity_name]
-                attr = entity._adict_[op.attr_name]
-                if attr.initial is not None:
-                    initials[attr] = attr.initial
+        for entity_name, prev_entity in entities.items():
+            new_entity = prev_entity._new_entity_
+            if new_entity is not None:
+                assert prev_entity._table_object_.new is new_entity._table_object_
+                assert new_entity._table_object_.prev is prev_entity._table_object_
+                for prev_attr in prev_entity._new_attrs_:
+                    new_attr = prev_attr.new_attr
+                    if new_attr is not None:
+                        assert len(prev_attr.columns) == len(new_attr.columns)
+                        assert len(prev_attr.column_objects) == len(new_attr.column_objects)
+                        for prev_col, new_col in zip(prev_attr.column_objects, new_attr.column_objects):
+                            prev_col.new = new_col
+                            new_col.prev = prev_col
 
         for prev_constraint in self.prev_schema.constraints.values():
             new_constrant = self.new_schema.constraints.get(prev_constraint.name)
@@ -87,63 +98,48 @@ class Executor(object):
         provider = schema.provider
         quote_name = provider.quote_name
 
-        extra_ops = []
-
-        for op in ops:
+        for op in ops[:]:
             col = op.obj
+            if not isinstance(col, Column): continue
+            new_attr = col.attr
+            prev_attr = new_attr.prev_attr
 
-            if not isinstance(col, Column):
-                continue
+            if (op.type == 'create' or op.type == 'alter' and new_attr.is_required and not prev_attr.is_required) \
+                    and col.is_not_null and not col.sql_default and new_attr.initial is not None:
 
-            if op.type != 'create' and not (
-                    op.type == 'alter' and col.attr.is_required and not col.attr.prev_attr.is_requried):
-                continue
+                value_class = provider.sqlbuilder_cls.value_class
+                op.sql += ' DEFAULT %s' % value_class(provider.paramstyle, new_attr.initial)  # fix Oracle?
 
-            if not col.is_not_null or col.sql_default:
-                continue
+                schema = col.table.schema
+                drop_default_clause = 'DROP DEFAULT' if provider.dialect != 'Oracle' else 'DEFAULT NULL'
+                sql = '%s %s %s' % (schema.MODIFY_COLUMN, quote_name(col.name), schema.case(drop_default_clause))
+                ops.append(Op(sql, obj=col, type='alter', prefix=alter_table(col.table)))
 
-            attr = col.attr
-            if attr not in self.initials: continue
-            value = self.initials.pop(attr)
+        for op in self.entity_ops:
+            if isinstance(op, ModifyAttr):
+                new_entity = self.new_db.entities[op.entity_name]
+                new_attr = new_entity._adict_[op.attr_name]
+                if new_attr.initial is not None and not (new_attr.is_required and not new_attr.prev_attr.is_required):
+                    assert len(new_attr.columns) == 1
+                    col_name = new_attr.columns[0]
+                    table_name = new_attr.entity._table_
+                    sql_ast = [ 'UPDATE', table_name, [ [ 'COLUMN', None, col_name ], [ 'VALUE', new_attr.initial ] ],
+                                [ 'WHERE', [ 'IS_NULL', [ 'COLUMN', None, col_name ] ] ] ]
+                    sql = provider.ast2sql(sql_ast)[0]
+                    ops.append(Op(sql, obj=new_attr, type='set_defaults'))
 
-            value_class = provider.sqlbuilder_cls.value_class
-            op.sql += ' DEFAULT %s' % value_class(provider.paramstyle, value)  # fix Oracle?
-
-
-
-            schema = col.table.schema
-            drop_default_clause = 'DROP DEFAULT' if provider.dialect != 'Oracle' else 'DEFAULT NULL'
-            sql = '{} {} {}'.format(
-                schema.MODIFY_COLUMN, quote_name(col.name), schema.case(drop_default_clause))
-
-            extra_ops.append(Op(sql, obj=col, type='alter', prefix=alter_table(col.table)))
-
-        for attr, value in self.initials.items():
-            assert len(attr.columns) == 1
-            col_name = attr.columns[0]
-            table_name = attr.entity._table_
-            sql_ast = [ 'UPDATE', table_name, [ [ 'COLUMN', None, col_name ], [ 'VALUE', value ] ],
-                        [ 'WHERE', [ 'IS_NULL', [ 'COLUMN', None, col_name ] ] ] ]
-            sql = provider.ast2sql(sql_ast)[0]
-            extra_ops.append(Op(sql, obj=attr, type='set_defaults'))
-
-        return ops + extra_ops
+        return ops
 
     def _sorted(self, ops):
         # handle renames
-        renamed_tables = self.renamed_tables
-        renamed_columns = self.renamed_columns
         extra_ops = []
-        if renamed_tables or renamed_columns:
-            for table in self.new_objects:
-                prev_name = renamed_tables.get(table.name)
-                if prev_name:
-                    extra_ops.extend(table.get_rename_ops(prev_name))
+        for prev_name, prev_table in self.prev_schema.tables.items():
+            if prev_name != prev_table.new.name:
+                extra_ops.extend(prev_table.get_rename_ops(prev_table.new.name))
             if self.new_schema.provider.dialect != 'SQLite':
-                for table in self.new_objects:
-                    for name, prev_name in renamed_columns.get(table.name, {}).items():
-                        col = table.column_dict[name]
-                        extra_ops.extend(col.get_rename_ops(prev_name))
+                for prev_col in prev_table.column_list:
+                    if prev_col.name != prev_col.new.name:
+                        extra_ops.extend(prev_col.get_rename_ops(prev_col.new.name))
         ops = extra_ops + ops
 
         ops = self.handle_initials(ops)
@@ -180,23 +176,15 @@ class Executor(object):
         new_objects = OrderedDict(
             ((t.name, objects.copy()) for t, objects in self.new_objects.items())
         )
-        renamed_tables = self.renamed_tables
 
         for table_name, objects in new_objects.items():
             for obj_name, new_obj in objects.items():
                 sql = new_obj.get_create_command()
-                if table_name in renamed_tables:
-                    table_name = renamed_tables[table_name]
-                if obj_name in renamed_tables:
-                    obj_name = renamed_tables[obj_name]
-                prev_obj = prev_objects.get(table_name, {}).pop(obj_name, None)
-                renamed_columns = self.renamed_columns.get(table_name, {})
-
+                prev_obj = new_obj.prev
                 if prev_obj is None:
                     yield Op(sql, obj=new_obj, type='create')
                 elif sql != prev_obj.get_create_command():
-                    for item in new_obj.get_alter_ops(
-                            prev_obj, new_objects, executor=self, renamed_columns=renamed_columns):
+                    for item in new_obj.get_alter_ops(prev_obj, new_objects, executor=self):
                         yield item
 
         for table_name, objects in prev_objects.items():
