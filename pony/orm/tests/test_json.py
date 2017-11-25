ponyorm/pony

commit 1ba414058166c38a7b68c59a08730b8c09232006

Remove unnecessary val2dbval conversion

pony/orm/core.py
@@ -4659,7 +4659,7 @@ class Entity(with_metaclass(EntityMeta)):
             val = obj._vals_[attr]
             if val is not None and val._status_ == 'created':
                 val._save_(dependent_objects)
-    def _update_dbvals_(obj, after_create):
+    def _update_dbvals_(obj, after_create, new_dbvals):
         bits = obj._bits_
         vals = obj._vals_
         dbvals = obj._dbvals_
@@ -4678,10 +4678,7 @@ class Entity(with_metaclass(EntityMeta)):
             elif after_create and val is None:
                 obj._rbits_ &= ~bits[attr]
             else:
-                # For normal attribute, set `dbval` to the same value as `val` after update/create
-                # dbvals[attr] = val
-                converter = attr.converters[0]
-                dbvals[attr] = converter.val2dbval(val, obj)  # TODO this conversion should be unnecessary
+                dbvals[attr] = new_dbvals.get(attr, val)
                 continue
             # Clear value of volatile attribute or null values after create, because the value may be changed in the DB
             del vals[attr]
@@ -4691,12 +4688,19 @@ class Entity(with_metaclass(EntityMeta)):
         auto_pk = (obj._pkval_ is None)
         attrs = []
         values = []
+        new_dbvals = {}
         for attr in obj._attrs_with_columns_:
             if auto_pk and attr.is_pk: continue
             val = obj._vals_[attr]
             if val is not None:
                 attrs.append(attr)
-                values.extend(attr.get_raw_values(val))
+                if not attr.reverse:
+                    assert len(attr.converters) == 1
+                    dbval = attr.converters[0].val2dbval(val, obj)
+                    new_dbvals[attr] = dbval
+                    values.append(dbval)
+                else:
+                    values.extend(attr.get_raw_values(val))
         attrs = tuple(attrs)
 
         database = obj._database_
@@ -4746,14 +4750,21 @@ class Entity(with_metaclass(EntityMeta)):
         obj._status_ = 'inserted'
         obj._rbits_ = obj._all_bits_except_volatile_
         obj._wbits_ = 0
-        obj._update_dbvals_(True)
+        obj._update_dbvals_(True, new_dbvals)
     def _save_updated_(obj):
         update_columns = []
         values = []
+        new_dbvals = {}
         for attr in obj._attrs_with_bit_(obj._attrs_with_columns_, obj._wbits_):
             update_columns.extend(attr.columns)
             val = obj._vals_[attr]
-            values.extend(attr.get_raw_values(val))
+            if not attr.reverse:
+                assert len(attr.converters) == 1
+                dbval = attr.converters[0].val2dbval(val, obj)
+                new_dbvals[attr] = dbval
+                values.append(dbval)
+            else:
+                values.extend(attr.get_raw_values(val))
         if update_columns:
             for attr in obj._pk_attrs_:
                 val = obj._vals_[attr]
@@ -4791,7 +4802,7 @@ class Entity(with_metaclass(EntityMeta)):
         obj._status_ = 'updated'
         obj._rbits_ |= obj._wbits_ & obj._all_bits_except_volatile_
         obj._wbits_ = 0
-        obj._update_dbvals_(False)
+        obj._update_dbvals_(False, new_dbvals)
     def _save_deleted_(obj):
         values = []
         values.extend(obj._get_raw_pkval_())

pony/orm/sqlbuilding.py
@@ -31,11 +31,11 @@ class Param(object):
         if j is not None:
             assert type(type(value)).__name__ == 'EntityMeta'
             value = value._get_raw_pkval_()[j]
-        if value is not None:  # can value be None at all?
-            converter = param.converter
-            if converter is not None:
-                if not param.optimistic: value = converter.val2dbval(value)
-                value = converter.py2sql(value)
+        converter = param.converter
+        if value is not None and converter is not None:
+            if converter.attr is None:
+                value = converter.val2dbval(value)
+            value = converter.py2sql(value)
         return value
     def __unicode__(param):
         paramstyle = param.style

commit e1dc7feb710df12e844bec7dbb5880e125ca5fb1

Fix #308: JSON updating bug introduced in commit 1ba41405

pony/orm/core.py
@@ -4810,7 +4810,8 @@ class Entity(with_metaclass(EntityMeta)):
             elif after_create and val is None:
                 obj._rbits_ &= ~bits[attr]
             else:
-                dbvals[attr] = new_dbvals.get(attr, val)
+                if attr in new_dbvals:
+                    dbvals[attr] = new_dbvals[attr]
                 continue
             # Clear value of volatile attribute or null values after create, because the value may be changed in the DB
             del vals[attr]
@@ -4832,6 +4833,7 @@ class Entity(with_metaclass(EntityMeta)):
                     new_dbvals[attr] = dbval
                     values.append(dbval)
                 else:
+                    new_dbvals[attr] = val
                     values.extend(attr.get_raw_values(val))
         attrs = tuple(attrs)
 
@@ -4896,6 +4898,7 @@ class Entity(with_metaclass(EntityMeta)):
                 new_dbvals[attr] = dbval
                 values.append(dbval)
             else:
+                new_dbvals[attr] = val
                 values.extend(attr.get_raw_values(val))
         if update_columns:
             for attr in obj._pk_attrs_:

pony/orm/tests/test_json.py
@@ -644,3 +644,13 @@ class TestJson(TestCase):
         with db_session:
             val = select(p.info['id'] for p in Product if not p.info['val'])
             self.assertEqual(tuple(sorted(val)), (2, 3, 5, 7, 9, 11))
+
+    @db_session
+    def test_optimistic_check(self):
+        p1 = self.Product.select().first()
+        p1.info['foo'] = 'bar'
+        flush()
+        p1.name = 'name2'
+        flush()
+        p1.name = 'name3'
+        flush()