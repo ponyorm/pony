864f55f78701fb9643b1fa4d7546ec675197b5d5
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2018-02-26 17:15:40 +0300

    Rename local variable

--- pony/orm/dbproviders/oracle.py
+++ pony/orm/dbproviders/oracle.py
@@ -442,10 +442,10 @@ class OraProvider(DBAPIProvider):
         return name[:provider.max_name_len].upper()
 
     def normalize_vars(provider, vars, vartypes):
-        for name, value in iteritems(vars):
+        for key, value in iteritems(vars):
             if value == '':
-                vars[name] = None
-                vartypes[name] = NoneType
+                vars[key] = None
+                vartypes[key] = NoneType
 
     @wrap_dbapi_exceptions
     def set_transaction_mode(provider, connection, cache):
