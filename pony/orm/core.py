ponyorm/pony

commit 88cb468cd7d6726dfe5db9dc4755b22cfc605859

Every translator now gets unique filter_num from core.filter_num_counter

pony/orm/core.py
@@ -5276,12 +5276,14 @@ def extract_vars(filter_num, extractors, globals, locals, cells=None):
 def unpickle_query(query_result):
     return query_result
 
+filter_num_counter = itertools.count()
+
 class Query(object):
     def __init__(query, code_key, tree, globals, locals, cells=None, left_join=False):
         assert isinstance(tree, ast.GenExprInner)
         extractors, tree, extractors_key = create_extractors(
             code_key, tree, globals, locals, special_functions, const_functions)
-        filter_num = 0
+        filter_num = next(filter_num_counter)
         vars, vartypes = extract_vars(filter_num, extractors, globals, locals, cells)
 
         node = tree.quals[0].iter
@@ -5305,11 +5307,12 @@ class Query(object):
             pickled_tree = pickle_ast(tree)
             tree_copy = unpickle_ast(pickled_tree)  # tree = deepcopy(tree)
             translator_cls = database.provider.translator_cls
-            translator = translator_cls(tree_copy, extractors, vartypes, left_join=left_join)
+            translator = translator_cls(tree_copy, filter_num, extractors, vartypes, left_join=left_join)
             name_path = translator.can_be_optimized()
             if name_path:
                 tree_copy = unpickle_ast(pickled_tree)  # tree = deepcopy(tree)
-                try: translator = translator_cls(tree_copy, extractors, vartypes, left_join=True, optimize=name_path)
+                try: translator = translator_cls(tree_copy, filter_num, extractors, vartypes,
+                                                 left_join=True, optimize=name_path)
                 except OptimizationFailed: translator.optimization_failed = True
             translator.pickled_tree = pickled_tree
             database._translator_cache[query._key] = translator
@@ -5606,7 +5609,7 @@ class Query(object):
                     throw(TypeError, 'Incorrect number of lambda arguments. '
                                      'Expected: %d, got: %d' % (expr_count, len(argnames)))
 
-        filter_num = len(query._filters) + 1
+        filter_num = next(filter_num_counter)
         extractors, func_ast, extractors_key = create_extractors(
             func_id, func_ast, globals, locals, special_functions, const_functions,
             argnames or prev_translator.subquery)
@@ -5627,11 +5630,10 @@ class Query(object):
                 name_path = new_translator.can_be_optimized()
                 if name_path:
                     tree_copy = unpickle_ast(prev_translator.pickled_tree)  # tree = deepcopy(tree)
-                    prev_extractors = prev_translator.extractors
-                    prev_vartypes = prev_translator.vartypes
                     translator_cls = prev_translator.__class__
-                    new_translator = translator_cls(tree_copy, prev_extractors, prev_vartypes,
-                                                    left_join=True, optimize=name_path)
+                    new_translator = translator_cls(
+                            tree_copy, prev_translator.original_filter_num, prev_translator.extractors, prev_translator.vartypes,
+                            left_join=True, optimize=name_path)
                     new_translator = query._reapply_filters(new_translator)
                     new_translator = new_translator.apply_lambda(filter_num, order_by, func_ast, argnames, original_names, extractors, vartypes)
             query._database._translator_cache[new_key] = new_translator

pony/orm/sqltranslation.py
@@ -163,12 +163,12 @@ class SQLTranslator(ASTTranslator):
                     else: throw(TranslationError, 'Too complex aggregation, expressions cannot be combined: %s' % ast2src(node))
             return monad
 
-    def __init__(translator, tree, extractors, vartypes, parent_translator=None, left_join=False, optimize=None):
+    def __init__(translator, tree, filter_num, extractors, vartypes, parent_translator=None, left_join=False, optimize=None):
         assert isinstance(tree, ast.GenExprInner), tree
         ASTTranslator.__init__(translator, tree)
         translator.database = None
         translator.lambda_argnames = None
-        translator.filter_num = parent_translator.filter_num if parent_translator is not None else 0
+        translator.filter_num = translator.original_filter_num = filter_num
         translator.extractors = extractors
         translator.vartypes = vartypes.copy()
         translator.parent = parent_translator
@@ -649,7 +649,7 @@ class SQLTranslator(ASTTranslator):
     def preGenExpr(translator, node):
         inner_tree = node.code
         translator_cls = translator.__class__
-        subtranslator = translator_cls(inner_tree, translator.extractors, translator.vartypes, translator)
+        subtranslator = translator_cls(inner_tree, translator.filter_num, translator.extractors, translator.vartypes, translator)
         return translator.QuerySetMonad(translator, subtranslator)
     def postGenExprIf(translator, node):
         monad = node.test.monad
@@ -773,7 +773,7 @@ class SQLTranslator(ASTTranslator):
         for_expr = ast.GenExprFor(ast.AssName(iter_name, 'OP_ASSIGN'), name_ast, [ if_expr ])
         inner_expr = ast.GenExprInner(ast.Name(iter_name), [ for_expr ])
         translator_cls = translator.__class__
-        subtranslator = translator_cls(inner_expr, translator.extractors, translator.vartypes, translator)
+        subtranslator = translator_cls(inner_expr, translator.filter_num, translator.extractors, translator.vartypes, translator)
         return translator.QuerySetMonad(translator, subtranslator)
     def postCallFunc(translator, node):
         args = []

commit 8d9cbec0d4a916a1cad3edecd5c6f2b508e8daed

Fixes #370 Memory leak in 0.7.4-0.7.5, caused by 88cb468c

pony/orm/core.py
@@ -5303,24 +5303,23 @@ def extract_vars(filter_num, extractors, globals, locals, cells=None):
 def unpickle_query(query_result):
     return query_result
 
-filter_num_counter = itertools.count()
-
 class Query(object):
     def __init__(query, code_key, tree, globals, locals, cells=None, left_join=False):
         assert isinstance(tree, ast.GenExprInner)
         tree, extractors = create_extractors(code_key, tree, globals, locals, special_functions, const_functions)
-        filter_num = next(filter_num_counter)
+        filter_num = 0
         vars, vartypes = extract_vars(filter_num, extractors, globals, locals, cells)
 
         node = tree.quals[0].iter
         origin = vars[filter_num, node.src]
         if isinstance(origin, Query):
-            database = origin._translator.database
+            base_query = origin
         elif isinstance(origin, QueryResult):
-            database = origin._query._translator.database
+            base_query = origin._query
         elif isinstance(origin, QueryResultIterator):
-            database = origin._query_result._query._translator.database
+            base_query = origin._query_result._query
         else:
+            base_query = None
             if isinstance(origin, EntityIter):
                 origin = origin.entity
             elif not isinstance(origin, EntityMeta):
@@ -5331,6 +5330,12 @@ class Query(object):
             if database is None: throw(TranslationError, 'Entity %s is not mapped to a database' % origin.__name__)
             if database.schema is None: throw(ERDiagramError, 'Mapping is not generated for entity %r' % origin.__name__)
 
+        if base_query is not None:
+            database = base_query._translator.database
+            filter_num = base_query._filter_num + 1
+            vars, vartypes = extract_vars(filter_num, extractors, globals, locals, cells)
+
+        query._filter_num = filter_num
         database.provider.normalize_vars(vars, vartypes)
 
         query._key = HashableDict(code_key=code_key, vartypes=vartypes, left_join=left_join, filters=())
@@ -5694,23 +5699,23 @@ class Query(object):
         else:
             original_names = True
 
-        filter_num = next(filter_num_counter)
+        new_filter_num = query._filter_num + 1
         func_ast, extractors = create_extractors(
             func_id, func_ast, globals, locals, special_functions, const_functions, argnames or prev_translator.namespace)
         if extractors:
-            vars, vartypes = extract_vars(filter_num, extractors, globals, locals, cells)
+            vars, vartypes = extract_vars(new_filter_num, extractors, globals, locals, cells)
             query._database.provider.normalize_vars(vars, vartypes)
             new_vars = query._vars.copy()
             new_vars.update(vars)
         else: new_vars, vartypes = query._vars, HashableDict()
         tup = (('order_by' if order_by else 'where' if original_names else 'filter', func_id, vartypes),)
         new_key = HashableDict(query._key, filters=query._key['filters'] + tup)
-        new_filters = query._filters + (('apply_lambda', filter_num, order_by, func_ast, argnames, original_names, extractors, None, vartypes),)
+        new_filters = query._filters + (('apply_lambda', new_filter_num, order_by, func_ast, argnames, original_names, extractors, None, vartypes),)
 
         new_translator, new_vars = query._get_translator(new_key, new_vars)
         if new_translator is None:
             prev_optimized = prev_translator.optimize
-            new_translator = prev_translator.apply_lambda(filter_num, order_by, func_ast, argnames, original_names, extractors, new_vars, vartypes)
+            new_translator = prev_translator.apply_lambda(new_filter_num, order_by, func_ast, argnames, original_names, extractors, new_vars, vartypes)
             if not prev_optimized:
                 name_path = new_translator.can_be_optimized()
                 if name_path:
@@ -5724,9 +5729,10 @@ class Query(object):
                     except UseAnotherTranslator:
                         assert False
                     new_translator = query._reapply_filters(new_translator)
-                    new_translator = new_translator.apply_lambda(filter_num, order_by, func_ast, argnames, original_names, extractors, new_vars, vartypes)
+                    new_translator = new_translator.apply_lambda(new_filter_num, order_by, func_ast, argnames, original_names, extractors, new_vars, vartypes)
             query._database._translator_cache[new_key] = new_translator
-        return query._clone(_vars=new_vars, _key=new_key, _filters=new_filters, _translator=new_translator)
+        return query._clone(_filter_num=new_filter_num, _vars=new_vars, _key=new_key, _filters=new_filters,
+                            _translator=new_translator)
     def _reapply_filters(query, translator):
         for tup in query._filters:
             method_name, args = tup[0], tup[1:]