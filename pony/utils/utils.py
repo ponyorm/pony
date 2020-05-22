243efeac070de93941249b7ab52b85d17293d4ea
Justin Butler <justin@workstep.com>
2020-05-22 16:02:14 -0700

    sweet mercy use thread local

--- pony/utils/utils.py
+++ pony/utils/utils.py
@@ -16,8 +16,7 @@ from pony import options
 
 from pony.thirdparty.decorator import decorator as _decorator
 
-if pony.MODE.startswith('GAE-'): localbase = object
-else: from threading import local as localbase
+from threading import local as localbase
 
 
 class PonyDeprecationWarning(DeprecationWarning):
