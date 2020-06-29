59f5e70f5a5254ef591328d30e0015729452e39d
Ivan Zhitnikov <ivan.zhitnikov@agilecode.io>
2020-06-29 18:51:43 +0500

    Added type casting test longstr -> datetime

--- pony/orm/migrations/tests/test_type_cast.py
+++ pony/orm/migrations/tests/test_type_cast.py
@@ -1780,6 +1780,40 @@ class TestTypeCast(unittest.TestCase):
         self.assertEqual("\n".join(sql_ops), correct_sql)
         self.assertEqual(expected_schema, actual_schema)
 
+    def test_change_attr_type_longstr_to_datetime(self):
+        """
+            Changes longstr attribute "description" in entity "Item" to datetime type
+        """
+        # Logically correct type casting
+        self.db = db = Database(**self.db_params)
+
+        class Item(db.Entity):
+            id = PrimaryKey(int, auto=True)
+            description = Required(LongStr)
+
+        db.generate_mapping(create_tables=True)
+
+        self.db2 = db2 = Database(**self.db_params)
+
+        class Item(db2.Entity):
+            id = PrimaryKey(int, auto=True)
+            description = Required(datetime)
+
+        correct_sql = 'ALTER TABLE "item" ALTER COLUMN "description" TYPE TIMESTAMP USING "description"::TIMESTAMP'
+
+        migration_op = "ChangeColumnType(entity_name='Item', attr_name='description', " \
+                       "new_options={'py_type': datetime}, cast_sql='{colname}::TIMESTAMP')"
+
+        expected_schema, actual_schema, migration, sql_ops = self.apply_migrate()
+        imports = defaultdict(set)
+        t = []
+        for op in migration.operations:
+            t.append(op.serialize(imports))
+
+        self.assertEqual("\n".join(t), migration_op)
+        self.assertEqual("\n".join(sql_ops), correct_sql)
+        self.assertEqual(expected_schema, actual_schema)
+
 
 if __name__ == '__main__':
     unittest.main()
