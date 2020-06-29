d059536ae403a5ce5b11aca59efb4f3e6682b5c6
Ivan Zhitnikov <ivan.zhitnikov@agilecode.io>
2020-06-29 13:30:58 +0500

    Fixed str -> float test

--- pony/orm/migrations/tests/test_type_cast.py
+++ pony/orm/migrations/tests/test_type_cast.py
@@ -86,9 +86,10 @@ class TestTypeCast(unittest.TestCase):
             id = PrimaryKey(int, auto=True)
             name = Required(float)
 
-        correct_sql = ''
+        correct_sql = 'ALTER TABLE "item" ALTER COLUMN "name" TYPE DOUBLE PRECISION USING "name"::DOUBLE PRECISION'
 
-        migration_op = ""
+        migration_op = "ChangeColumnType(entity_name='Item', attr_name='name', new_options={'py_type': float}, " \
+                       "cast_sql='{colname}::DOUBLE PRECISION')"
 
         expected_schema, actual_schema, migration, sql_ops = self.apply_migrate()
         imports = defaultdict(set)
