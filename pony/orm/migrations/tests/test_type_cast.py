3d53bf7752e8dee67b9a70cd8b987ea2f0620acd
Ivan Zhitnikov <ivan.zhitnikov@agilecode.io>
2020-06-29 16:33:23 +0500

    Added type casting test decimal -> bool

--- pony/orm/migrations/tests/test_type_cast.py
+++ pony/orm/migrations/tests/test_type_cast.py
@@ -887,6 +887,41 @@ class TestTypeCast(unittest.TestCase):
         self.assertEqual("\n".join(sql_ops), correct_sql)
         self.assertEqual(expected_schema, actual_schema)
 
+    def test_change_attr_type_decimal_to_bool(self):
+        """
+            Changes decimal attribute "number" in entity "Item" to bool type
+        """
+        # Logically correct type casting
+        self.db = db = Database(**self.db_params)
+
+        class Item(db.Entity):
+            id = PrimaryKey(int, auto=True)
+            number = Required(Decimal)
+
+        db.generate_mapping(create_tables=True)
+
+        self.db2 = db2 = Database(**self.db_params)
+
+        class Item(db2.Entity):
+            id = PrimaryKey(int, auto=True)
+            number = Required(bool)
+
+        correct_sql = ''
+
+        migration_op = ""
+
+        # apply migrate () raises exceptions :
+        # psycopg2.errors.CannotCoerce: cannot cast type double precision to boolean
+        # pony.orm.dbapiprovider.ProgrammingError: cannot cast type double precision to boolean
+        expected_schema, actual_schema, migration, sql_ops = self.apply_migrate()
+        imports = defaultdict(set)
+        t = []
+        for op in migration.operations:
+            t.append(op.serialize(imports))
+
+        self.assertEqual("\n".join(t), migration_op)
+        self.assertEqual("\n".join(sql_ops), correct_sql)
+        self.assertEqual(expected_schema, actual_schema)
 
 if __name__ == '__main__':
     unittest.main()
