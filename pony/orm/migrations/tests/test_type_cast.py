283080d5d8efbe05fcef2328329d2e660de5efa8
Ivan Zhitnikov <ivan.zhitnikov@agilecode.io>
2020-06-29 18:40:38 +0500

    Added type casting test longstr -> str

--- pony/orm/migrations/tests/test_type_cast.py
+++ pony/orm/migrations/tests/test_type_cast.py
@@ -1642,6 +1642,40 @@ class TestTypeCast(unittest.TestCase):
         self.assertEqual("\n".join(sql_ops), correct_sql)
         self.assertEqual(expected_schema, actual_schema)
 
+    def test_change_attr_type_longstr_to_str(self):
+        """
+            Changes longstr attribute "description" in entity "Item" to str type
+        """
+        # Logically correct type casting
+        self.db = db = Database(**self.db_params)
+
+        class Item(db.Entity):
+            id = PrimaryKey(int, auto=True)
+            description = Required(LongStr)
+
+        db.generate_mapping(create_tables=True)
+
+        self.db2 = db2 = Database(**self.db_params)
+
+        class Item(db2.Entity):
+            id = PrimaryKey(int, auto=True)
+            description = Required(str)
+
+        correct_sql = ''
+
+        migration_op = "ChangeColumnType(entity_name='Item', attr_name='description', new_options={'py_type': str}, " \
+                       "cast_sql='{colname}::TEXT')"
+
+        expected_schema, actual_schema, migration, sql_ops = self.apply_migrate()
+        imports = defaultdict(set)
+        t = []
+        for op in migration.operations:
+            t.append(op.serialize(imports))
+
+        self.assertEqual("\n".join(t), migration_op)
+        self.assertEqual("\n".join(sql_ops), correct_sql)
+        self.assertEqual(expected_schema, actual_schema)
+
 
 if __name__ == '__main__':
     unittest.main()
