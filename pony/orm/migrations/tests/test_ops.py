4cfb9384d9984a35d912395c55e14c69630edb3a
Ivan Zhitnikov <ivan.zhitnikov@agilecode.io>
2020-06-30 11:08:30 +0500

    Added test change optional to required

--- pony/orm/migrations/tests/test_ops.py
+++ pony/orm/migrations/tests/test_ops.py
@@ -2256,6 +2256,78 @@ class TestMigrations(unittest.TestCase):
 
         self.assertEqual(cm.exception.args[0], 'Cannot change primary key')
 
+    def test_change_optional_to_required(self):
+        """
+            Changes optional attribute "description" in entity "Course" to required with initial value
+        """
+        self.db2 = db2 = Database(**self.db_params)
+
+        class Department(db2.Entity):
+            number = PrimaryKey(int, auto=True)
+            name = Required(str, 100)
+            groups = Set('Group')
+            courses = Set('Course')
+            teachers = Set('Teacher')
+            rating = Optional(Decimal)
+
+        class Group(db2.Entity):
+            number = PrimaryKey(int, auto=True)
+            major = Required(str, unique=True)
+            dept = Required(Department)
+            students = Set('Student')
+            curator = Optional('Teacher')
+
+        class Course(db2.Entity):
+            name = Required(str)
+            semester = Required(int)
+            lect_hours = Required(int)
+            lab_hours = Required(int, unsigned=True)
+            credits = Required(int, size=8)
+            dept = Required(Department)
+            students = Set('Student')
+            teacher = Required('Teacher')
+            PrimaryKey(name, semester)
+            description = Required(str, initial="Empty description")
+            last_update = Optional(datetime)
+
+        class Student(db2.Entity):
+            id = PrimaryKey(int, auto=True)
+            name = Required(str)
+            dob = Required(date)
+            picture = Optional(buffer)
+            gpa = Optional(float)
+            group = Required(Group)
+            courses = Set(Course)
+
+        class Teacher(db2.Entity):
+            id = PrimaryKey(int)
+            name = Required(str)
+            surname = Optional(str)
+            dob = Required(date)
+            departments = Set(Department)
+            courses = Set(Course)
+            biography = Optional(str, nullable=True)
+            groups = Set(Group)
+            head_of_dept = Optional('DeptDirector')
+
+        class DeptDirector(Teacher):
+            is_director = Required(bool)
+            teacher = Optional(Teacher)
+
+        correct_sql = ''
+
+        migration_op = "ChangeAttributeClass(entity_name='Course', attr_name='description', new_class='Required')"
+
+        expected_schema, actual_schema, migration, sql_ops = self.apply_migrate()
+        imports = defaultdict(set)
+        t = []
+        for op in migration.operations:
+            t.append(op.serialize(imports))
+
+        self.assertEqual("\n".join(sql_ops), correct_sql)
+        self.assertEqual("\n".join(t), migration_op)
+        self.assertEqual(expected_schema, actual_schema) # Test fails on schemas comparing
+
 
 
 
