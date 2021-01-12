cc1f7f9d3d16fa9a6b16554d233d5bf106cbc024
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2021-01-12 13:55:18 +0100

    Update Pony version: 0.7.14 -> 0.7.15-dev

--- pony/__init__.py
+++ pony/__init__.py
@@ -4,7 +4,7 @@ import os, sys, time, threading, random
 from os.path import dirname
 from itertools import count
 
-__version__ = '0.7.14'
+__version__ = '0.7.15-dev'
 
 uid = str(random.randint(1, 1000000))
 
