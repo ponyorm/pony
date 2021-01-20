a82487f476135205a3a9d385c02eb54dfa23e186
Alexander Tischenko <sashaaero@yandex.ru>
2021-01-20 17:54:59 +0300

    Fix incorrect m2m table name
    In some cases m2m relation refers to another table name

--- pony/orm/core.py
+++ pony/orm/core.py
@@ -1049,7 +1049,7 @@ class Database(object):
                         m2m_table = attr.meta.m2m_table
                         fix_obsolete_table(m2m_table)
                         processed_m2m_attrs.add(attr)
-                    attr.table = m2m_table.name
+                        attr.table = m2m_table.name
                     attr.columns = [c.name for c in attr.meta.reverse.m2m_columns]
                     attr.converters = [c.converter for c in attr.meta.reverse.m2m_columns]
                     if attr.symmetric:
