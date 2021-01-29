e2baf9370216eafda6b916662055eeeee7d0b351
luckydonald <mac2019.git@luckydonald.de>
2021-01-29 23:53:06 +0100

    More tests, for crating values with it.

--- pony/orm/tests/test_enum.py
+++ pony/orm/tests/test_enum.py
@@ -80,6 +80,74 @@ class TestEnumCreation(unittest.TestCase):
 # end class
 
 
+class TestEnumInsertion(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # setup_database(db)
+        # with db_session:
+        #     d1 = Department(number=1)
+        # # end with
+        pass
+
+    # end def
+
+    @classmethod
+    def tearDownClass(cls):
+        # teardown_database(db)
+        pass
+    # end def
+
+    def setUp(self):
+        db = self.db = Database()
+
+        with db_session:
+            class FavoriteFruit(db.Entity):
+                id = PrimaryKey(int, auto=True)
+                user = Required(str)
+                fruit = Required(Fruits, size=16)
+            # end class
+
+            class TrafficLight(db.Entity):
+                id = PrimaryKey(int, auto=True)
+                state = Required(TrafficLightState)
+            # end class
+
+            # as those are not global, keep them around
+            self.FavoriteFruit = FavoriteFruit
+            self.TrafficLight = TrafficLight
+        # end with
+        setup_database(db)
+    # end def
+
+    def tearDown(self):
+        teardown_database(self.db)
+    # end def
+
+    def test_0(self):
+        """ Just make sure the setup did work """
+        db = self.db
+        self.assertEqual(self.FavoriteFruit, db.FavoriteFruit)
+        self.assertEqual(self.TrafficLight, db.TrafficLight)
+    # end def
+
+    def test__insert__int_enum(self):
+        FavoriteFruit = self.FavoriteFruit
+        with db_session:
+            FavoriteFruit(user="Me", fruit=Fruits.MANGO)
+            FavoriteFruit(user="Me", fruit=Fruits.BANANA)
+        # end with
+    # end def
+
+    def test__insert__str_enum(self):
+        TrafficLight = self.TrafficLight
+        with db_session:
+            TrafficLight(state=TrafficLightState.YELLOW)
+            TrafficLight(state=TrafficLightState.RED)
+        # end with
+    # end def
+# end class
+
+
 if __name__ == "__main__":
     unittest.main()
-
+# end if
