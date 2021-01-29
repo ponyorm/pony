d9874e0cdbb3f510ff98133bce44929de3344e8c
luckydonald <mac2019.git@luckydonald.de>
2021-01-29 23:53:46 +0100

    More tests, for using the enums in selects.

--- pony/orm/tests/test_enum.py
+++ pony/orm/tests/test_enum.py
@@ -145,9 +145,88 @@ class TestEnumInsertion(unittest.TestCase):
             TrafficLight(state=TrafficLightState.RED)
         # end with
     # end def
+
+    def test__select__int_enum(self):
+        FavoriteFruit = self.FavoriteFruit
+        with db_session:
+            select(ff for ff in FavoriteFruit if ff.fruit == Fruits.PEAR)
+            select(ff for ff in FavoriteFruit if ff.fruit != Fruits.PEAR)
+        # end with
+    # end def
+
+    def test__select__str_enum(self):
+        TrafficLight = self.TrafficLight
+        with db_session:
+            select(tl for tl in TrafficLight if tl.state == TrafficLightState.GREEN)
+            select(tl for tl in TrafficLight if tl.state != TrafficLightState.GREEN)
+        # end with
+    # end def
 # end class
 
 
 if __name__ == "__main__":
     unittest.main()
 # end if
+
+
+"""
+class TestProxy(unittest.TestCase):
+    def setUp(self):
+        setup_database(db)
+        with db_session:
+            c1 = Country(id=1, name='Russia')
+            c2 = Country(id=2, name='Japan')
+            Person(id=1, name='Alexander Nevskiy', country=c1)
+            Person(id=2, name='Raikou Minamoto', country=c2)
+            Person(id=3, name='Ibaraki Douji', country=c2)
+        # end with
+    # end def
+
+    def tearDown(self):
+        teardown_database(db)
+    # end def
+
+    def test_1(self):
+        with db_session:
+            p = make_proxy(Person[2])
+        # end with
+
+        with db_session:
+            x1 = db.local_stats[None].db_count  # number of queries
+            # it is possible to access p attributes in a new db_session
+            name = p.name
+            country = p.country
+            x2 = db.local_stats[None].db_count
+        # end with
+
+        # p.name and p.country are loaded with a single query
+        self.assertEqual(x1, x2 - 1)
+    # end def
+
+    def test_group_by_having(self):
+        result = set(select((s.age, sum(s.scholarship)) for s in Student if sum(s.scholarship) < 300))
+        self.assertEqual(result, {(20, 0), (21, 200)})
+        self.assertNotIn('distinct', db.last_sql.lower())
+    # end def
+
+    def test_aggregation_no_group_by_1(self):
+        result = set(select(sum(s.scholarship) for s in Student if s.age < 23))
+        self.assertEqual(result, {200})
+        self.assertNotIn('distinct', db.last_sql.lower())
+    # end def
+
+    def test_aggregation_no_group_by_2(self):
+        result = set(select((sum(s.scholarship), min(s.scholarship)) for s in Student if s.age < 23))
+        self.assertEqual(result, {(200, 0)})
+        self.assertNotIn('distinct', db.last_sql.lower())
+    # end def
+
+    def test_aggregation_no_group_by_3(self):
+        result = set(select((sum(s.scholarship), min(s.scholarship))
+                            for s in Student for g in Group
+                            if s.group == g and g.dept.number == 1))
+        self.assertEqual(result, {(400, 0)})
+        self.assertNotIn('distinct', db.last_sql.lower())
+    # end def
+# end class
+"""
