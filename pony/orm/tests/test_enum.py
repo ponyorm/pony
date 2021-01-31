4a3b045876ca877a3ce4facdd40cdea07ad1d345
luckydonald <mac2019.git@luckydonald.de>
2021-01-31 02:47:33 +0100

    Added more tests for the IntFlag Enum.

--- pony/orm/tests/test_enum.py
+++ pony/orm/tests/test_enum.py
@@ -98,6 +98,8 @@ class BinaryStuff(IntFlag):
     ONE_QUINTILLION_ONE_HUNDRED_AND_FIFTY_TWO_QUADRILLION_NINE_HUNDRED_AND_TWENTY_ONE_TRILLION_FIVE_HUNDRED_AND_FOUR_BILLION_SIX_HUNDRED_AND_SIX_MILLION_EIGHT_HUNDRED_AND_FORTY_SIX_THOUSAND_NINE_HUNDRED_AND_SEVENTY_SIX = auto()
     TWO_QUINTILLION_THREE_HUNDRED_AND_FIVE_QUADRILLION_EIGHT_HUNDRED_AND_FORTY_THREE_TRILLION_NINE_BILLION_TWO_HUNDRED_AND_THIRTEEN_MILLION_SIX_HUNDRED_AND_NINETY_THREE_THOUSAND_NINE_HUNDRED_AND_FIFTY_TWO = auto()
     FOUR_QUINTILLION_SIX_HUNDRED_AND_ELEVEN_QUADRILLION_SIX_HUNDRED_AND_EIGHTY_SIX_TRILLION_EIGHTEEN_BILLION_FOUR_HUNDRED_AND_TWENTY_SEVEN_MILLION_THREE_HUNDRED_AND_EIGHTY_SEVEN_THOUSAND_NINE_HUNDRED_AND_FOUR = auto()
+
+    ZERO = 0
     # The next one would fail for databases which can't do unsigned 64-bit/8-byte.
     # NINE_QUINTILLION_TWO_HUNDRED_AND_TWENTY_THREE_QUADRILLION_THREE_HUNDRED_AND_SEVENTY_TWO_TRILLION_THIRTY_SIX_BILLION_EIGHT_HUNDRED_AND_FIFTY_FOUR_MILLION_SEVEN_HUNDRED_AND_SEVENTY_FIVE_THOUSAND_EIGHT_HUNDRED_AND_EIGHT = auto()
 # end class
@@ -169,6 +171,7 @@ class TestEnumCreation(unittest.TestCase):
         """
         db = self.db
 
+        # noinspection PyUnusedLocal
         class CountingManually(db.Entity):
             id = PrimaryKey(int, auto=True)
             power_of_two = Required(BinaryStuff)
@@ -216,9 +219,15 @@ class TestEnumInsertion(unittest.TestCase):
                 state = Required(LightState)
             # end class
 
+            class CountingManually(db.Entity):
+                id = PrimaryKey(int, auto=True)
+                power_of_two = Required(BinaryStuff)
+            # end class
+
             # as those are not global, keep them around
             self.FavoriteFruit = FavoriteFruit
             self.TrafficLight = TrafficLight
+            self.CountingManually = CountingManually
         # end with
         setup_database(db)
     # end def
@@ -239,6 +248,7 @@ class TestEnumInsertion(unittest.TestCase):
         """
         Inserting integer Enums into the database
         """
+        # noinspection PyPep8Naming
         FavoriteFruit = self.FavoriteFruit
         with db_session:
             FavoriteFruit(user="Me", fruit=Fruits.MANGO)
@@ -257,10 +267,23 @@ class TestEnumInsertion(unittest.TestCase):
         # end with
     # end def
 
+    def test__insert__int_flag_enum(self):
+        """
+        Inserting string Enums into the database
+        """
+        # noinspection PyPep8Naming
+        CountingManually = self.CountingManually
+        with db_session:
+            CountingManually(power_of_two=BinaryStuff.TWO_HUNDRED_AND_SIXTY_EIGHT_MILLION_FOUR_HUNDRED_AND_THIRTY_FIVE_THOUSAND_FOUR_HUNDRED_AND_FIFTY_SIX)
+            CountingManually(power_of_two=BinaryStuff.FOUR_QUINTILLION_SIX_HUNDRED_AND_ELEVEN_QUADRILLION_SIX_HUNDRED_AND_EIGHTY_SIX_TRILLION_EIGHTEEN_BILLION_FOUR_HUNDRED_AND_TWENTY_SEVEN_MILLION_THREE_HUNDRED_AND_EIGHTY_SEVEN_THOUSAND_NINE_HUNDRED_AND_FOUR)
+        # end with
+    # end def
+
     def test__select__int_enum(self):
         """
         Integer Enums in an select statement
         """
+        # noinspection PyPep8Naming
         FavoriteFruit = self.FavoriteFruit
         with db_session:
             select(ff for ff in FavoriteFruit if ff.fruit == Fruits.PEAR)
@@ -272,10 +295,23 @@ class TestEnumInsertion(unittest.TestCase):
         """
         String Enums in an select statement
         """
+        # noinspection PyPep8Naming
         TrafficLight = self.TrafficLight
         with db_session:
             select(tl for tl in TrafficLight if tl.state == LightState.GREEN)
             select(tl for tl in TrafficLight if tl.state != LightState.GREEN)
+        # end with
+    # end def
+
+    def test__select__int_flag_enum(self):
+        """
+        String Enums in an select statement
+        """
+        # noinspection PyPep8Naming
+        CountingManually = self.CountingManually
+        with db_session:
+            select(cm for cm in CountingManually if cm.power_of_two == BinaryStuff.FOUR)
+            select(cm for cm in CountingManually if cm.power_of_two != BinaryStuff.SIXTY_FOUR)
         # end with
     # end def
 # end def
@@ -298,9 +334,15 @@ class TestEnumLoad(unittest.TestCase):
             state = Required(LightState)
         # end class
 
+        class CountingManually(db.Entity):
+            id = PrimaryKey(int, auto=True)
+            power_of_two = Required(BinaryStuff)
+        # end class
+
         # as those are not global, keep them around
         self.FavoriteFruit = FavoriteFruit
         self.TrafficLight = TrafficLight
+        self.CountingManually = CountingManually
         setup_database(db)
 
         with db_session:
@@ -309,6 +351,9 @@ class TestEnumLoad(unittest.TestCase):
 
             self.tl1 = TrafficLight(state=LightState.RED)
             self.tl2 = TrafficLight(state=LightState.GREEN)
+
+            self.cm1 = CountingManually(power_of_two=BinaryStuff.ONE)
+            self.cm2 = CountingManually(power_of_two=BinaryStuff.TWO)
         # end with
     # end def
 
@@ -320,6 +365,7 @@ class TestEnumLoad(unittest.TestCase):
         """
         Retrieving integer Enums from the database
         """
+        # noinspection PyPep8Naming
         FavoriteFruit = self.FavoriteFruit
         with db_session:
             ff1 = FavoriteFruit.get(user="Me")
@@ -355,6 +401,7 @@ class TestEnumLoad(unittest.TestCase):
         """
         Retrieving string Enums from the database
         """
+        # noinspection PyPep8Naming
         TrafficLight = self.TrafficLight
         with db_session:
             tl1 = TrafficLight.get(state=LightState.RED)
@@ -383,12 +430,45 @@ class TestEnumLoad(unittest.TestCase):
         self.assertIsInstance(tl2.state, LightState, msg="Loaded one must be LightState Enum")
     # end def
 
+    def test__load__int_flag_enum(self):
+        """
+        Retrieving IntFlag Enums from the database
+        """
+        # noinspection PyPep8Naming
+        CountingManually = self.CountingManually
+        with db_session:
+            cm1 = CountingManually.get(power_of_two=BinaryStuff.ONE)
+            cm2 = CountingManually.get(power_of_two=BinaryStuff.TWO)
+            cm0 = CountingManually.get(power_of_two=BinaryStuff.ZERO)
+        # end with
+
+        self.assertIsNone(cm0, msg="Requesting a value not in the database must return None")
+
+        self.assertEqual(self.cm1.id, cm1.id, msg="ID must be the same as the one inserted to the database")
+        self.assertEqual(self.cm2.id, cm2.id, msg="ID must be the same as the one inserted to the database")
+
+        self.assertEqual(self.cm1.power_of_two, cm1.power_of_two, msg="State (enum) must be the same as the one inserted to the database")
+        self.assertEqual(self.cm2.power_of_two, cm2.power_of_two, msg="State (enum) must be the same as the one inserted to the database")
+
+        self.assertIsInstance(self.cm1.power_of_two, Enum, msg="Original must be Enum")
+        self.assertIsInstance(self.cm2.power_of_two, Enum, msg="Original must be Enum")
+
+        self.assertIsInstance(self.cm1.power_of_two, BinaryStuff, msg="Original must be BinaryStuff Enum")
+        self.assertIsInstance(self.cm2.power_of_two, BinaryStuff, msg="Original must be BinaryStuff Enum")
+
+        self.assertIsInstance(cm1.power_of_two, Enum, msg="Loaded one must be Enum")
+        self.assertIsInstance(cm2.power_of_two, Enum, msg="Loaded one must be Enum")
+
+        self.assertIsInstance(cm1.power_of_two, BinaryStuff, msg="Loaded one must be BinaryStuff Enum")
+        self.assertIsInstance(cm2.power_of_two, BinaryStuff, msg="Loaded one must be BinaryStuff Enum")
+    # end def
+
     def test__to_json__int_enum(self):
         """
         to_json() of integer Enums
         """
-        self.assertEqual(Fruits.MANGO.value, +42, msg="Just to be sure the number of the enum is correct; needed below")
-        self.assertEqual(Fruits.BANANA.value, -7, msg="Just to be sure the number of the enum is correct; needed below")
+        self.assertEqual(Fruits.MANGO.value, +42, msg="Just to be sure the number of the enum is correct in the unittests; needed below")
+        self.assertEqual(Fruits.BANANA.value, -7, msg="Just to be sure the number of the enum is correct in the unittests; needed below")
 
         dict1 = self.ff1.to_dict()
         dict2 = self.ff2.to_dict()
@@ -401,14 +481,30 @@ class TestEnumLoad(unittest.TestCase):
         """
         to_json() of string Enums
         """
-        self.assertEqual(LightState.RED.value, '#f00', msg="Just to be sure the number of the enum is correct; needed below")
-        self.assertEqual(LightState.GREEN.value, '#00ff00', msg="Just to be sure the number of the enum is correct; needed below")
+        self.assertEqual(LightState.RED.value, '#f00', msg="Just to be sure the value of the enum is correct in the unittests; needed below")
+        self.assertEqual(LightState.GREEN.value, '#00ff00', msg="Just to be sure the value of the enum is correct in the unittests; needed below")
 
         dict1 = self.tl1.to_dict()
         dict2 = self.tl2.to_dict()
 
         self.assertDictEqual(dict1, {"id": 1, "state": '#f00'})
         self.assertDictEqual(dict2, {"id": 2, "state": '#00ff00'})
+    # end def
+
+    def test__to_json__int_flag_enum(self):
+        """
+        to_json() of string Enums
+        """
+        # self.cm1 = CountingManually(power_of_two=BinaryStuff.ONE)
+
+        self.assertEqual(BinaryStuff.ONE.value, 1, msg="Just to be sure the number of the enum is correct in the unittests; needed below")
+        self.assertEqual(BinaryStuff.TWO.value, 2, msg="Just to be sure the number of the enum is correct in the unittests; needed below")
+
+        dict1 = self.cm1.to_dict()
+        dict2 = self.cm2.to_dict()
+
+        self.assertDictEqual(dict1, {"id": 1, "power_of_two": 1})
+        self.assertDictEqual(dict2, {"id": 2, "power_of_two": 2})
     # end def
 # end class
 
