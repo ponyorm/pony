4e32ebf5af791a2646b3d5d29434cc0dec915b5e
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2022-12-14 18:34:38 +0100

    Add PY311 compat constant

--- pony/py23compat.py
+++ pony/py23compat.py
@@ -5,6 +5,7 @@ PY37 = sys.version_info[:2] >= (3, 7)
 PY38 = sys.version_info[:2] >= (3, 8)
 PY39 = sys.version_info[:2] >= (3, 9)
 PY310 = sys.version_info[:2] >= (3, 10)
+PY311 = sys.version_info[:2] >= (3, 11)
 
 unicode = str
 buffer = bytes
